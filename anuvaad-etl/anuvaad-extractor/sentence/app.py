659706ea2cbbb94255f3366593b63ba9f2f4a766
Zer0-dev115 <amit.tiwari.min11@gmail.com>
2020-05-29 15:14:18 +0530

    Commenting kafka

--- anuvaad-etl/anuvaad-extractor/sentence/app.py
+++ anuvaad-etl/anuvaad-extractor/sentence/app.py
@@ -17,11 +17,11 @@ for blueprint in vars(routes).values():
     if isinstance(blueprint, Blueprint):
         tok_app.register_blueprint(blueprint, url_prefix=config.API_URL_PREFIX)
 
-try:
-    t1 = threading.Thread(target=process_tokenization_kf, name='keep_on_running')
-    t1.start()
-except Exception as e:
-    logging.info('ERROR WHILE RUNNING CUSTOM THREADS '+str(e))
+# try:
+#     t1 = threading.Thread(target=process_tokenization_kf, name='keep_on_running')
+#     t1.start()
+# except Exception as e:
+#     logging.info('ERROR WHILE RUNNING CUSTOM THREADS '+str(e))
 
 if __name__ == "__main__":
     tok_app.run(host=config.HOST, port=config.PORT, debug=True)
