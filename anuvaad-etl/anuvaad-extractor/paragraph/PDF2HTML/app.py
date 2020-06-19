0a8df35ecf44bf03a52e2d27cabfe6dbc25705ce
Zer0-dev115 <amit.tiwari.min11@gmail.com>
2020-06-19 18:39:58 +0530

    git ignore

--- anuvaad-etl/anuvaad-extractor/paragraph/PDF2HTML/app.py
+++ anuvaad-etl/anuvaad-extractor/paragraph/PDF2HTML/app.py
@@ -31,34 +31,34 @@ if __name__ == "__main__":
     pdf2html_app.run(host=config.HOST, port=config.PORT, debug=False)
 
 # Log config
-# dictConfig({
-#     'version': 1,
-#     'formatters': {'default': {
-#         'format': '[%(asctime)s] {%(filename)s:%(lineno)d} %(threadName)s %(levelname)s in %(module)s: %(message)s',
-#     }},
-#     'handlers': {
-#         'info': {
-#             'class': 'logging.FileHandler',
-#             'level': 'DEBUG',
-#             'formatter': 'default',
-#             'filename': 'info.log'
-#         },
-#         'console': {
-#             'class': 'logging.StreamHandler',
-#             'level': 'DEBUG',
-#             'formatter': 'default',
-#             'stream': 'ext://sys.stdout',
-#         }
-#     },
-#     'loggers': {
-#         'file': {
-#             'level': 'DEBUG',
-#             'handlers': ['info', 'console'],
-#             'propagate': ''
-#         }
-#     },
-#     'root': {
-#         'level': 'DEBUG',
-#         'handlers': ['info', 'console']
-#     }
-# })
+dictConfig({
+    'version': 1,
+    'formatters': {'default': {
+        'format': '[%(asctime)s] {%(filename)s:%(lineno)d} %(threadName)s %(levelname)s in %(module)s: %(message)s',
+    }},
+    'handlers': {
+        'info': {
+            'class': 'logging.FileHandler',
+            'level': 'DEBUG',
+            'formatter': 'default',
+            'filename': 'info.log'
+        },
+        'console': {
+            'class': 'logging.StreamHandler',
+            'level': 'DEBUG',
+            'formatter': 'default',
+            'stream': 'ext://sys.stdout',
+        }
+    },
+    'loggers': {
+        'file': {
+            'level': 'DEBUG',
+            'handlers': ['info', 'console'],
+            'propagate': ''
+        }
+    },
+    'root': {
+        'level': 'DEBUG',
+        'handlers': ['info', 'console']
+    }
+})
