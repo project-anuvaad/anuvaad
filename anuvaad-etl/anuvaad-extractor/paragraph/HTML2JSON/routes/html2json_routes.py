91c7717209d750dbf882438720c002751ff6d0ec
Zer0-dev115 <amit.tiwari.min11@gmail.com>
2020-06-29 14:41:55 +0530

    end point change

--- anuvaad-etl/anuvaad-extractor/paragraph/HTML2JSON/routes/html2json_routes.py
+++ anuvaad-etl/anuvaad-extractor/paragraph/HTML2JSON/routes/html2json_routes.py
@@ -4,4 +4,4 @@ from resources.html2json_resource import Html2JsonConversion
 
 HTML2JSON_BLUEPRINT = Blueprint("HTML to Json", __name__)
 api = Api(HTML2JSON_BLUEPRINT)
-api.add_resource(Html2JsonConversion, "/HTML-to-JSON")
+api.add_resource(Html2JsonConversion, "/html-to-json")
