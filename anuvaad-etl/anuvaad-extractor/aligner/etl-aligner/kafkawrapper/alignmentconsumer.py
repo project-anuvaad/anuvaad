afe8631684ab4390108af3147a69485daf547b93
mahulivishal <vishalmahuli8@gmail.com>
2020-07-14 14:58:51 +0530

    changed consumer implementation

--- anuvaad-etl/anuvaad-extractor/aligner/etl-aligner/kafkawrapper/alignmentconsumer.py
+++ anuvaad-etl/anuvaad-extractor/aligner/etl-aligner/kafkawrapper/alignmentconsumer.py
@@ -1,7 +1,7 @@
 import json
 import logging
 
-from kafka import KafkaConsumer, OffsetAndMetadata
+from kafka import KafkaConsumer, OffsetAndMetadata, TopicPartition
 import os
 from service.alignmentservice import AlignmentService
 from logging.config import dictConfig
@@ -13,28 +13,36 @@ cluster_details = os.environ.get('KAFKA_CLUSTER_DETAILS', 'localhost:9092')
 align_job_topic = "anuvaad-etl-alignment-jobs-v3"
 align_job_consumer_grp = "anuvaad-etl-consumer-group"
 
-
 class Consumer:
 
     def __init__(self):
         pass
 
     # Method to instantiate the kafka consumer
-    def instantiate(self):
-        topics = [align_job_topic]
-        consumer = KafkaConsumer(*topics,
-                                 bootstrap_servers=[cluster_details],
+    def instantiate(self, topics):
+        topic_partitions = self.get_topic_paritions(topics)
+        consumer = KafkaConsumer(bootstrap_servers=[cluster_details],
                                  api_version=(1, 0, 0),
                                  group_id=align_job_consumer_grp,
                                  auto_offset_reset='latest',
                                  enable_auto_commit=False,
                                  max_poll_records=1,
                                  value_deserializer=lambda x: self.handle_json(x))
+        consumer.assign(topic_partitions)
         return consumer
+
+    # For all the topics, returns a list of TopicPartition Objects
+    def get_topic_paritions(self, topics):
+        topic_paritions = []
+        for topic in topics:
+            tp = TopicPartition(topic, 0) #for now the partition is hardocoded
+            topic_paritions.append(tp)
+        return topic_paritions
 
     # Method to read and process the requests from the kafka queue
     def consume(self):
-        consumer = self.instantiate()
+        topics = [align_job_topic]
+        consumer = self.instantiate(topics)
         service = AlignmentService()
         log.info("Align Consumer running.......")
         for msg in consumer:
@@ -42,11 +50,12 @@ class Consumer:
                 data = msg.value
                 log.info("Received on Topic: " + msg.topic)
                 service.process(data, False)
-                consumer.commit(OffsetAndMetadata(msg.offset + 1, None))
             except Exception as e:
                 log.exception("Exception while consuming: " + str(e))
                 post_error("ALIGNER_CONSUMER_ERROR", "Exception while consuming: " + str(e), None)
-                continue
+            finally:
+                partitions = consumer.partitions_for_topic(msg.topic)
+                consumer.commit(OffsetAndMetadata(msg.offset + 1, partitions))
 
     # Method that provides a deserialiser for the kafka record.
     def handle_json(self, x):
