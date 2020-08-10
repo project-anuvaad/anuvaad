11c72efa28978c27131e512328bce197eee55875
naresh <saininaresh4435@gmail.com>
2020-08-10 19:27:21 +0530

    block merger logic added to service package

--- anuvaad-etl/anuvaad-extractor/block-merger/src/services/service.py
+++ anuvaad-etl/anuvaad-extractor/block-merger/src/services/service.py
@@ -7,8 +7,34 @@ class BlockMerging(object):
         pass       
 
     # after successful block merging returning data
-    def merge_blocks(self,data):
-        log.info("Block Merging started")
+    def get_children(self, block):
+        if block['children']==None:
+            return block['text']
+        else:
+            text = ""
+            for sub_block in block['children']:
+                if sub_block['children']== None:
+                    text = text+" " + sub_block['text']
+                    continue
+                else:
+                    for sub_sub_block in  sub_block['children']:
+                        text = text+" " + sub_sub_block['text']
+            return text
+
+    def get_block(self, data):
+
+        for page_index in range(len(data)):
+            page_blocks = data[page_index]['blocks']
+            for index,block in enumerate(page_blocks): 
+                text = self.get_children(block)
+                page_blocks[index]['text']=str(text)
+                
+            data[page_index]['blocks']  = page_blocks
+        return data
+
+    def merge_blocks(self, data):
+        log.info("Block Merging started ===>")
+        data = self.get_block(data['result'])
         log.info("Block Merging completed")
         return data
 
