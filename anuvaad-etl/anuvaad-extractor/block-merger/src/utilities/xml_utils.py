e4d93a0185a0ddb4ecbacb86ceb87d68613e3908
naresh <saininaresh4435@gmail.com>
2020-08-14 13:48:14 +0530

    xml-utils  updated

--- anuvaad-etl/anuvaad-extractor/block-merger/src/utilities/xml_utils.py
+++ anuvaad-etl/anuvaad-extractor/block-merger/src/utilities/xml_utils.py
@@ -89,7 +89,7 @@ def get_xmltree(filepath, parse='xml'):
         tree   = etree.parse(open(filepath, mode='r', encoding='utf-8'), parser)
         return tree
     else:
-        with open(filepath,'r',encoding='utf-8') as file:
+        with open(filepath,'r',encoding='latin-1') as file:
             xml_string    = file.read()
             return etree.fromstring(bytes(xml_string, encoding='utf-8'))
         
