1c41f505e936732412cb088cacb3018ac28dee09
Zer0-dev115 <amit.tiwari.min11@gmail.com>
2020-08-17 14:47:20 +0530

    mid function response control changes

--- anuvaad-etl/anuvaad-extractor/sentence/etl-tokeniser/kafka_module/kf_service.py
+++ anuvaad-etl/anuvaad-extractor/sentence/etl-tokeniser/kafka_module/kf_service.py
@@ -13,12 +13,12 @@ import config
 
 def consumer_validator():
     try:
-        consumer_class = Consumer(config.tok_input_topic, config.bootstrap_server)
+        consumer_class = Consumer(config.input_topic, config.bootstrap_server)
         consumer = consumer_class.consumer_instantiate()
         log_info("consumer_validator","--- consumer running -----", None)
         return consumer
     except:
-        log_exception("consumer_validator", "error in kafka opertation while listening to consumer on topic %s"%(config.tok_input_topic), None, None)
+        log_exception("consumer_validator", "error in kafka opertation while listening to consumer on topic %s"%(config.input_topic), None, None)
         raise KafkaConsumerError(400, "Can not connect to consumer.")
 
 def push_output(producer, topic_name, output, jobid, task_id):
@@ -37,26 +37,21 @@ def process_tokenization_kf():
     task_id = str("TOK-" + str(time.time()).replace('.', ''))
     task_starttime = str(time.time()).replace('.', '')
     producer_tok = Producer(config.bootstrap_server)
-    print("kafka")
     # instatiation of consumer for respective topic
     try:
         consumer = consumer_validator()
-        print("consumer validated")
         log_info("process_tokenization_kf", "trying to receive value from consumer ", None)
         for msg in consumer:
-            log_info("process_tokenization_kf", "value received from consumer", None)
             data = msg.value
-            print("data received from consumer")
             task_id = str("TOK-" + str(time.time()).replace('.', ''))
             task_starttime = str(time.time()).replace('.', '')
             input_files, workflow_id, jobid, tool_name, step_order = file_ops.json_input_format(data)
             response_gen = Response(data, DOWNLOAD_FOLDER)
             file_value_response = response_gen.workflow_response(task_id, task_starttime)
-            print("response generated", file_value_response)
             if "errorID" not in file_value_response.keys():
-                print("pushing data")
-                push_output(producer, config.tok_output_topic, file_value_response, jobid)
-                log_info("process_tokenization_kf", "response send to topic %s"%(config.tok_output_topic), None)
+                producer = Producer(config.bootstrap_server)
+                push_output(producer, config.output_topic, file_value_response, jobid, task_id)
+                log_info("process_tokenization_kf", "response send to topic %s"%(config.output_topic), None)
             else:
                 log_info("process_tokenization_kf", "error send to error handler", jobid)
     except KafkaConsumerError as e:
@@ -68,4 +63,4 @@ def process_tokenization_kf():
         response_custom = e.code
         response_custom['message'] = e.message      
         file_ops.error_handler(response_custom, "KAFKA_PRODUCER_ERROR", True)
-        log_exception("process_tokenization_kf", "response send to topic %s"%(config.tok_output_topic), response_custom['jobID'], e)
+        log_exception("process_tokenization_kf", "response send to topic %s"%(config.output_topic), response_custom['jobID'], e)
