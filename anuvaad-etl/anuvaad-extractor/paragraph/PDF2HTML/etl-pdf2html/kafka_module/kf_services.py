0b7ea8e630c7480aaf70c2057f6d2c76f36e7968
Zer0-dev115 <amit.tiwari.min11@gmail.com>
2020-08-20 17:26:57 +0530

    changes

--- anuvaad-etl/anuvaad-extractor/paragraph/PDF2HTML/etl-pdf2html/kafka_module/kf_services.py
+++ anuvaad-etl/anuvaad-extractor/paragraph/PDF2HTML/etl-pdf2html/kafka_module/kf_services.py
@@ -15,17 +15,16 @@ import config
 def process_pdf_kf():
     file_ops = FileOperation()
     DOWNLOAD_FOLDER =file_ops.create_file_download_dir(config.download_folder)
-    task_id = str("TOK-" + str(time.time()).replace('.', ''))
+    task_id = str("PDF2HTML-" + str(time.time()).replace('.', ''))
     task_starttime = str(time.time()).replace('.', '')
-    producer_tok = Producer(config.bootstrap_server)
     # instatiation of consumer for respective topic
     try:
         consumer_class = Consumer(config.input_topic, config.bootstrap_server)
         consumer = consumer_class.consumer_instantiate()
-        log_info("process_tokenization_kf", "trying to receive value from consumer ", None)
+        log_info("process_pdf_kf", "trying to receive value from consumer ", None)
         for msg in consumer:
             data = msg.value
-            task_id = str("TOK-" + str(time.time()).replace('.', ''))
+            task_id = str("PDF2HTML-" + str(time.time()).replace('.', ''))
             task_starttime = str(time.time()).replace('.', '')
             input_files, workflow_id, jobid, tool_name, step_order = file_ops.json_input_format(data)
             response_gen = Response(data, DOWNLOAD_FOLDER)
@@ -34,14 +33,14 @@ def process_pdf_kf():
                 producer = Producer()
                 producer.push_data_to_queue(config.output_topic, file_value_response, jobid, task_id)
             else:
-                log_info("process_tokenization_kf", "error send to error handler", jobid)
+                log_info("process_pdf_kf", "error send to error handler", jobid)
     except KafkaConsumerError as e:
         response_custom = CustomResponse(Status.ERR_STATUS.value, None, None)
         response_custom['message'] = str(e)
         file_ops.error_handler(response_custom, "KAFKA_CONSUMER_ERROR", True)
-        log_exception("process_tokenization_kf", "Consumer didn't instantiate", None, e)
+        log_exception("process_pdf_kf", "Consumer didn't instantiate", None, e)
     except KafkaProducerError as e:
         response_custom = e.code
         response_custom['message'] = e.message      
         file_ops.error_handler(response_custom, "KAFKA_PRODUCER_ERROR", True)
-        log_exception("process_tokenization_kf", "response send to topic %s"%(config.output_topic), response_custom['jobID'], e)
+        log_exception("process_pdf_kf", "response send to topic %s"%(config.output_topic), response_custom['jobID'], e)
