9a46317287a624477ce60565f13abd43ee3228fd
Zer0-dev115 <amit.tiwari.min11@gmail.com>
2020-09-05 20:27:24 +0530

    checking save content api response

--- anuvaad-etl/anuvaad-extractor/sentence/etl-tokeniser/services/service.py
+++ anuvaad-etl/anuvaad-extractor/sentence/etl-tokeniser/services/service.py
@@ -87,6 +87,6 @@ class Tokenisation(object):
     def sending_data_to_content_handler(self, job_id, user_id, tokenised_block_json):
         data = {"process_identifier" : job_id, "pages" : tokenised_block_json['result']}
         headers = {"userid": user_id ,"Content-Type": "application/json"}
-        response = requests.post(url = config.internal_gateway_url_save_data, data = data, headers = headers)
+        response = requests.post(url = config.internal_gateway_url_save_data, json = data, headers = headers)
         log_info("tokenised block merger response saved in db %s"%(response.content), self.input_json_data)
         
