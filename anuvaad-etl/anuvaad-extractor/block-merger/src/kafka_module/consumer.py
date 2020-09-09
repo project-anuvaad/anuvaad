8c9c875826fc3916472e7f26b7fc0c6b4601fb22
kd <0xbeefdead@gmail.com>
2020-09-09 10:29:42 +0530

    borrowed consumer & producer part from file-converter

--- anuvaad-etl/anuvaad-extractor/block-merger/src/kafka_module/consumer.py
+++ anuvaad-etl/anuvaad-extractor/block-merger/src/kafka_module/consumer.py
@@ -1,6 +1,5 @@
 from kafka import KafkaConsumer
 from json import loads
-from src.errors.errors_exception import KafkaConsumerError
 from anuvaad_auditor.loghandler import log_info
 from anuvaad_auditor.loghandler import log_exception
 import config
@@ -15,11 +14,11 @@ class Consumer(object):
     def consumer_instantiate(self):
         try:
             consumer = KafkaConsumer(self.topic_name, bootstrap_servers = [self.server_address], auto_offset_reset = 'latest', 
-                                    enable_auto_commit=True, group_id = config.group_id, value_deserializer=lambda x: loads(x.decode('utf-8')))
-            log_info("consumer_instantiate", "Consumer returned for topic: %s"%(self.topic_name), None)
+                        group_id = config.CONSUMER_GROUP,
+                                    enable_auto_commit=True, value_deserializer=lambda x: loads(x.decode('utf-8')))
+            log_info("consumer_instantiate : Consumer returned for topic: %s"%(self.topic_name), None)
             return consumer
-        except:
-            log_exception("consumer_instantiate", "error occured for consumer topic: %s"%(self.topic_name), None, None)
-            raise KafkaConsumerError(400, "Can not connect to consumer.")
+        except Exception as e:
+            log_exception("consumer_instantiate : error occured for consumer topic: %s"%(self.topic_name), None, e)
 
 
