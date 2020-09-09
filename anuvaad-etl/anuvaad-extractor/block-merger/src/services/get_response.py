024ac07492575ad1c5dc7d9587a72d4c8cbf4d7e
kd <0xbeefdead@gmail.com>
2020-09-09 12:55:34 +0530

    removed unwanted logs

--- anuvaad-etl/anuvaad-extractor/block-merger/src/services/get_response.py
+++ anuvaad-etl/anuvaad-extractor/block-merger/src/services/get_response.py
@@ -8,7 +8,6 @@ import src.utilities.app_context as app_context
 import time
 
 def df_to_json(p_df):
-    start_time = time.time()
     page_data = []
     try:
         p_df      = p_df.where(p_df.notnull(), None)
@@ -36,10 +35,6 @@ def df_to_json(p_df):
                 page_data.append(block)
         else:
             page_data = None
-
-        end_time = time.time()
-        extraction_time = end_time - start_time
-        log_info('Generating response of p_df completed in {}'.format(extraction_time), app_context.application_context)
             
     except Exception as e :
         log_error('Error in generating response of p_df', app_context.application_context, e)
@@ -48,7 +43,6 @@ def df_to_json(p_df):
     return page_data
 
 def process_image_df(img_df):
-    start_time = time.time()
     image_data = []
     try:
         if len(img_df)>0:
@@ -66,13 +60,9 @@ def process_image_df(img_df):
         log_error('Error in generating response of img_df', app_context.application_context, e)
         return None
 
-    end_time            = time.time()
-    extraction_time     = end_time - start_time
-    log_info('Generating response of img_df completed in {}'.format(extraction_time), app_context.application_context)
     return image_data
 
 def process_table_df(table_df):
-    start_time = time.time()
     table_data = []
     try:
         if len(table_df)>0:
@@ -93,13 +83,10 @@ def process_table_df(table_df):
     except Exception as e :
         log_error('Error in generating response of table_df', app_context.application_context, e)
         return None
-    end_time            = time.time()
-    extraction_time     = end_time - start_time
-    log_info('Generating response of table_df completed in {}'.format(extraction_time), app_context.application_context)
+
     return table_data       
 
 def process_line_df(line_df):
-    start_time = time.time()
     line_data  = []
     try:
         if len(line_df)>0:
@@ -114,9 +101,7 @@ def process_line_df(line_df):
     except Exception as e :
         log_error('Error in generating response of line_df', app_context.application_context, e)
         return None
-    end_time            = time.time()
-    extraction_time     = end_time - start_time
-    log_info('Generating response of line_df completed in {}'.format(extraction_time), app_context.application_context)
+        
     return line_data        
 
 
