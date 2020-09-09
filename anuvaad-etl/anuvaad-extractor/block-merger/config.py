54b4b85483a59a46c252887c235c45597ac07c84
naresh <saininaresh4435@gmail.com>
2020-09-09 15:51:38 +0530

    topic name added to config

--- anuvaad-etl/anuvaad-extractor/block-merger/config.py
+++ anuvaad-etl/anuvaad-extractor/block-merger/config.py
@@ -11,9 +11,9 @@ BASE_DIR      = 'upload'
 ENABLE_CORS = False
 
 # kafka
-input_topic         = 'anuvaad-dp-tools-block-merger-input-v1'
+input_topic         = 'anuvaad-dp-tools-block-merger-input-v2'
 kf_local_server     = 'localhost:9092'
-output_topic        = 'anuvaad-dp-tools-block-merger-output-v1'
+output_topic        = 'anuvaad-dp-tools-block-merger-output-v2'
 kafka_ip_host       = 'KAFKA_IP_HOST'
 bootstrap_server    = os.environ.get(kafka_ip_host, kf_local_server)
 TASK_STAT           = 'BLOCK-MERGER'
