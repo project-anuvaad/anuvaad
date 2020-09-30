b41d44f998847282410836b610d347baa41a5cb7
Zer0-dev115 <amit.tiwari.min11@gmail.com>
2020-09-30 13:42:12 +0530

    kafka variable changes

--- anuvaad-etl/anuvaad-extractor/file-converter/src/config.py
+++ anuvaad-etl/anuvaad-extractor/file-converter/src/config.py
@@ -25,12 +25,12 @@ MONGO_SERVER_URL = os.environ.get(MONGO_IP, DEFAULT_VALUE)
 MONGO_DB = os.environ.get(MONGO_DB_IDENTIFIER, DEFAULT_MONGO_DB_IDENTIFIER)
 
 # kafka
-tok_input_topic_default = 'anuvaad-dp-tools-fc-input'
-tok_input_topic_identifier = 'KAFKA_ANUVAAD_DP_TOOLS_FC_INPUT_TOPIC'
+tok_input_topic_default = 'anuvaad-dp-tools-fc-input-v1'
+tok_input_topic_identifier = 'KAFKA_ANUVAAD_DP_TOOLS_FC_INPUT'
 tok_input_topic = os.environ.get(tok_input_topic_identifier, tok_input_topic_default)
 
-tok_output_topic_default = 'anuvaad-dp-tools-fc-output'
-tok_output_topic_identifier = 'KAFKA_ANUVAAD_DP_TOOLS_FC_OUTPUT_TOPIC'
+tok_output_topic_default = 'anuvaad-dp-tools-fc-output-v1'
+tok_output_topic_identifier = 'KAFKA_ANUVAAD_DP_TOOLS_FC_OUTPUT'
 tok_output_topic = os.environ.get(tok_output_topic_identifier, tok_output_topic_default)
 
 kf_local_server = 'localhost:9092'
