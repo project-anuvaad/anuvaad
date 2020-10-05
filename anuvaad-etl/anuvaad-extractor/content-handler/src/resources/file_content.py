f4c06b85772948592293d7ea3fdcef6ab52f4fab
kd <0xbeefdead@gmail.com>
2020-10-05 19:04:42 +0530

    made job_id optional in the query string

--- anuvaad-etl/anuvaad-extractor/content-handler/src/resources/file_content.py
+++ anuvaad-etl/anuvaad-extractor/content-handler/src/resources/file_content.py
@@ -31,7 +31,7 @@ class FileContentGetResource(Resource):
         parser.add_argument('start_page', type=int, location='args', help='start_page can be 0, set start_page & end_page as 0 to get entire document', required=True)
         parser.add_argument('end_page',  type=int, location='args', help='end_page can be 0, set start_page & end_page as 0 to get entire document', required=True)
         parser.add_argument('ad-userid', location='headers', type=str, help='userid cannot be empty', required=True)
-        parser.add_argument('job_id', type=str, location='args', help='Job Id is required', required=True)
+        parser.add_argument('job_id', type=str, location='args', help='Job Id is required', required=False)
         parser.add_argument('record_id', type=str, location='args', help='record_id is required', required=True)
 
         args    = parser.parse_args()
