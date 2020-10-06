9c5d7a472f7a696179669820144aeaf52c24de11
Zer0-dev115 <amit.tiwari.min11@gmail.com>
2020-10-06 13:27:01 +0530

    ignore and response changes

--- anuvaad-etl/anuvaad-extractor/document-converter/src/models/response.py
+++ anuvaad-etl/anuvaad-extractor/document-converter/src/models/response.py
@@ -11,7 +11,6 @@ from flask import jsonify
 class CustomResponse :
     def __init__(self, statuscode, data, count=0):
         self.statuscode = statuscode
-        self.statuscode['data'] = data
         self.statuscode['count'] = count
     
     def getres(self):
