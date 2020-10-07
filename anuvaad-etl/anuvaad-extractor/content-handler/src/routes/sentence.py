410e553113c1b28bac2ef67737cd610a998f8f04
kd <0xbeefdead@gmail.com>
2020-10-07 20:55:16 +0530

    updated sentence api

--- anuvaad-etl/anuvaad-extractor/content-handler/src/routes/sentence.py
+++ anuvaad-etl/anuvaad-extractor/content-handler/src/routes/sentence.py
@@ -1,18 +1,14 @@
 from flask import Blueprint
 from flask_restful import Api
 
-from resources import SentenceGetResource, SentencePostResource, SentenceBlockGetResource
+from resources import SaveSentenceResource, FetchSentenceResource
 
 SENTENCE_BLUEPRINT = Blueprint("sentence", __name__)
 
 Api(SENTENCE_BLUEPRINT).add_resource(
-    SentenceGetResource, "/sentence/<user_id>/<s_id>"
+    FetchSentenceResource, "/fetch-content-sentence"
 )
 
 Api(SENTENCE_BLUEPRINT).add_resource(
-    SentenceBlockGetResource, "/sentence/block/<user_id>/<s_id>"
-)
-
-Api(SENTENCE_BLUEPRINT).add_resource(
-    SentencePostResource, "/sentence/<user_id>"
+    SaveSentenceResource, "/save-content-sentence"
 )
