ebd2fb70bd077dc12601796c2599c100a255d08c
Zer0-dev115 <amit.tiwari.min11@gmail.com>
2020-10-08 13:42:04 +0530

    CH fetch-content url change

--- anuvaad-etl/anuvaad-extractor/document-converter/src/utilities/utils.py
+++ anuvaad-etl/anuvaad-extractor/document-converter/src/utilities/utils.py
@@ -41,6 +41,6 @@ class DocumentUtilities():
         px_to_inches   = 1.0 / float(dpi)
         return math.ceil(px * px_to_inches * INCH_TO_TWIPS)
 
-    def url_generation(self, url_pre, record_id, job_id, start_page, end_page):
-        url_modified = url_pre + '?record_id={}&job_id={}&start_page={}&end_page={}'.format(record_id, job_id, start_page, end_page)
+    def url_generation(self, url_pre, record_id, start_page, end_page):
+        url_modified = url_pre + '?record_id={}&start_page={}&end_page={}'.format(record_id, start_page, end_page)
         return url_modified
