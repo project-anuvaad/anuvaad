cdae6c9c1693b45770de8986c2dffe038c25a9ae
Zer0-dev115 <amit.tiwari.min11@gmail.com>
2020-10-08 16:32:41 +0530

    dummy doc

--- anuvaad-etl/anuvaad-extractor/document-converter/src/services/service.py
+++ anuvaad-etl/anuvaad-extractor/document-converter/src/services/service.py
@@ -8,7 +8,7 @@ def document_saving(record_id, user_id, download_folder):
         log_info("document formation started", MODULE_CONTEXT)
         doc_conversion = DocumentConversion(download_folder)
         pages = doc_conversion.get_data_from_content_handler(record_id, user_id)
-        dataframes, page_layout = doc_conversion.convert_page_data_into_dataframes(pages)
+        #dataframes, page_layout = doc_conversion.convert_page_data_into_dataframes(pages)
         #output_filename = doc_conversion.document_creation(dataframes, page_layout, record_id)
         output_filename = doc_conversion.dummy_doc(record_id)
         log_info("document saving completed: %s"%output_filename, MODULE_CONTEXT)
