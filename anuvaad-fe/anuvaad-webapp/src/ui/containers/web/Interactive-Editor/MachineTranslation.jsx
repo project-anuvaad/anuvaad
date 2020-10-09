9a32416bf861f3df843b4393a702db3ccc232ffd
meghaMS <meghana.suresh@tarento.com>
2020-10-09 10:03:26 +0530

    Changes for Mach trans page

--- anuvaad-fe/anuvaad-webapp/src/ui/containers/web/Interactive-Editor/MachineTranslation.jsx
+++ anuvaad-fe/anuvaad-webapp/src/ui/containers/web/Interactive-Editor/MachineTranslation.jsx
@@ -8,24 +8,31 @@ class MachineTranslation extends Component {
   constructor() {
     super();
     this.state = {
-    
+
     };
   }
 
   render() {
     const { classes, sentence } = this.props;
     return (
-        <Paper className={classes.paper} >
-            <div>
-            <Typography variant="h5" gutterBottom style={{ color: '#000000', marginLeft: "30px", textAlign: "left" }} >Machine Translation</Typography>
-          </div>
-           
-            <hr/>
-            <div className={classes.div} >{sentence && sentence.src}</div>
-            <hr/>
-            <div>{sentence&& sentence.tgt}</div>
-        </Paper>
+      <Paper className={classes.paper} >
+        <div>
+          <Typography variant="h5" gutterBottom className={classes.header} >Machine Translation</Typography>
+        </div>
+
+        <hr />
+        <div className={classes.div} >
+          {sentence && sentence.src}
+
+        </div>
+        <hr />
+        <div className={classes.div}>
+          {sentence && sentence.tgt}
+
+        </div>
+      </Paper>
     );
+
   }
 }
 
--- anuvaad-fe/anuvaad-webapp/src/ui/styles/web/MachineTranslationStyle.js
+++ anuvaad-fe/anuvaad-webapp/src/ui/styles/web/MachineTranslationStyle.js
@@ -1,13 +1,20 @@
 const MachineTranslationStyles = theme => ({
     paper: {
         border: "1px solid #1C9AB7",
-        minHeight:"60%"
+        minHeight:"60%",
+        backgroundColor: "#F4FDFF"
     },
     header: {
-      fontSize: 14
+      color: '#000000', 
+      paddingLeft: "3%", 
+      textAlign: "left",
     },
     div:{
-      minHeight:"60%"
+      // maxHeight:"50%",
+      // overflowY: "scroll",
+      // height: "50%",
+      minHeight:"60%",
+      padding: "0% 3%"
     }
 
   });
