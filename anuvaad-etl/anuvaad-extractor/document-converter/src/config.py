7e1dbca6934ac8635be7e294c2eeba3f84b560de
kd <0xbeefdead@gmail.com>
2020-10-14 17:48:11 +0530

    corrected syntax issues

--- anuvaad-etl/anuvaad-extractor/document-converter/src/config.py
+++ anuvaad-etl/anuvaad-extractor/document-converter/src/config.py
@@ -12,5 +12,5 @@ ENABLE_CORS = False
 download_folder = 'upload'
 
 # internal url
-CONTENT_HANDLER_ENDPOINT    = os.environ.get('CONTENT_HANDLER_SERVER_URL', 'http://gateway_anuvaad-content-handler:5001/)
+CONTENT_HANDLER_ENDPOINT    = os.environ.get('CONTENT_HANDLER_SERVER_URL', 'http://gateway_anuvaad-content-handler:5001/')
 FILE_CONVERTER_ENDPOINT     = os.environ.get('FILE_CONVERTER_SERVER_URL', 'http://gateway_anuvaad-file-converter:5001/')
