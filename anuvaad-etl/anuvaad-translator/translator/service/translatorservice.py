e6bd8462b9f049619ae1bd3003b544c3e94e7e20
mahulivishal <vishalmahuli8@gmail.com>
2020-10-19 00:17:42 +0530

    bug fix in sentence count

--- anuvaad-etl/anuvaad-translator/translator/service/translatorservice.py
+++ anuvaad-etl/anuvaad-translator/translator/service/translatorservice.py
@@ -30,11 +30,13 @@ class TranslatorService:
             try:
                 dumped = self.dump_file_to_db(file["path"], translate_wf_input)
                 if not dumped:
-                    error_list.append({"inputFile": str(file["path"]), "outputFile": "FAILED", "error": "File received couldn't be downloaded!"})
+                    error_list.append({"inputFile": str(file["path"]), "outputFile": "FAILED",
+                                       "error": "File received couldn't be downloaded!"})
                 else:
                     pushed = self.push_sentences_to_nmt(file, translate_wf_input)
                     if not pushed:
-                        error_list.append({"inputFile": str(file["path"]), "outputFile": "FAILED", "error": "Error while pushing sentences to NMT"})
+                        error_list.append({"inputFile": str(file["path"]), "outputFile": "FAILED",
+                                           "error": "Error while pushing sentences to NMT"})
             except Exception as e:
                 log_exception("Exception while posting sentences to NMT: " + str(e), translate_wf_input, e)
                 continue
@@ -61,8 +63,6 @@ class TranslatorService:
                     "totalSentences": -1, "translatedSentences": -2, "skippedSentences": -2, "data": data
                 }
                 repo.create(db_in)
-                content_from_db = self.get_content_from_db(str(translate_wf_input["jobID"]) + "|" + str(file_id), None, translate_wf_input)
-                log_info(content_from_db, translate_wf_input)
                 return True
         except Exception as e:
             log_exception("Exception while dumping content to DB: " + str(e), translate_wf_input, e)
@@ -75,7 +75,8 @@ class TranslatorService:
             record_id = str(translate_wf_input["jobID"]) + "|" + str(file["path"])
             content_from_db = self.get_content_from_db(record_id, None, translate_wf_input)
             if not content_from_db:
-                log_error("File content from DB couldn't be fetched, jobID: " + str(translate_wf_input["jobID"]), translate_wf_input, None)
+                log_error("File content from DB couldn't be fetched, jobID: " + str(translate_wf_input["jobID"]),
+                          translate_wf_input, None)
                 return None
             content_from_db = content_from_db[0]
             data = content_from_db["data"]
@@ -94,16 +95,17 @@ class TranslatorService:
                 for batch_no in batches.keys():
                     batch = batches[batch_no]
                     record_id_enhanced = record_id + "|" + str(len(batch))
-                    nmt_in = {"url_end_point": file["model"]["url_end_point"], "record_id": record_id_enhanced, "message": batch}
+                    nmt_in = {"url_end_point": file["model"]["url_end_point"], "record_id": record_id_enhanced,
+                              "message": batch}
                     producer.produce(nmt_in, anu_nmt_input_topic)
                     sentences_per_page += len(batch)
                     total_sentences += len(batch)
-                log_info("PAGE NO: " + str(page["page_no"]) + " | SENTENCES: " + str(sentences_per_page), translate_wf_input)
+                log_info("PAGE NO: " + str(page["page_no"]) + " | SENTENCES: " + str(sentences_per_page),
+                         translate_wf_input)
             if total_sentences > 0:
                 repo.update({"totalSentences": total_sentences}, {"recordID": record_id})
-                log_info("recordID: " + record_id + " | TOTAL NO. OF SENTENCES SENT TO NMT : " + str(total_sentences), translate_wf_input)
-                content_from_db = self.get_content_from_db(record_id, None, translate_wf_input)
-                log_info(content_from_db, translate_wf_input)
+                log_info("recordID: " + record_id + " | TOTAL NO. OF SENTENCES SENT TO NMT : " + str(total_sentences),
+                         translate_wf_input)
                 return True
             else:
                 log_error("No sentences sent to NMT, recordID: " + record_id, translate_wf_input, None)
@@ -121,7 +123,8 @@ class TranslatorService:
             page_no = page["page_no"]
             text_blocks = page["text_blocks"]
             if text_blocks:
-                sentences_for_trans, batch_key = self.fetch_batches_of_blocks(record_id, page_no, text_blocks, file, sentences_for_trans, translate_wf_input)
+                sentences_for_trans, batch_key = self.fetch_batches_of_blocks(record_id, page_no, text_blocks, file,
+                                                                              sentences_for_trans, translate_wf_input)
                 log_info("Text Blocks batched.", translate_wf_input)
             else:
                 log_error("There are no text blocks for this page: " + str(page_no), translate_wf_input, None)
@@ -138,7 +141,8 @@ class TranslatorService:
             if 'tokenized_sentences' in block.keys():
                 for sentence in block["tokenized_sentences"]:
                     node_id = str(record_id) + "|" + str(page_no) + "|" + str(block_id)
-                    sent_nmt_in = {"src": sentence["src"], "s_id": sentence["s_id"], "id": file["model"]["model_id"], "n_id": node_id}
+                    sent_nmt_in = {"src": sentence["src"], "s_id": sentence["s_id"], "id": file["model"]["model_id"],
+                                   "n_id": node_id}
                     if batch_key in sentences_for_trans.keys():
                         sentence_list = sentences_for_trans[batch_key]
                         sentence_list.append(sent_nmt_in)
@@ -164,7 +168,8 @@ class TranslatorService:
             translate_wf_input = {"jobID": job_id, "metadata": {"module": tool_translator}}
             file = self.get_content_from_db(record_id, None, translate_wf_input)
             if not file:
-                log_error("There is no data for this recordID: " + str(record_id), translate_wf_input, nmt_output["status"])
+                log_error("There is no data for this recordID: " + str(record_id), translate_wf_input,
+                          nmt_output["status"])
                 return None
             file, skip_count, trans_count = file[0], 0, 0
             translate_wf_input = file["transInput"]
@@ -173,7 +178,8 @@ class TranslatorService:
                 if 'statusCode' in nmt_output["status"].keys():
                     if nmt_output["status"]["statusCode"] != 200:
                         skip_count += batch_size
-                        log_error("Error from NMT: " + str(nmt_output["status"]["why"]), translate_wf_input, nmt_output["status"])
+                        log_error("Error from NMT: " + str(nmt_output["status"]["why"]), translate_wf_input,
+                                  nmt_output["status"])
             if 'response_body' in nmt_output.keys():
                 sentences_of_the_batch = []
                 for response in nmt_output["response_body"]:
@@ -184,7 +190,8 @@ class TranslatorService:
                         continue
                     sentences_of_the_batch.append(response)
                 try:
-                    self.update_sentences(record_id, sentences_of_the_batch, translate_wf_input)  # Find a way to do batch update directly on MongoDB
+                    self.update_sentences(record_id, sentences_of_the_batch,
+                                          translate_wf_input)  # Find a way to do batch update directly on MongoDB
                     trans_count += len(sentences_of_the_batch)
                 except Exception as e:
                     log_exception("Exception while saving translations: " + str(e), translate_wf_input, e)
@@ -209,15 +216,19 @@ class TranslatorService:
     # Updates the no of sentences translated after every iteration.
     def update_translation_status(self, record_id, trans_count, skip_count, translate_wf_input):
         content_from_db = self.get_content_from_db(record_id, None, translate_wf_input)[0]
-        total_trans = content_from_db["translatedSentences"] + trans_count
-        total_skip = content_from_db["skippedSentences"] + skip_count
+        if content_from_db["translatedSentences"] < 0:
+            total_trans = trans_count
+        else:
+            total_trans = content_from_db["translatedSentences"] + trans_count
+        if content_from_db["skippedSentences"] < 0:
+            total_skip = skip_count
+        else:
+            total_skip = content_from_db["skippedSentences"] + skip_count
         query = {"recordID": record_id}
         object_in = {"skippedSentences": total_skip, "translatedSentences": total_trans}
         repo.update(object_in, query)
         log_info("Batch processed, TRANSLATED: " + str(trans_count) + " | SKIPPED: " + str(skip_count) +
                  " | recordID: " + translate_wf_input["recordID"], translate_wf_input)
-        content_from_db = self.get_content_from_db(record_id, None, translate_wf_input)
-        log_info(content_from_db, translate_wf_input)
 
     # Back up method to update sentences from DB.
     def update_sentences(self, record_id, nmt_res_batch, translate_wf_input):
@@ -247,8 +258,3 @@ class TranslatorService:
         query = {"recordID": record_id}
         object_in = {"data.result": job_details["data"]["result"]}
         repo.update(object_in, query)
-
-
-
-
-
