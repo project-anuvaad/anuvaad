85fdd5d616e670cfdc1aed5ddaafff60f1663f23
kd <0xbeefdead@gmail.com>
2020-10-22 19:21:48 +0530

    updated workflow code

--- anuvaad-etl/anuvaad-extractor/content-handler/src/repositories/file_content.py
+++ anuvaad-etl/anuvaad-extractor/content-handler/src/repositories/file_content.py
@@ -138,10 +138,10 @@ class FileContentRepositories:
 
         '''
             - workflowCode: 
-            - DP_WFLOW_S_TR and DP_WFLOW_S_TTR, changes the sentence structure hence s0 pair needs to be updated
+            - WF_S_TR and WF_S_TKTR, changes the sentence structure hence s0 pair needs to be updated
             - DP_WFLOW_S_C, doesn't changes the sentence structure hence no need to update the s0 pair
         '''
-        if workflowCode is not None and (workflowCode == 'DP_WFLOW_S_TR' or workflowCode == 'DP_WFLOW_S_TTR'):
+        if workflowCode is not None and (workflowCode == 'WF_S_TR' or workflowCode == 'WF_S_TKTR'):
             update_s0 = True
 
         for block in blocks:
