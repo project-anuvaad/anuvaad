d490f93a5308adb3caca1ca568145208f6bc92bb
mahulivishal <vishalmahuli8@gmail.com>
2020-10-28 14:29:02 +0530

    added extra logs

--- anuvaad-etl/anuvaad-extractor/aligner/etl-aligner/embedder/labse.py
+++ anuvaad-etl/anuvaad-extractor/aligner/etl-aligner/embedder/labse.py
@@ -1,13 +1,6 @@
 #!/bin/python
 import logging
-import os
-from collections import OrderedDict
-from functools import partial
 
-import requests
-import multiprocessing
-from configs.alignerconfig import laser_url
-from configs.alignerconfig import no_of_processes
 from anuvaad_auditor.loghandler import log_info
 from sentence_transformers import SentenceTransformer
 
@@ -20,14 +13,14 @@ class Labse:
     def __init__(self):
         pass
 
-    def vecotrize_sentences(self, source, target, src_loc, trgt_loc, object_in):
-        log_info("Generating embedding using Labse model",object_in)
-        log_info("Vectorizing Source.......", object_in)
-        log_info("Text Locale: " + src_loc, object_in)
-        embeddings_source = model.encode(source,show_progress_bar=True)
-        log_info("Done.", object_in)
-        log_info("Vectorizing Target.......", object_in)
-        log_info("Text Locale: " + trgt_loc, object_in)
-        embeddings_target = model.encode(target,show_progress_bar=True)
-        log_info("Done.", object_in)
+    def vecotrize_sentences_labse(self, source, target, src_loc, trgt_loc, object_in):
+        log_info("Generating embedding using Labse model", object_in)
+        log_info("LaBSE: Vectorizing Source.......", object_in)
+        log_info("LaBSE: Text Locale: " + src_loc, object_in)
+        embeddings_source = model.encode(source, show_progress_bar=True)
+        log_info("LaBSE: Done.", object_in)
+        log_info("LaBSE: Vectorizing Target.......", object_in)
+        log_info("LaBSE: Text Locale: " + trgt_loc, object_in)
+        embeddings_target = model.encode(target, show_progress_bar=True)
+        log_info("LaBSE: Done.", object_in)
         return embeddings_source, embeddings_target
