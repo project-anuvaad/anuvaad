aab743bc2dc74e0b4a22dd46543a46d40ca1f065
kd <0xbeefdead@gmail.com>
2020-10-29 20:51:20 +0530

    fixed sentence order

--- anuvaad-fe/anuvaad-webapp/src/utils/page.operations.js
+++ anuvaad-fe/anuvaad-webapp/src/utils/page.operations.js
@@ -1,4 +1,30 @@
 var jp              = require('jsonpath')
+const BLOCK_OPS     = require('./block.operations.js')
+
+/**
+ * @description sort sentence of a page by position
+ * @param {*} sentences 
+ * @returns sorted sentences
+ */
+function get_page_sorted_sentences(sentences) {
+    let sorted_sentences      = sentences.sort((a, b) => {
+
+        if (a.text_top < b.text_top) {
+            return -1
+        }
+        if (a.text_top > b.text_top) {
+            return 1
+        }
+        if (a.text_left > b.text_left) {
+            return 1
+        }
+        if (a.text_left < b.text_left) {
+            return -1
+        }
+        return 0
+    })
+    return sorted_sentences;
+}
 
 /**
  * function takes 'data' key of fetch_content api response as input 
@@ -12,82 +38,51 @@ function get_pages_children_information(data) {
             'images': [],
             'texts': [],
             'lines': [],
+            'translated_texts': [],
             "page_height": element['page_height'],
             "page_no": element['page_no'],
             "page_width": element['page_width'],
         }
 
-        if ('images' in element) {
+        if (element['images']) {
             element['images'].forEach(image => {
-                let block = {}
-                block['block_identifier']    = image['block_identifier'];
-                block['base64']              = image['base64'];
-                block['text_top']            = image['text_top'];
-                block['text_left']           = image['text_left'];
-                block['text_width']          = image['text_width'];
-                block['text_height']         = image['text_height'];
-                page['images'].push(block);
+                page['images'].push(image);
             })
         }
 
-        if ('lines' in element) {
+        if (element['lines']) {
             element['lines'].forEach(line => {
-                let block = {}
-                block['block_identifier']       = line['block_identifier'];
-                page['text_top']                = line['text_top'];
-                page['text_left']               = line['text_left'];
-                page['text_width']              = line['text_width'];
-                page['text_height']             = line['text_height'];
-                page['lines'].push(block);
+                page['lines'].push(line);
             })
         }
 
-        if ('text_blocks' in element) {
-            element['text_blocks'].forEach(text => {
-                text.children.forEach(child => {
-                    let block = {}
-                    if (child.children == null) {
-                        block['block_identifier']       = text['block_identifier'];
-                        block['text']                   = child['text'];
-                        block['text_top']               = child['text_top'];
-                        block['text_left']              = child['text_left'];
-                        block['text_width']             = child['text_width'];
-                        block['text_height']            = child['text_height'];
-                        block['attrib']                 = child['attrib'];
-                        block['font_color']             = child['font_color'];
-                        block['font_family']            = child['font_family'];
-                        block['font_size']              = child['font_size'];
-                        block['avg_line_height']        = child['avg_line_height'];
-                        page['texts'].push(block);
-                    } else {
-                        child.children.forEach(child_elem => {
-                            block['block_identifier']       = text['block_identifier'];
-                            block['text']                   = child_elem['text'];
-                            block['text_top']               = child_elem['text_top'];
-                            block['text_left']              = child_elem['text_left'];
-                            block['text_width']             = child_elem['text_width'];
-                            block['text_height']            = child_elem['text_height'];
-                            block['attrib']                 = child_elem['attrib'];
-                            block['font_color']             = child_elem['font_color'];
-                            block['font_family']            = child_elem['font_family'];
-                            block['font_size']              = child_elem['font_size'];
-                            block['avg_line_height']        = child_elem['avg_line_height'];
-                            page['texts'].push(block);
+        if (element['text_blocks']) {
+            let sorted_text_blocks  = BLOCK_OPS.get_sorted_blocks(element['text_blocks'])
+
+            sorted_text_blocks.forEach(text_block => {
+                page['translated_texts'].push(...text_block['tokenized_sentences']);
+
+                text_block.children.forEach(grandchildren => {
+                    if (grandchildren['children']) {
+                        grandchildren.children.forEach(child_elem => {
+                            child_elem['block_identifier']  = text_block['block_identifier'];
+                            child_elem['tag']               = 'GRAND_CHILDREN'
+                            page['texts'].push(child_elem);
                         })
+                    } else {
+                        grandchildren['block_identifier']   = text_block['block_identifier'];
+                        grandchildren['tag']                = 'CHILDREN'
+                        page['texts'].push(grandchildren);
                     }
                 })
             })
         }
 
-        let condition                       = `$..tokenized_sentences[*]`;
-        let selected_tokenized_sentences    = jp.query(element, condition)
-        page['translated_texts']            = JSON.parse(JSON.stringify(selected_tokenized_sentences))
         pages.push(page)
     });
 
     return pages
 }
-
 
 module.exports = {
     get_pages_children_information,
