e161e9dee05204d0b9f423f1c9063eecde22de33
amritha-devadiga <71374195+amritha-devadiga@users.noreply.github.com>
2020-10-30 15:57:33 +0530

    Update build.sh

--- anuvaad-api/anuvaad-zuul-api-gw/anuvaad-zuul/build.sh
+++ anuvaad-api/anuvaad-zuul-api-gw/anuvaad-zuul/build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#mvn compile
-#mvn clean install
+mvn compile
 mvn clean install
-gradle clean build
+
+
 commit_id=${BUILD_ID}-$(git rev-parse --short HEAD)
 #commit_id=$(git rev-parse --short HEAD)
 echo $commit_id> commit_id.txt
