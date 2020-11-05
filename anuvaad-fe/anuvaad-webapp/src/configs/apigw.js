dfe3c59c3af7775d20168ae142fac6522cc71de8
sajish <sajish.sasidharan@tarento.com>
2020-11-05 15:42:53 +0530

    changed client id

--- anuvaad-fe/anuvaad-webapp/src/configs/apigw.js
+++ anuvaad-fe/anuvaad-webapp/src/configs/apigw.js
@@ -6,9 +6,9 @@ const configs = {
     RETURN_TO:'returnTo='+window.location.protocol + '//' + window.location.hostname + (window.location.port ? (':' + window.location.port) : '')+'/callback',
     RESPONSE_TYPE: 'response_type=token',
     //Test
-    CLIENT_ID: 'client_id=ef79a009-444e-4de4-90c4-52a17ec783f9',
+    // CLIENT_ID: 'client_id=ef79a009-444e-4de4-90c4-52a17ec783f9',
     //Prod
-    // CLIENT_ID: 'client_id='+process.env.REACT_APP_CLIENT_ID ,
+    CLIENT_ID: 'client_id=fc7ed37f-546a-41d3-b0f4-96e6ab8274bf',
     REDIRECT_URI: 'redirect_uri='+window.location.protocol + '//' + window.location.hostname + (window.location.port ? (':' + window.location.port) : '')+'/callback',
     
 };
