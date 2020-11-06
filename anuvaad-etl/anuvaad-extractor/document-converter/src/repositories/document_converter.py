3d539c14341ce635cb2ef935948347cf8d9cb6ad
Zer0-dev115 <amit.tiwari.min11@gmail.com>
2020-11-06 16:00:36 +0530

    Check font_size

--- anuvaad-etl/anuvaad-extractor/document-converter/src/repositories/document_converter.py
+++ anuvaad-etl/anuvaad-extractor/document-converter/src/repositories/document_converter.py
@@ -143,7 +143,7 @@ class DocumentConversion(object):
                             run.bold                   = True
                         font                           = run.font
                         font.name                      = 'Arial'
-                        if row['font_size'] != None or row['font_size'] != 'NaN':
+                        if row['font_size'] != None:
                             font.size                      = Twips(doc_utils.pixel_to_twips(row['font_size'])) 
                         run.add_text(row['text'])
                 run.add_break(WD_BREAK.PAGE)
