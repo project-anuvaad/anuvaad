9396e95d70eb1bcad8afe6057de4a4dcc3062a19
mahulivishal <vishalmahuli8@gmail.com>
2020-11-10 11:14:58 +0530

    changed interval

--- anuvaad-etl/anuvaad-cron-jobs/jobs-manager/wfm_jobsmanager/configs.py
+++ anuvaad-etl/anuvaad-cron-jobs/jobs-manager/wfm_jobsmanager/configs.py
@@ -5,5 +5,5 @@ mongo_wfm_jobs_col = os.environ.get('MONGO_WFMJOBS_COL', 'anuvaad-etl-wfm-jobs-c
 mongo_server_host = os.environ.get('MONGO_SERVER_HOST', 'mongodb://localhost:27017/')
 
 module_name = "JOBS-MANAGER"
-js_cron_interval_sec = os.environ.get('WFM_JS_CRON_INTERVAL_SEC', 1800) # 1/2 hr
-js_job_failure_interval_sec = os.environ.get('WFM_JS_CRON_FAILURE_INTERVAL_SEC', 7200) # 2 hrs
+js_cron_interval_sec = os.environ.get('WFM_JS_CRON_INTERVAL_SEC', 3600) # 1/2 hr
+js_job_failure_interval_sec = os.environ.get('WFM_JS_CRON_FAILURE_INTERVAL_SEC', 86400) # 2 hrs
