02616c2bb51aab4339e20bd54da91546d8c4df65
kd <0xbeefdead@gmail.com>
2020-11-20 18:59:19 +0530

    updating env

--- anuvaad-etl/anuvaad-extractor/content-handler/src/config.py
+++ anuvaad-etl/anuvaad-extractor/content-handler/src/config.py
@@ -11,6 +11,5 @@ ENABLE_CORS = False
 #folders and file path
 download_folder = 'upload'
 # new mongo config
-MONGO_DB_HOST   = os.environ.get('MONGO_IP', '192.168.1.9')
-MONGO_DB_PORT   = os.environ.get('MONGO_PORT', 27017)
-MONGO_DB_SCHEMA = os.environ.get('MONGO_DB_IDENTIFIER', 'preprocessing')
+MONGO_DB_SCHEMA         = os.environ.get('MONGO_CH_DB', 'preprocessing')
+MONGO_CONNECTION_URL    = os.environ.get('MONGO_CLUSTER_URL', 'mongodb://localhost:27017')
