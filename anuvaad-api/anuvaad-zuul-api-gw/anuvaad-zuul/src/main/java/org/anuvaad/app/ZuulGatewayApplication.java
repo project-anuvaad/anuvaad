720c8e5b9942685de4da0d3009fb4d7543f9d73a
mahulivishal <vishalmahuli8@gmail.com>
2020-11-23 22:21:33 +0530

    added cors config

--- anuvaad-api/anuvaad-zuul-api-gw/anuvaad-zuul/src/main/java/org/anuvaad/app/ZuulGatewayApplication.java
+++ anuvaad-api/anuvaad-zuul-api-gw/anuvaad-zuul/src/main/java/org/anuvaad/app/ZuulGatewayApplication.java
@@ -19,6 +19,8 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 @EnableZuulProxy
@@ -67,21 +69,14 @@ public class ZuulGatewayApplication {
     @Bean
     public ResponseFilter responseFilter() {return new ResponseFilter();}
 
-/*    @Bean
-    public CorsFilter corsFilter() {
-        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        final CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true);
-        config.addAllowedOrigin("*");
-        config.addAllowedHeader("*");
-        config.addAllowedMethod("OPTIONS");
-        config.addAllowedMethod("HEAD");
-        config.addAllowedMethod("GET");
-        config.addAllowedMethod("PUT");
-        config.addAllowedMethod("POST");
-        config.addAllowedMethod("DELETE");
-        config.addAllowedMethod("PATCH");
-        source.registerCorsConfiguration("/**", config);
-        return new CorsFilter(source);
-    }*/
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("*")
+                        .allowedOrigins("/**")
+                        .allowedMethods("GET", "POST", "PUT");
+            }
+        };
+    }
 }
