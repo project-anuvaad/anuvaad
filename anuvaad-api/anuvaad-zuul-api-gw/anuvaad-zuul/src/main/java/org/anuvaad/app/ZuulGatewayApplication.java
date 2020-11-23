317ffa95f1b21dd51d9898c1ece34adac7965e5d
mahulivishal <vishalmahuli8@gmail.com>
2020-11-23 16:03:25 +0530

    added cors

--- anuvaad-api/anuvaad-zuul-api-gw/anuvaad-zuul/src/main/java/org/anuvaad/app/ZuulGatewayApplication.java
+++ anuvaad-api/anuvaad-zuul-api-gw/anuvaad-zuul/src/main/java/org/anuvaad/app/ZuulGatewayApplication.java
@@ -14,6 +14,10 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 
 @EnableZuulProxy
@@ -57,5 +61,23 @@ public class ZuulGatewayApplication {
     @Bean
     public ErrorFilterFilter errorFilterFilter(){
         return new ErrorFilterFilter();
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        final CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("OPTIONS");
+        config.addAllowedMethod("HEAD");
+        config.addAllowedMethod("GET");
+        config.addAllowedMethod("PUT");
+        config.addAllowedMethod("POST");
+        config.addAllowedMethod("DELETE");
+        config.addAllowedMethod("PATCH");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter((CorsConfigurationSource) source);
     }
 }
