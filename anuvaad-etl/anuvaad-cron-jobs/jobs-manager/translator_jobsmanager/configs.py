88992656f4f498538c4e76999c5df79f0b376618
mahulivishal <vishalmahuli8@gmail.com>
2020-11-24 16:55:09 +0530

    config changes

--- anuvaad-etl/anuvaad-cron-jobs/jobs-manager/translator_jobsmanager/configs.py
+++ anuvaad-etl/anuvaad-cron-jobs/jobs-manager/translator_jobsmanager/configs.py
@@ -6,7 +6,8 @@ mongo_translator_db = os.environ.get('MONGO_ANUVAAD_TRANSLATOR_DB', 'anuvaad-etl
 mongo_translator_collection = os.environ.get('MONGO_ANUVAAD_TRANSLATOR_COL', 'anuvaad-etl-translator-content-collection')
 
 anu_translator_output_topic = os.environ.get('KAFKA_ANUVAAD_DP_TRANSLATOR_OUTPUT_TOPIC', 'anuvaad-dp-tools-translator-output-v2')
-save_content_url = os.environ.get('SAVE_CONTENT_URL', 'http://gateway_anuvaad-content-handler:5001/api/v0/save-content')
+save_content_url = str(os.environ.get('CONTENT_HANDLER_HOST', 'http://gateway_anuvaad-content-handler:5001')) \
+                   + str(os.environ.get('SAVE_CONTENT_ENDPOINT', '/anuvaad/content-handler/v0/save-content'))
 
 
 jm_cron_interval_sec = os.environ.get('TRANSLATOR_JM_INTERVAL_SEC', 30)
