885e09174b30d436939cdc4796e71aa5bc567f08
mahulivishal <vishalmahuli8@gmail.com>
2020-11-24 12:09:22 +0530

    added cors config

--- anuvaad-api/anuvaad-zuul-api-gw/anuvaad-zuul/src/main/java/org/anuvaad/app/ZuulGatewayApplication.java
+++ anuvaad-api/anuvaad-zuul-api-gw/anuvaad-zuul/src/main/java/org/anuvaad/app/ZuulGatewayApplication.java
@@ -16,11 +16,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 @EnableZuulProxy
@@ -69,14 +66,15 @@ public class ZuulGatewayApplication {
     @Bean
     public ResponseFilter responseFilter() {return new ResponseFilter();}
 
-/*    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("*")
-                        .allowedOrigins("http://localhost:3000")
-                        .allowedMethods("GET", "POST", "PUT");
-            }
-        };
-    }*/
+    @Bean
+    public CorsFilter corsFilter() {
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        final CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("*", config);
+        return new CorsFilter(source);
+    }
 }
