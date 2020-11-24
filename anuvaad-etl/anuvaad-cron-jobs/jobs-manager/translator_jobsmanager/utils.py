46a2678328a6d76ea2881ae06d26b422b0af0fe0
mahulivishal <vishalmahuli8@gmail.com>
2020-11-24 18:37:53 +0530

    logs added

--- anuvaad-etl/anuvaad-cron-jobs/jobs-manager/translator_jobsmanager/utils.py
+++ anuvaad-etl/anuvaad-cron-jobs/jobs-manager/translator_jobsmanager/utils.py
@@ -48,6 +48,7 @@ class TranslatorCronUtils:
     # Util method to make an API call and fetch the result
     def call_api(self, uri, method, api_input, params, user_id):
         try:
+            log_info("URI: " + uri, api_input)
             response = None
             if method == "POST":
                 api_headers = {'userid': user_id, 'ad-userid': user_id, 'Content-Type': 'application/json'}
@@ -57,6 +58,8 @@ class TranslatorCronUtils:
                 response = requests.get(url=uri, params=params, headers=api_headers)
             if response is not None:
                 if response.text is not None:
+                    log_info(response.text, api_input)
+                    log_info(type(response.text), api_input)
                     return json.loads(response.text)
                 else:
                     log_error("API response was None! URI: " + str(uri), api_input, None)
