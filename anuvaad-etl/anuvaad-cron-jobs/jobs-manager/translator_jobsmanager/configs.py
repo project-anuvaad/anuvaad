971e467b2af858180404ca384d7f53ccf9909e8b
mahulivishal <vishalmahuli8@gmail.com>
2020-11-25 13:50:09 +0530

    updated translator endpoint

--- anuvaad-etl/anuvaad-cron-jobs/jobs-manager/translator_jobsmanager/configs.py
+++ anuvaad-etl/anuvaad-cron-jobs/jobs-manager/translator_jobsmanager/configs.py
@@ -5,7 +5,7 @@ mongo_server_host = os.environ.get('MONGO_CLUSTER_URL', 'mongodb://localhost:270
 mongo_translator_db = os.environ.get('MONGO_ANUVAAD_TRANSLATOR_DB', 'anuvaad-etl-translator-db')
 mongo_translator_collection = os.environ.get('MONGO_ANUVAAD_TRANSLATOR_COL', 'anuvaad-etl-translator-content-collection')
 
-anu_translator_output_topic = os.environ.get('KAFKA_ANUVAAD_DP_TRANSLATOR_OUTPUT_TOPIC', 'anuvaad-dp-tools-translator-output-v2')
+anu_translator_output_topic = os.environ.get('KAFKA_ANUVAAD_DP_TRANSLATOR_OUTPUT_TOPIC', 'anuvaad-dp-tools-translator-output-v3')
 save_content_url = str(os.environ.get('CONTENT_HANDLER_HOST', 'http://gateway_anuvaad-content-handler:5001')) \
                    + str(os.environ.get('SAVE_CONTENT_ENDPOINT', '/anuvaad/content-handler/v0/save-content'))
 
