c3e37b422f2b7f04475cd9d16be7e5bc6245a909
naresh <saininaresh4435@gmail.com>
2020-11-26 13:41:39 +0530

    end point updated

--- anuvaad-etl/anuvaad-extractor/document-processor/ocr/google/config.py
+++ anuvaad-etl/anuvaad-extractor/document-processor/ocr/google/config.py
@@ -3,7 +3,7 @@ import os
 import time
 
 DEBUG = False
-API_URL_PREFIX = "/anuvaad-etl/document-processor"
+API_URL_PREFIX = "/anuvaad-etl/document-processor/ocr/google-vision"
 HOST = '0.0.0.0'
 PORT = 5001
 BASE_DIR      = 'upload'
