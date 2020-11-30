bddb7f3cd706053572fdf3a7d1cf908be95d9f5b
mahulivishal <vishalmahuli8@gmail.com>
2020-11-30 18:45:43 +0530

    updated db variables

--- anuvaad-etl/anuvaad-cron-jobs/jobs-manager/translator_jobsmanager/configs.py
+++ anuvaad-etl/anuvaad-cron-jobs/jobs-manager/translator_jobsmanager/configs.py
@@ -2,8 +2,8 @@ import os
 
 kafka_bootstrap_server_host = os.environ.get('KAFKA_BOOTSTRAP_SERVER_HOST', 'localhost:9092')
 mongo_server_host = os.environ.get('MONGO_CLUSTER_URL', 'mongodb://localhost:27017,localhost:27018/?replicaSet=foo')
-mongo_translator_db = os.environ.get('MONGO_ANUVAAD_TRANSLATOR_DB', 'anuvaad-etl-translator-db')
-mongo_translator_collection = os.environ.get('MONGO_ANUVAAD_TRANSLATOR_COL', 'anuvaad-etl-translator-content-collection')
+mongo_translator_db = os.environ.get('MONGO_TRANSLATOR_DB', 'anuvaad-etl-translator-db')
+mongo_translator_collection = os.environ.get('MONGO_TRANSLATOR_COL', 'anuvaad-etl-translator-content-collection')
 
 anu_translator_output_topic = os.environ.get('KAFKA_ANUVAAD_DP_TRANSLATOR_OUTPUT_TOPIC', 'anuvaad-dp-tools-translator-output-v3')
 save_content_url = str(os.environ.get('CONTENT_HANDLER_HOST', 'http://gateway_anuvaad-content-handler:5001')) \
