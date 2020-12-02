d2a160c7e75d0824007b51ce917ed6b8e8b19d5e
mahulivishal <vishalmahuli8@gmail.com>
2020-12-02 15:32:47 +0530

    new tmx endpoint

--- anuvaad-etl/anuvaad-translator/translator/controller/tmxcontroller.py
+++ anuvaad-etl/anuvaad-translator/translator/controller/tmxcontroller.py
@@ -11,7 +11,7 @@ def tmx_create():
     return service.push_to_tmx_store(request.get_json())
 
 
-@translatorapp.route(context_path + '/v1/tmx/bulk/create', methods=["POST"])
+@translatorapp.route(context_path + '/v1/tmx/bulk/create/xls-upload', methods=["POST"])
 def tmx_create_bulk():
     service = TEService()
     return service.push_csv_to_tmx_store(request.get_json())
