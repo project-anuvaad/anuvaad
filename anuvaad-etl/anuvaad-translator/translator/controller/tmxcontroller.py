a27e95863b2498b5c14434b38ceeb248f3a72b4c
mahulivishal <vishalmahuli8@gmail.com>
2020-12-03 17:47:07 +0530

    changed bulk update

--- anuvaad-etl/anuvaad-translator/translator/controller/tmxcontroller.py
+++ anuvaad-etl/anuvaad-translator/translator/controller/tmxcontroller.py
@@ -8,10 +8,14 @@ from configs.translatorconfig import context_path
 @translatorapp.route(context_path + '/v1/tmx/create', methods=["POST"])
 def tmx_create():
     service = TEService()
-    return service.push_to_tmx_store(request.get_json())
+    data = request.get_json()
+    data["userID"] = request.headers["x-user-id"]
+    return service.push_to_tmx_store(data)
 
 
 @translatorapp.route(context_path + '/v1/tmx/bulk/create/xls-upload', methods=["POST"])
 def tmx_create_bulk():
     service = TEService()
-    return service.push_csv_to_tmx_store(request.get_json())
+    data = request.get_json()
+    data["userID"] = request.headers["x-user-id"]
+    return service.push_csv_to_tmx_store(data)
