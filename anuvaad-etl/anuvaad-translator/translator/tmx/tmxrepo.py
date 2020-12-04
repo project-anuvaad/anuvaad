221b8cddac1c750adf7b872ce8a0d6f8ccd73082
mahulivishal <vishalmahuli8@gmail.com>
2020-12-04 14:10:32 +0530

    redis search debug

--- anuvaad-etl/anuvaad-translator/translator/tmx/tmxrepo.py
+++ anuvaad-etl/anuvaad-translator/translator/tmx/tmxrepo.py
@@ -1,6 +1,6 @@
 #!/bin/python
 import json
-from anuvaad_auditor.loghandler import log_exception
+from anuvaad_auditor.loghandler import log_exception, log_info
 from configs.translatorconfig import redis_server_host
 from configs.translatorconfig import redis_server_port
 import redis
@@ -42,15 +42,34 @@ class TMXRepository:
             return None
 
     def get_all_records(self, key_list):
+        log_info("Fetching all data......", None)
         try:
             client = self.get_redis_instance()
             if not key_list:
-                key_list = client.keys()
+                key_list = client.keys('*')
             result = []
             for key in key_list:
-                data = client.get(key)
-                if data:
-                    result.append(json.loads(data))
+                log_info(key, None)
+                key_type = client.type(key)
+                val = None
+                if key_type == "string":
+                    log_info("STRING", None)
+                    val = client.get(key)
+                if key_type == "hash":
+                    log_info("HASH", None)
+                    val = client.hgetall(key)
+                if key_type == "zset":
+                    log_info("ZSET", None)
+                    val = client.zrange(key, 0, -1)
+                if key_type == "list":
+                    log_info("LIST", None)
+                    val = client.lrange(key, 0, -1)
+                if key_type == "set":
+                    log_info("SET", None)
+                    val = client.smembers(key)
+                if val:
+                    log_info(val, None)
+                    result.append(json.loads(val))
             return result
         except Exception as e:
             log_exception("Exception in REPO: search | Cause: " + str(e), None, e)
