78db999b65d632e83cea67d06df117a6b7b98503
naresh <saininaresh4435@gmail.com>
2020-12-09 22:16:27 +0530

    json_input_format function updated with input key

--- anuvaad-etl/anuvaad-extractor/document-processor/word-detector/craft/src/utilities/utils.py
+++ anuvaad-etl/anuvaad-extractor/document-processor/word-detector/craft/src/utilities/utils.py
@@ -32,7 +32,7 @@ class FileOperation(object):
 
     # extracting data from received json input
     def json_input_format(self, json_data):
-        input_data = json_data['inputs']
+        input_data = json_data['input']['inputs']
         workflow_id = json_data['workflowCode']
         jobid = json_data['jobID']
         tool_name = json_data['tool']
