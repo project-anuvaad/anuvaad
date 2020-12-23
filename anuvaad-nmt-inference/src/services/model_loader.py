235a0fcff314947a332658776b176588ac50a7ee
aj7tesh <ajitesh1993@gmail.com>
2020-12-23 11:19:15 +0530

    updated dockerfile and requirements for gpu flow in container

--- anuvaad-nmt-inference/src/services/model_loader.py
+++ anuvaad-nmt-inference/src/services/model_loader.py
@@ -29,7 +29,7 @@ class Loadmodels:
     def return_loaded_models(self,model_paths,ids):
         loaded_models = {}
         for i,path in enumerate(model_paths):             
-            translator = ctranslate2.Translator(path)
+            translator = ctranslate2.Translator(path,device="cuda")
             loaded_models[ids[i]] = translator
             log_info("Model Loaded: {}".format(ids[i]),MODULE_CONTEXT) 
         return loaded_models 
