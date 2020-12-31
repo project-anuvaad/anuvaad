8aae52eed8cc31ba0817e538707cc5e856258a3b
sajish <sajish.sasidharan@tarento.com>
2020-12-31 15:10:05 +0530

    changing admin panel

---
+++ anuvaad-fe/anuvaad-webapp/src/configs/adminConfig.js
@@ -0,0 +1,20 @@
+const adminConfig = {
+    roles:[
+        {
+        
+            "roleCode":"TRANSLATOR",
+            "roleDesc":"Has access to translation related resources"
+        },
+        
+        {
+        
+            "roleCode":"ADMIN",
+            "roleDesc":"Has access to manage the users"
+        
+        }
+    
+    ],
+    orgID : ["ANUVAAD"]
+}
+
+export default adminConfig;
