4f543fadda7b3eb63ad63055775bf22ff9a12044
dhiraj <dhiraj.daga@tarento.com>
2021-01-04 19:32:36 +0530

    changes in table region aggregation

--- anuvaad-etl/anuvaad-extractor/document-processor/block-segmenter/src/services/region_unifier.py
+++ anuvaad-etl/anuvaad-extractor/document-processor/block-segmenter/src/services/region_unifier.py
@@ -337,9 +337,10 @@ class Region_Unifier:
             
             text_region,n_text_table_regions,tabel_region,image_region = self.get_text_tabel_region(page_regions)
 
-
+            print(len(tabel_region), 'beforeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee')
             tabel_region  = remvoe_regions(copy.deepcopy(image_region), copy.deepcopy(tabel_region))
             text_region     = remvoe_regions(copy.deepcopy(tabel_region) ,copy.deepcopy(text_region))
+            print(len(tabel_region), 'afterrrrrrrrrrrrrrrrrrrrrrrrr')
 
 
             filtered_words     = remvoe_regions(copy.deepcopy(tabel_region), copy.deepcopy(page_words))
