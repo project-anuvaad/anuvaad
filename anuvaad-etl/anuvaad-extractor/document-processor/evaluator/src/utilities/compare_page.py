581b1e5b0afb435f611b1dfc4d11f9ef30bd13dc
dhiraj <dhiraj.daga@tarento.com>
2021-01-07 15:41:43 +0530

    changes is evaluator moudule

--- anuvaad-etl/anuvaad-extractor/document-processor/evaluator/src/utilities/compare_page.py
+++ anuvaad-etl/anuvaad-extractor/document-processor/evaluator/src/utilities/compare_page.py
@@ -62,7 +62,7 @@ def compare_regions(gt_regions, predicted_regions):
 
         for line_index, line in enumerate(predicted_regions):
             if line_index not in lines_intersected:
-                output.append({'ground': None, 'input': line, 'iou': iou})
+                output.append({'ground': None, 'input': line, 'iou': 0})
 
             # else :
             #     output.append({'ground': intersecting_region, 'input': base_region, 'iou': iou})
