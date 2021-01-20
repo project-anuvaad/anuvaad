f56dae911013647d4bc7ca846d201c1b16218ee2
Roshan Shah <rosu285shah@gmail.com>
2021-01-20 20:16:00 +0530

    Sentence Highlight reducer changes

--- anuvaad-fe/anuvaad-webapp/src/flux/reducers/document_translate/fetchcontent.js
+++ anuvaad-fe/anuvaad-webapp/src/flux/reducers/document_translate/fetchcontent.js
@@ -3,31 +3,47 @@ import C from "../../actions/constants";
 const initialUserState = {
   result: []
 };
-export default function(state = initialUserState, action) {
+export default function (state = initialUserState, action) {
   switch (action.type) {
     case C.FETCH_CONTENT:
       let result = state.result;
       let status = false, i, pageD;
+      let actionPayload = Object.assign({}, action.payload)
+      if (actionPayload.data !== undefined) {
+        actionPayload.data.forEach(data => {
+          if (data.text_blocks !== undefined)
+            data.text_blocks.forEach(text_blocks => {
+              if (text_blocks.text !== undefined || text_blocks.children !== undefined) {
+                text_blocks.text = text_blocks.text.replace(/\s{2,}/g, " ").trim()
+                text_blocks.children.forEach(children => {
+                  if (children.text !== undefined)
+                    children.text = children.text.replace(/\s{2,}/g, " ").trim()
+                })
+              }
+            })
+        })
+      }
       if (result !== null && result.data) {
         action.payload.data.map(payloadData => {
           result.data.map((pageDetails, index) => {
             if (pageDetails.page_no === payloadData.page_no) {
               pageDetails = payloadData;
               status = true;
-              i=  index;
+              i = index;
               pageD = payloadData;
             }
-          return null;});
+            return null;
+          });
           if (status) {
             result.data[i] = pageD
             status = false;
           } else {
             result.data = result.data.concat(payloadData);
           }
-        return null;});
+          return null;
+        });
       } else {
-        
-        result = action.payload;
+        result = actionPayload//action.payload;
       }
       return {
         ...state,
--- anuvaad-fe/anuvaad-webapp/src/utils/SentenceHighlight.js
+++ anuvaad-fe/anuvaad-webapp/src/utils/SentenceHighlight.js
@@ -5,7 +5,7 @@ import difflab from 'difflib'
 const sentenceHighlight = (block_highlight, text, merged_block_id, renderTextSpan) => {
     if (block_highlight && block_highlight.src && (block_highlight.block_identifier === text.block_identifier || block_highlight.block_identifier === merged_block_id)) {
         let s = new difflab.SequenceMatcher(null, block_highlight.src, text.text)
-        if (s.a.toString().replace(/\s{2,}/g, ' ').trim().includes( s.b.toString().replace(/\s{2,}/g, ' ').trim())) {
+        if (s.a.toString().includes(s.b.toString())) {
             return (
                 <Textfit mode="single" style={{ width: parseInt(text.text_width), color: text.font_color }} min={1} max={text.font_size ? parseInt(text.font_size) : 16} >
                     {renderTextSpan(text, true)}
@@ -15,7 +15,7 @@ const sentenceHighlight = (block_highlight, text, merged_block_id, renderTextSpa
             let blocks = s.getMatchingBlocks();
             let position = blocks[0][2]
             let colorText = text.text.substr(0, position)
-            if (block_highlight.src.replace(/\s{2,}/g, ' ').includes(colorText.trim()) && colorText != "" && position > 1) {
+            if (block_highlight.src.includes(colorText.trim()) && colorText != "" && position > 1) {
                 let coloredText = Object.assign({}, text)
                 let nonColoredText = Object.assign({}, text)
                 coloredText.text = colorText
@@ -30,7 +30,7 @@ const sentenceHighlight = (block_highlight, text, merged_block_id, renderTextSpa
                 let blocks = s.getMatchingBlocks();
                 let position = blocks[0][1]
                 let colorText = text.text.substr(position)
-                if (block_highlight.src.replace(/\s{2,}/g, ' ').includes(colorText.trim()) && colorText != "" && position > 1) {
+                if (block_highlight.src.includes(colorText.trim()) && colorText != "" && position > 1) {
                     let coloredText = Object.assign({}, text)
                     let nonColoredText = Object.assign({}, text)
                     coloredText.text = colorText
@@ -46,7 +46,7 @@ const sentenceHighlight = (block_highlight, text, merged_block_id, renderTextSpa
                     let startPosition = blocks[0][1]
                     let endPosition = blocks[0][2]
                     let colorText = text.text.substr(startPosition, endPosition)
-                    if (block_highlight.src.replace(/\s{2,}/g, ' ').includes(colorText.trim()) && colorText != "" && startPosition > 1 && endPosition > 2) {
+                    if (block_highlight.src.includes(colorText.trim()) && colorText != "" && startPosition > 1 && endPosition > 2) {
                         let coloredText = Object.assign({}, text)
                         let firstHalf = Object.assign({}, text)
                         let secondHalf = Object.assign({}, text)
