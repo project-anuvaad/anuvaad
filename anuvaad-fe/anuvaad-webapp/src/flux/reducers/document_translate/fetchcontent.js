40f98949681d5c152d7b13cc7409a535fc8cd854
Roshan Shah <rosu285shah@gmail.com>
2021-01-21 23:27:39 +0530

    Removed spaces from block highlight

--- anuvaad-fe/anuvaad-webapp/src/flux/reducers/document_translate/fetchcontent.js
+++ anuvaad-fe/anuvaad-webapp/src/flux/reducers/document_translate/fetchcontent.js
@@ -3,27 +3,27 @@ import C from "../../actions/constants";
 const initialUserState = {
   result: []
 };
+
+function removeSpaces(actionPayload) {
+  actionPayload.data.forEach(data => {
+    if (data.text_blocks) {
+      data.text_blocks.forEach(text_blocks => {
+        text_blocks.text = text_blocks.text.replace(/\s{2,}/g, " ").trim()
+        text_blocks.children.forEach(children => {
+          children.text = children.text.replace(/\s{2,}/g, " ").trim()
+        })
+      })
+    }
+  })
+  return actionPayload
+}
 export default function (state = initialUserState, action) {
   switch (action.type) {
     case C.FETCH_CONTENT:
       let result = state.result;
       let status = false, i, pageD;
-      let actionPayload = Object.assign({}, action.payload)
-      if (actionPayload.data !== undefined) {
-        actionPayload.data.forEach(data => {
-          if (data.text_blocks !== undefined)
-            data.text_blocks.forEach(text_blocks => {
-              if (text_blocks.text !== undefined || text_blocks.children !== undefined) {
-                text_blocks.text = text_blocks.text.replace(/\s{2,}/g, " ").trim()
-                text_blocks.children.forEach(children => {
-                  children.text = children.text.replace(/\s{2,}/g, " ").trim()
-                })
-              }
-            })
-        })
-      }
       if (result !== null && result.data) {
-        actionPayload.data.map(payloadData => {
+        action.payload.data.map(payloadData => {
           result.data.map((pageDetails, index) => {
             if (pageDetails.page_no === payloadData.page_no) {
               pageDetails = payloadData;
@@ -42,12 +42,13 @@ export default function (state = initialUserState, action) {
           return null;
         });
       } else {
-        result = actionPayload//action.payload;
+        result = action.payload;
       }
       return {
         ...state,
-        result: result
+        result: removeSpaces(result)
       };
+
     case C.CLEAR_CONTENT:
       return initialUserState;
     default:
