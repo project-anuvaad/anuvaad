6a7bcf8f11a706fc27d7ca3ce0a7afbfaeee0aee
Roshan Shah <rosu285shah@gmail.com>
2021-01-24 21:44:28 +0530

    FetchContent changes

--- anuvaad-fe/anuvaad-webapp/src/flux/reducers/document_translate/fetchcontent.js
+++ anuvaad-fe/anuvaad-webapp/src/flux/reducers/document_translate/fetchcontent.js
@@ -10,11 +10,13 @@ function removeSpaces(actionPayload) {
       data.text_blocks.forEach(text_blocks => {
         text_blocks.text = text_blocks.text.replace(/\s{2,}/g, " ").trim()
         text_blocks.children.forEach(children => {
-          children.text = children.text.replace(/\s{2,}/g, " ").trim()
-          if (children.children) {
-            children.children.forEach(nestedChild => {
-              nestedChild.text = nestedChild.text.replace(/\s{2,}/g, " ").trim()
-            })
+          if (children.text) {
+            children.text = children.text.replace(/\s{2,}/g, " ").trim()
+            if (children.children) {
+              children.children.forEach(nestedChild => {
+                nestedChild.text = nestedChild.text.replace(/\s{2,}/g, " ").trim()
+              })
+            }
           }
         })
       })
