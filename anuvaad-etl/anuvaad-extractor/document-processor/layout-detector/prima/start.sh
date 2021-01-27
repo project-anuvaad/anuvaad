5220d533d5d12d3230707c99f9baf4438f1498ba
dhiraj <dhiraj.daga@tarento.com>
2021-01-27 14:05:05 +0530

    layout weight file replaced

--- anuvaad-etl/anuvaad-extractor/document-processor/layout-detector/prima/start.sh
+++ anuvaad-etl/anuvaad-extractor/document-processor/layout-detector/prima/start.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
+
+modelpath='./src/utilities/primalaynet/model_final.pth'
+url='https://anuvaad-pubnet-weights.s3.amazonaws.com/model_final.pth?AWSAccessKeyId=AKIAUAXLRTC3KS46AZTB&Signature=tkvJtYR4bYKrkyRUX1RPC%2B1Et7s%3D&Expires=1672216087'
+
+if ! [ -f $modelpath ]; then
+  curl -o $modelpath $url
+fi
+
 python app.py
