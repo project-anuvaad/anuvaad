73a8d292ffbb35b2cadfd672d424623ec1c3b18a
Roshan Shah <rosu285shah@gmail.com>
2021-01-28 13:54:23 +0530

    Report per user code refactoring

--- anuvaad-fe/anuvaad-webapp/src/flux/reducers/document_translate/fetchcontent.js
+++ anuvaad-fe/anuvaad-webapp/src/flux/reducers/document_translate/fetchcontent.js
@@ -4,6 +4,43 @@ const initialUserState = {
   result: []
 };
 
+function getTimeSpent(time_spent) {
+  let h, m, s;
+  let time_spent_ms;
+  if (time_spent) {
+    h = Math.floor(time_spent / 1000 / 60 / 60);
+    m = Math.floor((time_spent / 1000 / 60 / 60 - h) * 60);
+    s = Math.floor(((time_spent / 1000 / 60 / 60 - h) * 60 - m) * 60);
+    s < 10 ? s = `0${s}` : s = `${s}`
+    m < 10 ? m = `0${m}` : m = `${m}`
+    h < 10 ? h = `0${h}` : h = `${h}`
+    time_spent_ms = `${h}:${m}:${s}`
+  } else {
+    time_spent_ms = '-'
+  }
+  return time_spent_ms
+}
+
+function getUserContent(data) {
+  let sentence = []
+  data.data.forEach(data => {
+    data.text_blocks.forEach(token => {
+      token.tokenized_sentences.forEach(val => {
+        if (val.save) {
+          sentence.push({
+            s0_src: val.s0_src,
+            s0_tgt: val.s0_tgt,
+            tgt: val.tgt,
+            bleu_score: val.bleu_score ? Math.round(val.bleu_score * 100) / 100 : '-',
+            time_spent: getTimeSpent(val.time_spent_ms)
+          })
+        }
+      })
+    })
+  })
+  return sentence
+}
+
 function removeSpaces(actionPayload) {
   actionPayload.data.forEach(data => {
     if (data.text_blocks) {
@@ -60,6 +97,13 @@ export default function (state = initialUserState, action) {
 
     case C.CLEAR_CONTENT:
       return initialUserState;
+
+    case C.FETCH_USER_CONTENT:
+      return {
+        ...state,
+        data: getUserContent(action.payload),
+      }
+
     default:
       return state;
   }
