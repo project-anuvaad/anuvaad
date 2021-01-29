9d3d90ecb4d20e6074ed1ac80754b9dcc5f0a388
dhiraj <dhiraj.daga@tarento.com>
2021-01-29 15:13:25 +0530

    change in requirements

--- anuvaad-etl/anuvaad-extractor/block-merger/start.sh
+++ anuvaad-etl/anuvaad-extractor/block-merger/start.sh
@@ -10,11 +10,11 @@ curl -L -o /usr/share/tesseract-ocr/4.00/tessdata/Telugu.traineddata https://git
 curl -L -o /usr/share/tesseract-ocr/4.00/tessdata/Bengali.traineddata https://github.com/tesseract-ocr/tessdata_best/raw/master/script/Bengali.traineddata
 
 modelpath='./src/utilities/primalaynet/model_final.pth'
-
 #aws s3 presign s3://anuvaad-pubnet-weights/model_final.pth --expires-in 60480000
 url='https://anuvaad-pubnet-weights.s3.amazonaws.com/model_final.pth?AWSAccessKeyId=AKIAUAXLRTC3KS46AZTB&Signature=tkvJtYR4bYKrkyRUX1RPC%2B1Et7s%3D&Expires=1672216087'
 
 if ! [ -f $modelpath ]; then
+  echo Downloading PRIMA weights
   curl -o $modelpath $url
 fi
 
