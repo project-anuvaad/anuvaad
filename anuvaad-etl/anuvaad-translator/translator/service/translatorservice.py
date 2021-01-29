1722701b944e660e7dd5692eda19327d0fccdf8a
mahulivishal <vishalmahuli8@gmail.com>
2021-01-29 16:55:12 +0530

    added logs

--- anuvaad-etl/anuvaad-translator/translator/service/translatorservice.py
+++ anuvaad-etl/anuvaad-translator/translator/service/translatorservice.py
@@ -327,11 +327,8 @@ class TranslatorService:
     # Back up method to update sentences from DB.
     def update_sentences(self, record_id, nmt_res_batch, translate_wf_input):
         page_no = str(nmt_res_batch[0]["n_id"]).split("|")[2]
-        log_info("record_id: " + str(record_id), translate_wf_input)
-        log_info("page_no: " + str(page_no), translate_wf_input)
-        pages = repo.fetch_pages({"record_id": record_id})
-        log_info(pages, translate_wf_input)
-        page = repo.fetch_pages({"record_id": record_id, "page_no": page_no})[0]
+        page = repo.fetch_pages({"record_id": record_id, "page_no": page_no})
+        log_info(page, translate_wf_input)
         for nmt_res_sentence in nmt_res_batch:
             node = str(nmt_res_sentence["n_id"]).split("|")
             if nmt_res_sentence["tmx_phrases"]:
