315fc22f8420cd658374fe7f141cd036c481e64a
dhiraj <dhiraj.daga@tarento.com>
2021-02-04 17:17:49 +0530

    multiporcessing disabled

--- anuvaad-etl/anuvaad-extractor/block-merger/app.py
+++ anuvaad-etl/anuvaad-extractor/block-merger/app.py
@@ -19,34 +19,34 @@ merge_app = Flask(__name__)
 
 def start_kafka():
     try:
-        # t1 = threading.Thread(target=process_block_merger_kf, name='BM-consumer-thread')
-        # t1.start()
-        # log_info("multithread Kafka running on multithread", LOG_WITHOUT_CONTEXT)
+        t1 = threading.Thread(target=process_block_merger_kf, name='BM-consumer-thread')
+        t1.start()
+        log_info("multithread Kafka running on multithread", LOG_WITHOUT_CONTEXT)
+
+        t2 = threading.Thread(target=block_merger_request_worker, name='BM-worker-thread')
+        t2.start()
+        log_info("Starting block_merger_request_worker", LOG_WITHOUT_CONTEXT)
+
+        t3 = threading.Thread(target=block_merger_request_worker_ocr, name='BM-worker-ocr-thread')
+        t3.start()
+        log_info("Starting block_merger_request_worker_ocr", LOG_WITHOUT_CONTEXT)
+
+        # request_process = multiprocessing.Process(target=process_block_merger_kf)
+        # request_process.start()
+        # log_info("Starting block_merger_request_kf process", LOG_WITHOUT_CONTEXT)
         #
-        # t2 = threading.Thread(target=block_merger_request_worker, name='BM-worker-thread')
-        # t2.start()
-        # log_info("Starting block_merger_request_worker", LOG_WITHOUT_CONTEXT)
+        # bm_process = []
+        # for p in range(config.BM_PROCESSES):
+        #     bm_process.append(multiprocessing.Process(target=block_merger_request_worker))
+        #     bm_process[-1].start()
+        #     log_info("multiprocessing Kafka running on bm worker : {} ".format(p), LOG_WITHOUT_CONTEXT)
         #
-        # t3 = threading.Thread(target=block_merger_request_worker_ocr, name='BM-worker-ocr-thread')
-        # t3.start()
-        # log_info("Starting block_merger_request_worker_ocr", LOG_WITHOUT_CONTEXT)
-
-        request_process = multiprocessing.Process(target=process_block_merger_kf)
-        request_process.start()
-        log_info("Starting block_merger_request_kf process", LOG_WITHOUT_CONTEXT)
-
-        bm_process = []
-        for p in range(config.BM_PROCESSES):
-            bm_process.append(multiprocessing.Process(target=block_merger_request_worker))
-            bm_process[-1].start()
-            log_info("multiprocessing Kafka running on bm worker : {} ".format(p), LOG_WITHOUT_CONTEXT)
-
-        bm__ocr_process = []
-        for p in range(config.BM_OCR_PROCESSES):
-            bm__ocr_process.append(multiprocessing.Process(target=block_merger_request_worker_ocr))
-            bm__ocr_process[-1].start()
-            log_info("multiprocessing Kafka running on ocr worker : {} ".format(p), LOG_WITHOUT_CONTEXT)
-
+        # bm__ocr_process = []
+        # for p in range(config.BM_OCR_PROCESSES):
+        #     bm__ocr_process.append(multiprocessing.Process(target=block_merger_request_worker_ocr))
+        #     bm__ocr_process[-1].start()
+        #     log_info("multiprocessing Kafka running on ocr worker : {} ".format(p), LOG_WITHOUT_CONTEXT)
+        #
 
 
     except Exception as e:
