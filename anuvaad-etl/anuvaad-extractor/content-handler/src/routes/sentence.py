44d4ec54fc6a7fa2639b1f367c5ec088a78b10c4
JainyJoy <jainyjoy97@gmail.com>
2021-02-16 12:52:52 +0530

    enabling user translation data backup and retrieval

--- anuvaad-etl/anuvaad-extractor/content-handler/src/routes/sentence.py
+++ anuvaad-etl/anuvaad-extractor/content-handler/src/routes/sentence.py
@@ -1,7 +1,7 @@
 from flask import Blueprint
 from flask_restful import Api
 
-from resources import SaveSentenceResource, FetchSentenceResource, SentenceStatisticsCount
+from resources import SaveSentenceResource, FetchSentenceResource, SentenceStatisticsCount, GetSentencesResource
 
 SENTENCE_BLUEPRINT = Blueprint("sentence", __name__)
 
@@ -15,4 +15,8 @@ Api(SENTENCE_BLUEPRINT).add_resource(
 
 Api(SENTENCE_BLUEPRINT).add_resource(
     SentenceStatisticsCount, "/v0/records/search"
+)
+
+Api(SENTENCE_BLUEPRINT).add_resource(
+    GetSentencesResource, "/v0/records/user-translation-search"
 )
