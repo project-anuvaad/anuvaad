69b3099c745445432647cccfa54f76c66564b940
dhiraj <dhiraj.daga@tarento.com>
2021-02-23 00:30:50 +0530

    debug off

--- anuvaad-etl/anuvaad-extractor/block-merger/config.py
+++ anuvaad-etl/anuvaad-extractor/block-merger/config.py
@@ -125,7 +125,7 @@ LAYOUT_CONFIG_PATH = "./src/utilities/primalaynet/config.yaml"
 # LAYOUT_MODEL_PATH = '/home/dhiraj/Downloads/model_final.pth'
 # LAYOUT_CONFIG_PATH = '/home/dhiraj/Documents/anuvaad/anuvaad-etl/anuvaad-extractor/block-merger/src/utilities/primalaynet/config.yaml'
 
-HEADER_FOOTER_BY_PRIMA = False
+HEADER_FOOTER_BY_PRIMA = True
 
 LAYOUT_CLASSES = ['HEADER','FOOTER']
 
