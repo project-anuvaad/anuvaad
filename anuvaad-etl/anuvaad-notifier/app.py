b28200fe55d4970012ff0dba46c58a3467bc9857
Abhilash-tarento <abhilash.seth@tarento.com>
2021-03-04 16:54:41 +0530

    added changes related to bootstrapserver

--- anuvaad-etl/anuvaad-notifier/app.py
+++ anuvaad-etl/anuvaad-notifier/app.py
@@ -16,6 +16,10 @@ app.config['SECRET_KEY'] = NOTIFIER_SECRET_KEY
 
 socketio = SocketIO(app, async_mode='eventlet', cors_allowed_origins='*')
 # socketio.init_app(app, message_queue='kafka://localhost:9092', channel = input_topic, write_only = True)
+
+KAFKA_URL = 'kafka://' + list(str(KAFKA_URL).split(","))[0]
+print("KAFKA URL: ", KAFKA_URL)
+
 KAFKA_MESSAGE_QUEUE_LISTNER = Listner(url=KAFKA_URL, channel=KAFKA_INPUT_TOPIC, write_only=False)
 
 socketio.init_app(app, client_manager=KAFKA_MESSAGE_QUEUE_LISTNER)
