4257573195409f911e963e9e0eb2942923ccac0b
Sujit27 <sujit271290@gmail.com>
2021-03-08 18:34:18 +0530

    malayalam models 73 and 74 added

--- anuvaad-nmt-inference/src/services/document_translate.py
+++ anuvaad-nmt-inference/src/services/document_translate.py
@@ -254,7 +254,19 @@ class NMTTranslateService:
                 input_sentence_array_prepd = [sentence_processor.moses_tokenizer(sentence) for sentence in input_sentence_array_prepd]
                 translation_array, input_subwords_list, output_subwords_list, score_list = \
                 encode_translate_decode_v2(input_sentence_array_prepd,sp_encoder,translator,sp_decoder,input_subwords_list,output_subwords_list,score_list)     
-                translation_array = [sentence_processor.indic_detokenizer(translation) for translation in translation_array]                                                     
+                translation_array = [sentence_processor.indic_detokenizer(translation) for translation in translation_array]    
+            elif model_id == 73:
+                "ml-en 2nd"
+                input_sentence_array_prepd = [sentence_processor.indic_tokenizer(sentence) for sentence in input_sentence_array_prepd]
+                translation_array, input_subwords_list, output_subwords_list, score_list = \
+                encode_translate_decode_v2(input_sentence_array_prepd,sp_encoder,translator,sp_decoder,input_subwords_list,output_subwords_list,score_list)     
+                translation_array = [sentence_processor.moses_detokenizer(translation) for translation in translation_array]
+            elif model_id == 74:
+                "en-ml 3rd"
+                input_sentence_array_prepd = [sentence_processor.moses_tokenizer(sentence) for sentence in input_sentence_array_prepd]
+                translation_array, input_subwords_list, output_subwords_list, score_list = \
+                encode_translate_decode_v2(input_sentence_array_prepd,sp_encoder,translator,sp_decoder,input_subwords_list,output_subwords_list,score_list)     
+                translation_array = [sentence_processor.indic_detokenizer(translation) for translation in translation_array]                                                   
             else:
                 log_info("Unsupported model id: {} for given input".format(model_id),MODULE_CONTEXT)
                 raise Exception("Unsupported Model ID - id: {} for given input".format(model_id))      
