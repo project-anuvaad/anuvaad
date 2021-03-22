d027cc80157a21616dc68d5ada20e1b2e447c8d9
JainyJoy <jainyjoy97@gmail.com>
2021-03-22 22:05:44 +0530

    adding recordID

--- anuvaad-etl/anuvaad-extractor/ocr-content-handler/src/models/ocr_document.py
+++ anuvaad-etl/anuvaad-extractor/ocr-content-handler/src/models/ocr_document.py
@@ -42,8 +42,11 @@ class DigitalDocumentModel(object):
             
             # regions = []                                 
             for doc in docs:
-                region = doc['wordRegions'][0]
-                return region
+                if len(doc['wordRegions'])==1:
+                    region = doc['wordRegions'][0]
+                    return region
+                else:
+                    return None
 
         except Exception as e:
             # print(e,"***********")
