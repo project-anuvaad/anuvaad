cedda21e851a6d3037e08c6c4a28b0d90aee7c5e
dhiraj <dhiraj.daga@tarento.com>
2021-03-22 15:33:54 +0530

    gv-digitization topics changed

--- anuvaad-etl/anuvaad-extractor/document-processor/gv-document-digitization/config.py
+++ anuvaad-etl/anuvaad-extractor/document-processor/gv-document-digitization/config.py
@@ -16,23 +16,23 @@ ENABLE_CORS = False
 
 # kafka
 #
-# input_topic_default = 'anuvaad-dp-tools-gv-document-digitization-input-v1'
-# input_topic_identifier = 'KAFKA_ANUVAAD_DP_TOOLS_GV_DOCUMENT_DIGITIZATION_INPUT'
-# input_topic = os.environ.get(input_topic_identifier, input_topic_default)
-#
-# output_topic_default = 'anuvaad-dp-tools-gv-document-digitization-output-v1'
-# output_topic_identifier = 'KAFKA_ANUVAAD_DP_TOOLS_GV_DOCUMENT_DIGITIZATION_OUTPUT'
-# output_topic = os.environ.get(output_topic_identifier, output_topic_default)
-#
-# kf_local_server     = 'localhost:9092'
-# kafka_ip_host       = 'KAFKA_BOOTSTRAP_SERVER_HOST'
-# bootstrap_server    = os.environ.get(kafka_ip_host, kf_local_server)
-#
-# TASK_STAT           = 'GV-DOCUMENT-DIGITIZATION'
-#
-# CONSUMER_GROUP_default       = 'anuvaad-etl-gv-document-digitization-consumer-group'
-# CONSUMER_GROUP_identifier    = 'ANUVAAD_ETL_GV_DOCUMENT_DIGITIZATION_CONSUMER_GROUP_V1'
-# CONSUMER_GROUP               = os.environ.get(CONSUMER_GROUP_identifier,CONSUMER_GROUP_default)
+input_topic_default = 'anuvaad-dp-tools-gv-document-digitization-input-v10'
+input_topic_identifier = 'KAFKA_ANUVAAD_DP_TOOLS_GV_DOCUMENT_DIGITIZATION_INPUT'
+input_topic = os.environ.get(input_topic_identifier, input_topic_default)
+
+output_topic_default = 'anuvaad-dp-tools-gv-document-digitization-output-v10'
+output_topic_identifier = 'KAFKA_ANUVAAD_DP_TOOLS_GV_DOCUMENT_DIGITIZATION_OUTPUT'
+output_topic = os.environ.get(output_topic_identifier, output_topic_default)
+
+kf_local_server     = 'localhost:9092'
+kafka_ip_host       = 'KAFKA_BOOTSTRAP_SERVER_HOST'
+bootstrap_server    = os.environ.get(kafka_ip_host, kf_local_server)
+
+TASK_STAT           = 'GV-DOCUMENT-DIGITIZATION'
+
+CONSUMER_GROUP_default       = 'anuvaad-etl-gv-document-digitization-consumer-group-v10'
+CONSUMER_GROUP_identifier    = 'ANUVAAD_ETL_GV_DOCUMENT_DIGITIZATION_CONSUMER_GROUP_V10'
+CONSUMER_GROUP               = os.environ.get(CONSUMER_GROUP_identifier,CONSUMER_GROUP_default)
 
 
 # API_URL_PREFIX = "/anuvaad-etl/document-processor/ocr/google-vision"
@@ -69,23 +69,23 @@ ENABLE_CORS = False
 
 
 
-input_topic_default = 'anuvaad-dp-tools-ocr-google-vision-input-v1'
-input_topic_identifier = 'KAFKA_ANUVAAD_DP_TOOLS_OCR_GOOGLE_VISION_INPUT'
-input_topic = os.environ.get(input_topic_identifier, input_topic_default)
+# input_topic_default = 'anuvaad-dp-tools-ocr-google-vision-input-v1'
+# input_topic_identifier = 'KAFKA_ANUVAAD_DP_TOOLS_OCR_GOOGLE_VISION_INPUT'
+# input_topic = os.environ.get(input_topic_identifier, input_topic_default)
 
-output_topic_default = 'anuvaad-dp-tools-ocr-google-vision-output-v1'
-output_topic_identifier = 'KAFKA_ANUVAAD_DP_TOOLS_OCR_GOOGLE_VISION_OUTPUT'
-output_topic = os.environ.get(output_topic_identifier, output_topic_default)
+# output_topic_default = 'anuvaad-dp-tools-ocr-google-vision-output-v1'
+# output_topic_identifier = 'KAFKA_ANUVAAD_DP_TOOLS_OCR_GOOGLE_VISION_OUTPUT'
+# output_topic = os.environ.get(output_topic_identifier, output_topic_default)
 
-kf_local_server     = 'localhost:9092'
-kafka_ip_host       = 'KAFKA_BOOTSTRAP_SERVER_HOST'
-bootstrap_server    = os.environ.get(kafka_ip_host, kf_local_server)
+# kf_local_server     = 'localhost:9092'
+# kafka_ip_host       = 'KAFKA_BOOTSTRAP_SERVER_HOST'
+# bootstrap_server    = os.environ.get(kafka_ip_host, kf_local_server)
 
-TASK_STAT           = 'GOOGLE-VISION-OCR'
+# TASK_STAT           = 'GOOGLE-VISION-OCR'
 
-CONSUMER_GROUP_default       = 'anuvaad-etl-gvocr-consumer-group'
-CONSUMER_GROUP_identifier    = 'ANUVAAD_ETL_GVOCR_CONSUMER_GROUP_V1'
-CONSUMER_GROUP               = os.environ.get(CONSUMER_GROUP_identifier,CONSUMER_GROUP_default)
+# CONSUMER_GROUP_default       = 'anuvaad-etl-gvocr-consumer-group'
+# CONSUMER_GROUP_identifier    = 'ANUVAAD_ETL_GVOCR_CONSUMER_GROUP_V1'
+# CONSUMER_GROUP               = os.environ.get(CONSUMER_GROUP_identifier,CONSUMER_GROUP_default)
 #
 #
 # logging.basicConfig(
