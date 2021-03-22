303aeeaf2b7092d6f6f66c27b7df8e1fbfa10f44
JainyJoy <jainyjoy97@gmail.com>
2021-03-22 17:02:47 +0530

    document exporter code changes

--- anuvaad-etl/anuvaad-extractor/document-converter/src/routes/doc_converter_routes.py
+++ anuvaad-etl/anuvaad-extractor/document-converter/src/routes/doc_converter_routes.py
@@ -1,8 +1,8 @@
 from flask import Blueprint
 from flask_restful import Api
-from resources.doc_converter_resources import DocumentConverter
+from resources import DocumentExporterResource
 
 # end-point for independent service
 DOCUMENT_CONVERTER_BLUEPRINT = Blueprint("document_converter", __name__)
 api = Api(DOCUMENT_CONVERTER_BLUEPRINT)
-api.add_resource(DocumentConverter, "/v0/document-converter")
+api.add_resource(DocumentExporterResource, "/v0/document-converter")
