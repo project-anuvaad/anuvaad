a0088c5aec02ccc0713b55f53c1de2c84df0b71f
Roshan Shah <rosu285shah@gmail.com>
2021-03-23 15:09:37 +0530

    Is_dev flag added

--- anuvaad-fe/anuvaad-webapp/src/flux/actions/apis/view_digitized_document/fetch_digitized_document.js
+++ anuvaad-fe/anuvaad-webapp/src/flux/actions/apis/view_digitized_document/fetch_digitized_document.js
@@ -45,7 +45,7 @@ export default class BulkSearchAPI extends API {
       "limit": this.limit,
       "jobIDs": this.jobIds,
       "taskDetails": true,
-      "workflowCodes": ["WF_A_OGV","WF_A_FCWDLDBSOGV"],
+      "workflowCodes": ["WF_A_OD10GV","WF_A_FCWDLDBSOD15GV"],
       "userIDs": this.userId
     }
   }
