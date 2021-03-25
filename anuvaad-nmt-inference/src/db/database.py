262e605730fd355aae2395b519cf1176b9f90230
aj7tesh <ajitesh1993@gmail.com>
2021-03-25 20:08:54 +0530

    reverting old changes in config mongo

--- anuvaad-nmt-inference/src/db/database.py
+++ anuvaad-nmt-inference/src/db/database.py
@@ -9,7 +9,7 @@ import config
 def connectmongo():
     try:
         # dbs = connect(db = 'nmt-inference',host='localhost', port=27017)
-        dbs = connect(db = config.DB_NAME, host='172.30.0.176:27017')
+        dbs = connect(db = config.DB_NAME, host=config.MONGO_SERVER_URL)
         log_info('***********Mongo Connected*************** ', MODULE_CONTEXT)
     except Exception as e:
         log_exception("cannot connect to database: {}".format(e),MODULE_CONTEXT,e)
