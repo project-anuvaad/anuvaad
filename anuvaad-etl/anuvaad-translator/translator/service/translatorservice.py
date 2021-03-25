a715211c868dd25688da86a0f0a294ae05a975df
Vishal Mahuli <mahulivishal@Vishals-MacBook-Pro.local>
2021-03-25 17:05:16 +0530

    config driven NMT kafka flow 5

--- anuvaad-etl/anuvaad-translator/translator/service/translatorservice.py
+++ anuvaad-etl/anuvaad-translator/translator/service/translatorservice.py
@@ -277,6 +277,7 @@ class TranslatorService:
             file, skip_count, trans_count, batch_id = file[0], 0, 0, None
             translate_wf_input = file["transInput"]
             translate_wf_input["recordID"] = record_id
+            log_info(nmt_output, translate_wf_input)
             if 'status' in nmt_output.keys():
                 if nmt_output["status"]["statusCode"] != 200:
                     skip_count += batch_size
@@ -304,6 +305,8 @@ class TranslatorService:
                     try:
                         self.update_sentences(record_id, sentences_of_the_batch, translate_wf_input)
                         trans_count += len(sentences_of_the_batch)
+                        log_info("Sent: {}".format(len(sentences_of_the_batch)), translate_wf_input)
+                        log_info("TC: {}, SC: {}".format(trans_count, skip_count), translate_wf_input)
                     except Exception as e:
                         log_exception("Exception while saving translations to DB: " + str(e), translate_wf_input, e)
                         skip_count += len(sentences_of_the_batch)
