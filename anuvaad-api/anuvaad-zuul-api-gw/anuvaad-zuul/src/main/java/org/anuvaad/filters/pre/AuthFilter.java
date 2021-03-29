71a29decbbb38008cff70d589f1e8d58cb849f73
Vishal Mahuli <mahulivishal@Vishals-MacBook-Pro.local>
2021-03-29 15:58:55 +0530

    roles in header fix

--- anuvaad-api/anuvaad-zuul-api-gw/anuvaad-zuul/src/main/java/org/anuvaad/filters/pre/AuthFilter.java
+++ anuvaad-api/anuvaad-zuul-api-gw/anuvaad-zuul/src/main/java/org/anuvaad/filters/pre/AuthFilter.java
@@ -14,7 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
+import java.util.*;
 
 import static org.anuvaad.constants.RequestContextConstants.*;
 
