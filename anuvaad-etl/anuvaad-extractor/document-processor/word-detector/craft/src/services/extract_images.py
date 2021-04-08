93be0e6950d1fc9915ca4c1196d62f09360bbb6d
dhiraj <dhiraj.daga@tarento.com>
2021-04-08 19:15:32 +0530

    resize interpolation changed

--- anuvaad-etl/anuvaad-extractor/document-processor/word-detector/craft/src/services/extract_images.py
+++ anuvaad-etl/anuvaad-extractor/document-processor/word-detector/craft/src/services/extract_images.py
@@ -48,7 +48,7 @@ def resize_image(image_paths):
     '''
     Google ocr will not process an image if it has more than 75M pixels
     '''
-    max_res  = 75_000_000
+    max_res  = 74_000_000
     try:
         if image_paths is not None and len(image_paths) > 0:
             for path in image_paths:
@@ -58,8 +58,8 @@ def resize_image(image_paths):
                 
                 if img_res >= max_res:
                     log_info("Resolution of pdf too high scaling down to enable OCR" ,app_context.application_context)
-                    scaling_factor = max_res / img_res
-                    img = cv2.resize(img,None,fx= scaling_factor,fy=scaling_factor)
+                    scaling_factor = math.sqrt(max_res / img_res)
+                    img = cv2.resize(img,None,fx= scaling_factor,fy=scaling_factor,interpolation=cv2.INTER_AREA)
                     cv2.imwrite(path,img)
     except Exception as e :
         log_error('error in resizing images ' + str(e), app_context.application_context, e)
