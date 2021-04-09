36b03d5274e245c36333be072fccf765ea989e62
Roshan Shah <rosu285shah@gmail.com>
2021-04-09 18:35:57 +0530

    Role based OCR added

--- anuvaad-fe/anuvaad-webapp/src/web.routes.js
+++ anuvaad-fe/anuvaad-webapp/src/web.routes.js
@@ -153,7 +153,7 @@ class AppRoutes extends React.Component {
 
             <PrivateRoute
               path={`${process.env.PUBLIC_URL}/interactive-digitization/:jobId/:filename/:inputfileid/:og_fname`}
-              userRoles={["TRANSLATOR"]}
+              userRoles={["TRANSLATOR","ANNOTATOR"]}
               component={DigitizedDocumentEditor}
               title="Digitized File"
               authenticate={this.authenticateUser}
@@ -239,7 +239,7 @@ class AppRoutes extends React.Component {
               path={`${process.env.PUBLIC_URL}/document-digitization`}
               dontShowLoader
               title={"Document Digitization"}
-              userRoles={["TRANSLATOR"]}
+              userRoles={["TRANSLATOR","ANNOTATOR"]}
               component={ViewDocumentDigitization}
               authenticate={this.authenticateUser}
               currentMenu="document-digitization"
@@ -250,7 +250,7 @@ class AppRoutes extends React.Component {
               path={`${process.env.PUBLIC_URL}/digitize-document-upload`}
               dontShowLoader
               title={"Start Digitization"}
-              userRoles={["TRANSLATOR"]}
+              userRoles={["TRANSLATOR","ANNOTATOR"]}
               component={DigitzeDocumentUpload}
               authenticate={this.authenticateUser}
               currentMenu="digitize-document-upload"
