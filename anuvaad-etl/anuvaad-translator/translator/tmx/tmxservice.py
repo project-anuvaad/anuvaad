4f6896ab89852d5da03f6ea7fee9743511b81fc9
Vishal Mahuli <mahulivishal@Vishals-MacBook-Pro.local>
2021-04-22 13:46:03 +0530

    tmx hopping window update

--- anuvaad-etl/anuvaad-translator/translator/tmx/tmxservice.py
+++ anuvaad-etl/anuvaad-translator/translator/tmx/tmxservice.py
@@ -158,12 +158,8 @@ class TMXService:
             tmx_record["userID"] = user_id
         if org_id:
             tmx_record["orgID"] = org_id
-        try:
-            tmx_phrases, res_dict = self.tmx_phrase_search(tmx_record, tmx_level, tmx_file_cache, ctx)
-            return tmx_phrases, res_dict
-        except Exception as e:
-            log_exception("Exception while searching tmx from redis: " + str(e), ctx, e)
-            return [], {"computed": 0, "redis": 0, "cache": 0}
+        tmx_phrases, res_dict = self.tmx_phrase_search(tmx_record, tmx_level, tmx_file_cache, ctx)
+        return tmx_phrases, res_dict
 
     # Generates a 3 flavors for a sentence - title case, lowercase and uppercase.
     def fetch_diff_flavors_of_sentence(self, sentence):
@@ -187,38 +183,41 @@ class TMXService:
         sentence, tmx_phrases = tmx_record["src"], []
         hopping_pivot, sliding_pivot, i = 0, len(sentence), 1
         computed, r_count, c_count = 0, 0, 0,
-        while hopping_pivot < len(sentence):
-            phrase = sentence[hopping_pivot:sliding_pivot]
-            phrase_size = phrase.split(" ")
-            if len(phrase_size) <= tmx_word_length:
-                phrase_list = [phrase]
-                if str(phrase).endswith(".") or str(phrase).endswith(","):
-                    phrase_list.append(phrase[:-1])
-                for phrases in phrase_list:
-                    tmx_record["src"] = phrases
-                    tmx_result, fetch = self.get_tmx_with_fallback(tmx_record, tmx_level, tmx_file_cache, ctx)
-                    computed += 1
-                    if tmx_result:
-                        tmx_phrases.append(tmx_result[0])
-                        phrase_list = phrases.split(" ")
-                        hopping_pivot += (1 + len(' '.join(phrase_list)))
-                        sliding_pivot = len(sentence)
-                        i = 1
-                        if fetch is True:
-                            r_count += 1
-                        else:
-                            c_count += 1
-                        break
-                continue
-            sent_list = sentence.split(" ")
-            phrase_list = phrase.split(" ")
-            reduced_phrase = ' '.join(sent_list[0: len(sent_list) - i])
-            sliding_pivot = len(reduced_phrase)
-            i += 1
-            if hopping_pivot == sliding_pivot or (hopping_pivot - 1) == sliding_pivot:
-                hopping_pivot += (1 + len(' '.join(phrase_list)))
-                sliding_pivot = len(sentence)
-                i = 1
+        try:
+            while hopping_pivot < len(sentence):
+                phrase = sentence[hopping_pivot:sliding_pivot]
+                phrase_size = phrase.split(" ")
+                if len(phrase_size) <= tmx_word_length:
+                    suffix_phrase_list = [phrase]
+                    if phrase.endswith(".") or phrase.endswith(","):
+                        suffix_phrase_list.append(phrase[:-1])
+                    for phrases in suffix_phrase_list:
+                        tmx_record["src"] = phrases
+                        tmx_result, fetch = self.get_tmx_with_fallback(tmx_record, tmx_level, tmx_file_cache, ctx)
+                        computed += 1
+                        if tmx_result:
+                            tmx_phrases.append(tmx_result[0])
+                            phrase_list = phrases.split(" ")
+                            hopping_pivot += (1 + len(' '.join(phrase_list)))
+                            sliding_pivot = len(sentence)
+                            i = 1
+                            if fetch is True:
+                                r_count += 1
+                            else:
+                                c_count += 1
+                            break
+                    continue
+                sent_list = sentence.split(" ")
+                phrase_list = phrase.split(" ")
+                reduced_phrase = ' '.join(sent_list[0: len(sent_list) - i])
+                sliding_pivot = len(reduced_phrase)
+                i += 1
+                if hopping_pivot == sliding_pivot or (hopping_pivot - 1) == sliding_pivot:
+                    hopping_pivot += (1 + len(' '.join(phrase_list)))
+                    sliding_pivot = len(sentence)
+                    i = 1
+        except Exception as e:
+            log_exception("Exception in Hopping Window Search: {}".format(e), ctx, None)
         res_dict = {"computed": computed, "redis": r_count, "cache": c_count}
         return tmx_phrases, res_dict
 
