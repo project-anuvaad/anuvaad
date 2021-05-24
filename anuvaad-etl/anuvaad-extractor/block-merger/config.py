54d543532d564dedcec47fb033f8350decb43530
dhiraj <dhiraj.daga@tarento.com>
2021-05-24 20:10:23 +0530

    resolved the issue of text mergining with empty table cells

--- anuvaad-etl/anuvaad-extractor/block-merger/config.py
+++ anuvaad-etl/anuvaad-extractor/block-merger/config.py
@@ -119,15 +119,15 @@ WATERMARK_THRESHOLD_HIGH = 250
 
 
 PRIMA_SCORE_THRESH_TEST =0.5
-LAYOUT_MODEL_PATH =  "./src/utilities/primalaynet/model_final.pth"
-LAYOUT_CONFIG_PATH = "./src/utilities/primalaynet/config.yaml"
+# LAYOUT_MODEL_PATH =  "./src/utilities/primalaynet/model_final.pth"
+# LAYOUT_CONFIG_PATH = "./src/utilities/primalaynet/config.yaml"
 #
 #LAYOUT_MODEL_PATH =  "/home/naresh/anuvaad_BM_prima_model/model_final.pth"
 #LAYOUT_CONFIG_PATH = "/home/naresh/anuvaad-BM/anuvaad/anuvaad-etl/anuvaad-extractor/block-merger/src/utilities/primalaynet/config.yaml"
 
 
-#LAYOUT_MODEL_PATH = '/home/dhiraj/Downloads/model_final.pth'
-#LAYOUT_CONFIG_PATH = '/home/dhiraj/Documents/anuvaad/anuvaad-etl/anuvaad-extractor/block-merger/src/utilities/primalaynet/config.yaml'
+LAYOUT_MODEL_PATH = '/home/dhiraj/Downloads/model_final.pth'
+LAYOUT_CONFIG_PATH = '/home/dhiraj/Documents/anuvaad/anuvaad-etl/anuvaad-extractor/block-merger/src/utilities/primalaynet/config.yaml'
 
 HEADER_FOOTER_BY_PRIMA = True
 
