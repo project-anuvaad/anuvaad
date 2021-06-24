1255d71ddc98247cefb78d56af446651c8189ab8
aj7tesh <ajitesh1993@gmail.com>
2021-06-24 17:18:31 +0530

    Testing-3: gpustat

--- anuvaad-nmt-inference/src/resources/health.py
+++ anuvaad-nmt-inference/src/resources/health.py
@@ -5,19 +5,22 @@ from utilities import MODULE_CONTEXT
 from anuvaad_auditor.loghandler import log_info, log_exception
 import os
 import subprocess
+import GPUtil
 
 class HealthResource(Resource):
     def get(self):
         try:
             log_info("NMT Health api called",MODULE_CONTEXT)
-            pipe = subprocess.Popen(["gpustat", "--json"], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
-            res = pipe.stdout.read()
-            res = res.decode('utf-8')
-            usage_0 = res["gpus"][0]
-            usage_percent = usage_0['memory.used']/usage_0['memory.total']
+            Gpus = GPUtil.getGPUs()
+            GPUtil.showUtilization()
+            # pipe = subprocess.Popen(["gpustat", "--json"], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+            # res = pipe.stdout.read()
+            # res = res.decode('utf-8')
+            usage_0 = Gpus[0]
+            usage_percent = usage_0.memoryUsed/usage_0.memoryTotal
             log_info("GPU usage :{}".format(usage_percent),MODULE_CONTEXT)
             # pipe = os.system('gpustat --json')
-            out = CustomResponse(Status.SUCCESS.value,res)
+            out = CustomResponse(Status.SUCCESS.value,usage_percent)
             return out.jsonify_res()      
         except Exception as e:
             log_exception("GPU out of memory usage",MODULE_CONTEXT,e)  
