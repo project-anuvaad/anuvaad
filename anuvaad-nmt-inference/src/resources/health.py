01ecd9b864a2359327da4578c67fa492a9a9adb5
aj7tesh <ajitesh1993@gmail.com>
2021-06-24 16:17:20 +0530

    Testing-1: gpustat

--- anuvaad-nmt-inference/src/resources/health.py
+++ anuvaad-nmt-inference/src/resources/health.py
@@ -4,11 +4,14 @@ from models import CustomResponse, Status
 from utilities import MODULE_CONTEXT
 from anuvaad_auditor.loghandler import log_info, log_exception
 import os
+import subprocess
 
 class HealthResource(Resource):
     def get(self):
         log_info("NMT Health api called",MODULE_CONTEXT)
-        res = os.system('gpustat --json')
+        pipe = subprocess.Popen(["gpustat", "--json"], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+        res = pipe.stdout.read().decode('utf-8')
+        # pipe = os.system('gpustat --json')
         out = CustomResponse(Status.SUCCESS.value,res)
         return out.getres() 
         
