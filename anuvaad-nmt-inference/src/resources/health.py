c44b4ebad1329061ef700b3527d112ca21b471cc
aj7tesh <ajitesh1993@gmail.com>
2021-06-24 16:40:28 +0530

    Testing-2: gpustat

--- anuvaad-nmt-inference/src/resources/health.py
+++ anuvaad-nmt-inference/src/resources/health.py
@@ -8,10 +8,20 @@ import subprocess
 
 class HealthResource(Resource):
     def get(self):
-        log_info("NMT Health api called",MODULE_CONTEXT)
-        pipe = subprocess.Popen(["gpustat", "--json"], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
-        res = pipe.stdout.read().decode('utf-8')
-        # pipe = os.system('gpustat --json')
-        out = CustomResponse(Status.SUCCESS.value,res)
-        return out.getres() 
+        try:
+            log_info("NMT Health api called",MODULE_CONTEXT)
+            pipe = subprocess.Popen(["gpustat", "--json"], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+            res = pipe.stdout.read()
+            res = res.decode('utf-8')
+            usage_0 = res["gpus"][0]
+            usage_percent = usage_0['memory.used']/usage_0['memory.total']
+            log_info("GPU usage :{}".format(usage_percent),MODULE_CONTEXT)
+            # pipe = os.system('gpustat --json')
+            out = CustomResponse(Status.SUCCESS.value,res)
+            return out.jsonify_res()      
+        except Exception as e:
+            log_exception("GPU out of memory usage",MODULE_CONTEXT,e)  
+            out = CustomResponse(Status.SEVER_MODEL_ERR.value,res)
+            return out.jsonify_res()  
+        
         
