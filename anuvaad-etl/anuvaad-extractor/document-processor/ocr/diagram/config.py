660f573ebca8c939fa0b573dbed9f907513b415e
dhiraj <dhiraj.daga@tarento.com>
2021-06-25 10:48:58 +0530

    added env variables

--- anuvaad-etl/anuvaad-extractor/document-processor/ocr/diagram/config.py
+++ anuvaad-etl/anuvaad-extractor/document-processor/ocr/diagram/config.py
@@ -5,33 +5,52 @@ import time
 API_URL_PREFIX = "/anuvaad-etl/block-merger"
 HOST = '0.0.0.0'
 PORT = 5001
-BASE_DIR      = 'upload'
+BASE_DIR = 'upload'
 
 ENABLE_CORS = False
 
 # kafka
 
-input_topic_default = 'anuvaad-dp-tools-block-merger-input-v2'
-input_topic_identifier = 'KAFKA_ANUVAAD_DP_TOOLS_BLOCK_MERGER_INPUT'
+input_topic_default = 'anuvaad-dp-tools-ocr-diagram-input'
+input_topic_identifier = 'KAFKA_ANUVAAD_DP_TOOLS_OCR_DIAGRAM_INPUT'
 input_topic = os.environ.get(input_topic_identifier, input_topic_default)
 
-output_topic_default = 'anuvaad-dp-tools-block-merger-output-v2'
-output_topic_identifier = 'KAFKA_ANUVAAD_DP_TOOLS_BLOCK_MERGER_OUTPUT'
+output_topic_default = 'anuvaad-dp-tools-ocr-diagram-output'
+output_topic_identifier = 'KAFKA_ANUVAAD_DP_TOOLS_OCR_DIAGRAM__OUTPUT'
 output_topic = os.environ.get(output_topic_identifier, output_topic_default)
 
-kf_local_server     = 'localhost:9092'
-kafka_ip_host       = 'KAFKA_BOOTSTRAP_SERVER_HOST'
-bootstrap_server    = os.environ.get(kafka_ip_host, kf_local_server)
+kf_local_server = 'localhost:9092'
+kafka_ip_host = 'KAFKA_BOOTSTRAP_SERVER_HOST'
+bootstrap_server = os.environ.get(kafka_ip_host, kf_local_server)
 
-TASK_STAT           = 'BLOCK-MERGER'
+TASK_STAT = 'IMAGE-OCR'
 
-CONSUMER_GROUP_default       = 'anuvaad-etl-bm-consumer-group'
-CONSUMER_GROUP_identifire    = 'KAFKA_ANUVAAD_ETL_BM_CONSUMER_GRP'
-CONSUMER_GROUP               = os.environ.get(CONSUMER_GROUP_default,CONSUMER_GROUP_identifire)
-#folders and file path
+CONSUMER_GROUP_default = 'anuvaad-etl-ocr-diagram-consumer-group'
+CONSUMER_GROUP_identifire = 'ANUVAAD_ETL_OCR_DIAGRAM_CONSUMER_GROUP'
+CONSUMER_GROUP = os.environ.get(
+    CONSUMER_GROUP_default, CONSUMER_GROUP_identifire)
+
+
+BULK_SEARCH_default = 'https://auth.anuvaad.org/anuvaad-etl/wf-manager/v1/workflow/jobs/search/bulk'
+BULK_SEARCH_identifire = 'BLUK_SEARCH'
+BULK_SEARCH = os.environ.get(BULK_SEARCH_default, BULK_SEARCH_identifire)
+
+
+OCR_CODE_default = 'WF_A_OD10GV'
+OCR_CODE_identifire = 'OCR_CODE'
+OCR_CODE = os.environ.get(OCR_CODE_default, OCR_CODE_identifire)
+
+
+WF_INIT_default = 'https://auth.anuvaad.org/anuvaad-etl/wf-manager/v1/workflow/async/initiate'
+WF_INIT_identifire = 'WORKFLOW_INIT'
+WF_INIT = os.environ.get(WF_INIT_default, WF_INIT_identifire)
+
+
+# folders and file path
 download_folder = 'upload'
 file_upload_url = str(os.environ.get('USER_FILE_UPLOADER_HOST', 'http://gateway_anuvaad-user-fileuploader:5001')) \
-                    + str(os.environ.get('USER_FILE_UPLOAD_ENDPOINT', '/anuvaad-api/file-uploader/v0/upload-file'))
+    + str(os.environ.get('USER_FILE_UPLOAD_ENDPOINT',
+          '/anuvaad-api/file-uploader/v0/upload-file'))
 
 logging.basicConfig(
     filename=os.getenv("SERVICE_LOG", "server.log"),
@@ -42,7 +61,4 @@ logging.basicConfig(
 )
 
 
-
-
-
-auth_token=None
+auth_token = None
--- anuvaad-etl/anuvaad-extractor/document-processor/ocr/diagram/src/services/ocr_request.py
+++ anuvaad-etl/anuvaad-extractor/document-processor/ocr/diagram/src/services/ocr_request.py
@@ -1,102 +1,97 @@
 #logging.basicConfig(level=logging.INFO, filename=config.LOGS, format='%(asctime)s-%(levelname)s-%(message)s')
-import os
-import sys
-import json
+
 import config
-import logging
 import requests
 from time import sleep
-import config
 
 
 class InitRequest:
-    
-    def __init__(self,file_id,f_type,header,lang):
+
+    def __init__(self, file_id, f_type, header, lang):
         self.file_id = file_id
-        self.header = {'auth-token' :header}
-        self.lang= lang
+        self.header = {'auth-token': header}
+        self.lang = lang
         self.type = f_type
         self.ocr()
         self.download_file()
-        return self.ocr_json
+        #return self.ocr_json
 
     def ocr(self):
         self.init_request()
         self.ocr_file_id = self.bulk_serach()
 
     def init_request(self):
-        try :
+        try:
             file = {
-                    "workflowCode":config.WF_CODE,
-                    "files": [{ "path":str(self.file_id),
-                                "type": self.type,
-                                "locale":self.lang,
-                                 "config":{"OCR": {"option": "HIGH_ACCURACY","language": self.lang}
-                            }}]
-                    }
+                "workflowCode": config.OCR_CODE,
+                "files": [{"path": str(self.file_id),
+                           "type": self.type,
+                           "locale": self.lang,
+                           "config": {"OCR": {"option": "HIGH_ACCURACY", "language": self.lang}
+                                      }}]
+            }
 
             #logging.info('OCR   for {}'.format(self.file_name))
-            if self.header is not None :
-                res  = requests.post(config.WF_INIT,json=file,headers=self.header)
+            if self.header is not None:
+                res = requests.post(
+                    config.WF_INIT, json=file, headers=self.header)
             else:
-                res  = requests.post(config.WF_INIT,json=file)
-                
+                res = requests.post(config.WF_INIT, json=file)
+
             self.job_id = res.json()['jobID']
-            
+
         except Exception as e:
             #logging.error('Error in initiating WF requeest for {} due to {} '.format(self.file_name ,e),exc_info=True)
             self.job_id = None
 
-
     def bulk_serach(self):
-        if self.job_id is not None :
+        if self.job_id is not None:
             bs_request = {
-        "jobIDs": [self.job_id],
-        "taskDetails":"false"
-        }
-        #logging.info('process started for file  {} with job id {}'.format(self.file_name,self.job_id))        
-        res = requests.post(config.SEARCH,json=bs_request,headers=self.header, timeout = 10000)
-        #print(res.json()['jobs'][0]['status'])
-        retry=0
+                "jobIDs": [self.job_id],
+                "taskDetails": "false"
+            }
+        #logging.info('process started for file  {} with job id {}'.format(self.file_name,self.job_id))
+        res = requests.post(config.SEARCH, json=bs_request,
+                            headers=self.header, timeout=10000)
+        # print(res.json()['jobs'][0]['status'])
+        retry = 0
         while(1):
-            
-            try :
+
+            try:
                 r_j = res.json()
                 progress = r_j['jobs'][0]['status']
-                #print(progress)
-            except Exception as e :
+                # print(progress)
+            except Exception as e:
                 #logging.error('Error in bulk serach for {} due to {} '.format(self.file_name ,e),exc_info=True)
-                progress='p'
-                retry +=1
-                
+                progress = 'p'
+                retry += 1
+
             if retry > 10:
                 return None
-             
-            
-            if progress in ['COMPLETED','FAILED','SUCCESS']:
+
+            if progress in ['COMPLETED', 'FAILED', 'SUCCESS']:
                 if progress is 'FAILED':
                     #logging.error('Process faild for file  {} with job id {} '.format(self.file_name,self.job_id ))
                     return None
-                else :
+                else:
                     print(progress)
                     print(r_j)
                     outputfile = r_j['jobs'][0]['output'][0]['outputFile']
                     print(outputfile)
                     return outputfile
             sleep(1)
-            
-            res = requests.post(config.SEARCH,json=bs_request,headers=self.header, timeout = 10000)
-            
+
+            res = requests.post(
+                config.BULK_SEARCH, json=bs_request, headers=self.header, timeout=10000)
+
     def download_file(self):
         if self.ocr_file_id is not None:
             try:
                 download_url = config.DOWNLOAD + str(self.ocr_file_id)
-                res = requests.get(download_url,headers=self.header)
+                res = requests.get(download_url, headers=self.header)
                 self.ocr_json = res.json()
             except Exception as e:
                 #logging.error('Error in downloading file  {} \n {}  '.format(self.tok_json,e ),exc_info=True)
-                self.ocr_json =None
+                self.ocr_json = None
         else:
             self.ocr_json = None
-
-
