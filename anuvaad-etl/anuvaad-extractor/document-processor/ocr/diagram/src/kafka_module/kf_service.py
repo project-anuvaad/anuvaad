a20d22e4db3a296118a3d7acfebe66a244d2caa7
dhiraj <dhiraj.daga@tarento.com>
2021-06-25 16:17:19 +0530

    enbaled debug

--- anuvaad-etl/anuvaad-extractor/document-processor/ocr/diagram/src/kafka_module/kf_service.py
+++ anuvaad-etl/anuvaad-extractor/document-processor/ocr/diagram/src/kafka_module/kf_service.py
@@ -15,16 +15,17 @@ from kafka import TopicPartition
 import time
 import os
 
-import threading, queue
+import threading
+import queue
 
 #from multiprocessing import Queue
 
 import config
 from src.utilities.app_context import LOG_WITHOUT_CONTEXT
 
-blockMergerQueue    = queue.Queue()
+blockMergerQueue = queue.Queue()
 #blockMergerOCRQueue = queue.Queue()
-controlQueue  = queue.Queue()
+controlQueue = queue.Queue()
 controlQueue.put(1)
 
 
@@ -38,29 +39,36 @@ def consumer_validator():
         log_info("consumer_validator --- consumer running -----", None)
         return consumer
     except:
-        log_exception("consumer_validator : error in kafka opertation while listening to consumer on topic %s"%(config.input_topic), None, None)
+        log_exception("consumer_validator : error in kafka opertation while listening to consumer on topic %s" % (
+            config.input_topic), None, None)
         raise KafkaConsumerError(400, "Can not connect to consumer.")
 
-def push_output(producer, topic_name, output, jobid, task_id,data):
+
+def push_output(producer, topic_name, output, jobid, task_id, data):
     try:
         producer.push_data_to_queue(topic_name, output)
-        log_info("push_output : producer flushed value on topic %s"%(topic_name), data)
+        log_info("push_output : producer flushed value on topic %s" %
+                 (topic_name), data)
     except Exception as e:
-        response_custom = CustomResponse(Status.ERR_STATUS.value, jobid, task_id)
-        log_exception("push_output : Response can't be pushed to queue %s"%(topic_name), data, None)
-        raise KafkaProducerError(response_custom, "data Not pushed to queue: %s"%(topic_name))
+        response_custom = CustomResponse(
+            Status.ERR_STATUS.value, jobid, task_id)
+        log_exception("push_output : Response can't be pushed to queue %s" % (
+            topic_name), data, None)
+        raise KafkaProducerError(
+            response_custom, "data Not pushed to queue: %s" % (topic_name))
 
 
 # main function for async process
 def process_block_merger_kf():
-    file_ops            = FileOperation()
-    DOWNLOAD_FOLDER     = file_ops.create_file_download_dir(config.download_folder)
-    producer_tok        = Producer(config.bootstrap_server)
+    file_ops = FileOperation()
+    DOWNLOAD_FOLDER = file_ops.create_file_download_dir(config.download_folder)
+    producer_tok = Producer(config.bootstrap_server)
 
     # instatiation of consumer for respective topic
     try:
         consumer = consumer_validator()
-        log_info("process_block_merger_kf : trying to receive value from consumer ", LOG_WITHOUT_CONTEXT)
+        log_info(
+            "process_block_merger_kf : trying to receive value from consumer ", LOG_WITHOUT_CONTEXT)
 
         while True:
             wait_for_control = controlQueue.get(block=True)
@@ -68,16 +76,21 @@ def process_block_merger_kf():
             for msg in consumer:
 
                 if Consumer.get_json_data(msg.value) == None:
-                    log_info('process_block_merger_kf - received invalid data {}'.format(msg.value), None)
+                    log_info(
+                        'process_block_merger_kf - received invalid data {}'.format(msg.value), None)
                     continue
                 data = Consumer.get_json_data(msg.value)
                 consumer.commit()  # <--- This is what we need
                 # Optionally, To check if everything went good
-                print('New Kafka offset: %s' % consumer.committed(TopicPartition(config.input_topic, msg.partition)))
-                log_info('process_block_merger_kf - received message from kafka, dumping into internal queue', data)
-                
-                jobid           = data['jobID']
-                input_files, workflow_id, jobid, tool_name, step_order = file_ops.json_input_format(data)
+                print(data)
+                print('New Kafka offset: %s' % consumer.committed(
+                    TopicPartition(config.input_topic, msg.partition)))
+                log_info(
+                    'process_block_merger_kf - received message from kafka, dumping into internal queue', data)
+
+                jobid = data['jobID']
+                input_files, workflow_id, jobid, tool_name, step_order = file_ops.json_input_format(
+                    data)
 
                 blockMergerQueue.put(data)
                 break
@@ -86,47 +99,59 @@ def process_block_merger_kf():
         response_custom = {}
         response_custom['message'] = str(e)
         file_ops.error_handler(response_custom, "KAFKA_CONSUMER_ERROR", True)
-        log_exception("process_block_merger_kf : Consumer didn't instantiate", None, e)
+        log_exception(
+            "process_block_merger_kf : Consumer didn't instantiate", None, e)
     except KafkaProducerError as e:
         response_custom = {}
         response_custom['message'] = e.message
         file_ops.error_handler(response_custom, "KAFKA_PRODUCER_ERROR", True)
-        log_exception("process_block_merger_kf : response send to topic %s"%(config.output_topic), None, e)
+        log_exception("process_block_merger_kf : response send to topic %s" % (
+            config.output_topic), None, e)
+
 
 def block_merger_request_worker():
-    file_ops            = FileOperation()
-    DOWNLOAD_FOLDER     = file_ops.create_file_download_dir(config.download_folder)
-    producer_tok        = Producer(config.bootstrap_server)
-    log_info("block_merger_request_worker : starting thread ", LOG_WITHOUT_CONTEXT)
+    file_ops = FileOperation()
+    DOWNLOAD_FOLDER = file_ops.create_file_download_dir(config.download_folder)
+    producer_tok = Producer(config.bootstrap_server)
+    log_info("block_merger_request_worker : starting thread ",
+             LOG_WITHOUT_CONTEXT)
 
     while True:
-        data            = blockMergerQueue.get(block=True)
-        task_id         = str("BM-" + str(time.time()).replace('.', ''))
-        task_starttime  = str(time.time()).replace('.', '')
+        data = blockMergerQueue.get(block=True)
+        task_id = str("BM-" + str(time.time()).replace('.', ''))
+        task_starttime = str(time.time()).replace('.', '')
         if not data:
             continue
-        input_files, workflow_id, jobid, tool_name, step_order = file_ops.json_input_format(data)
+        input_files, workflow_id, jobid, tool_name, step_order = file_ops.json_input_format(
+            data)
         if not input_files:
             continue
-        
-        log_info("block_merger_request_worker processing -- received message "+str(jobid), data)
+
+        log_info(
+            "block_merger_request_worker processing -- received message "+str(jobid), data)
 
         try:
-            response_gen    = Response(data, DOWNLOAD_FOLDER)
+            response_gen = Response(data, DOWNLOAD_FOLDER)
 
-            file_value_response = response_gen.workflow_response(task_id, task_starttime, False)
+            file_value_response = response_gen.workflow_response(
+                task_id, task_starttime, False)
             if file_value_response != None:
                 if "errorID" not in file_value_response.keys():
-                    push_output(producer_tok, config.output_topic, file_value_response, jobid, task_id,data)
-                    log_info("block_merger_request_worker : response send to topic %s"%(config.output_topic), LOG_WITHOUT_CONTEXT)
+                    push_output(producer_tok, config.output_topic,
+                                file_value_response, jobid, task_id, data)
+                    log_info("block_merger_request_worker : response send to topic %s" % (
+                        config.output_topic), LOG_WITHOUT_CONTEXT)
                 else:
-                    log_info("block_merger_request_worker : error send to error handler", data)
+                    log_info(
+                        "block_merger_request_worker : error send to error handler", data)
 
-            log_info('block_merger_request_worker - request in internal queue {}'.format(blockMergerQueue.qsize()), data)
+            log_info('block_merger_request_worker - request in internal queue {}'.format(
+                blockMergerQueue.qsize()), data)
 
             blockMergerQueue.task_done()
         except Exception as e:
-            log_exception("block_merger_request_worker ",  LOG_WITHOUT_CONTEXT, e)
+            log_exception("block_merger_request_worker ",
+                          LOG_WITHOUT_CONTEXT, e)
 
         controlQueue.put(1)
 #
--- anuvaad-etl/anuvaad-extractor/document-processor/ocr/diagram/start.sh
+++ anuvaad-etl/anuvaad-extractor/document-processor/ocr/diagram/start.sh
@@ -1,22 +1,3 @@
 #!/bin/bash
-curl -L -o /usr/share/tesseract-ocr/4.00/tessdata/eng.traineddata https://github.com/tesseract-ocr/tessdata_best/raw/master/eng.traineddata
-curl -L -o /usr/share/tesseract-ocr/4.00/tessdata/hin.traineddata https://github.com/tesseract-ocr/tessdata_best/raw/master/hin.traineddata
-curl -L -o /usr/share/tesseract-ocr/4.00/tessdata/Malayalam.traineddata https://github.com/tesseract-ocr/tessdata_best/raw/master/script/Malayalam.traineddata
-curl -L -o /usr/share/tesseract-ocr/4.00/tessdata/Devanagari.traineddata https://github.com/tesseract-ocr/tessdata_best/raw/master/script/Devanagari.traineddata
-curl -L -o /usr/share/tesseract-ocr/4.00/tessdata/Latin.traineddata https://github.com/tesseract-ocr/tessdata_best/raw/master/script/Latin.traineddata
-curl -L -o /usr/share/tesseract-ocr/4.00/tessdata/Kannada.traineddata https://github.com/tesseract-ocr/tessdata_best/raw/master/script/Kannada.traineddata
-curl -L -o /usr/share/tesseract-ocr/4.00/tessdata/Tamil.traineddata https://github.com/tesseract-ocr/tessdata_best/raw/master/script/Tamil.traineddata
-curl -L -o /usr/share/tesseract-ocr/4.00/tessdata/Telugu.traineddata https://github.com/tesseract-ocr/tessdata_best/raw/master/script/Telugu.traineddata
-curl -L -o /usr/share/tesseract-ocr/4.00/tessdata/Bengali.traineddata https://github.com/tesseract-ocr/tessdata_best/raw/master/script/Bengali.traineddata
-
-modelpath='./src/utilities/primalaynet/model_final.pth'
-#aws s3 presign s3://anuvaad-pubnet-weights/model_final.pth --expires-in 60480000
-#url='https://anuvaad-pubnet-weights.s3.amazonaws.com/model_final.pth?AWSAccessKeyId=AKIAUAXLRTC3KS46AZTB&Signature=tkvJtYR4bYKrkyRUX1RPC%2B1Et7s%3D&Expires=1672216087'
-url='https://anuvaad-pubnet-weights.s3.amazonaws.com/judgement_prima_table_layout_modelv3.pth?AWSAccessKeyId=AKIAUAXLRTC3KS46AZTB&Signature=3QL3hxZ4Jn%2FBHlppqJp7jdHvT7g%3D&Expires=1677506023'
-if ! [ -f $modelpath ]; then
-  echo Downloading PRIMA weights
-  curl -o $modelpath $url
-fi
-
 
 python app.py
