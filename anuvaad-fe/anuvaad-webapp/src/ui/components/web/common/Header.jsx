5a3297f4686ad00487859035fe701c92963323c1
Roshan2810 <rosu285shah@gmail.com>
2021-06-30 17:15:50 +0530

    Scholar role removed from header and route

--- anuvaad-fe/anuvaad-webapp/src/ui/components/web/common/Header.jsx
+++ anuvaad-fe/anuvaad-webapp/src/ui/components/web/common/Header.jsx
@@ -406,8 +406,8 @@ class Header extends React.Component {
                     </div>
                   )}
 
-                  
-                  {role && Array.isArray(role) && (role.includes("TRANSLATOR") || (role.includes("ANNOTATOR")) || (role.includes("SCHOLAR"))) && (
+
+                  {role && Array.isArray(role) && (role.includes("TRANSLATOR") || (role.includes("ANNOTATOR"))) && (
                     <>
                       <div>
                         <Divider className={classes.divider} />
@@ -431,30 +431,28 @@ class Header extends React.Component {
                           />
                         </ListItem>
                       </div>
-                      {role && Array.isArray(role) && !role.includes("SCHOLAR") &&
-                        <div>
-                          <Divider className={classes.divider} />
-                          <ListItem
-                            id="document-digitization"
-                            style={{ paddingTop: "8%", paddingBottom: "8%", backgroundColor: currentMenu === "document-digitization" && themeAnuvaad.palette.primary.main }}
-                            button
-                            onClick={() => {
-                              this.handleDrawerClose(false);
-                              history.push(`${process.env.PUBLIC_URL}/document-digitization`);
-                            }}
-                          >
+                      <div>
+                        <Divider className={classes.divider} />
+                        <ListItem
+                          id="document-digitization"
+                          style={{ paddingTop: "8%", paddingBottom: "8%", backgroundColor: currentMenu === "document-digitization" && themeAnuvaad.palette.primary.main }}
+                          button
+                          onClick={() => {
+                            this.handleDrawerClose(false);
+                            history.push(`${process.env.PUBLIC_URL}/document-digitization`);
+                          }}
+                        >
 
-                            <ListItemText
-                              disableTypography
-                              primary={
-                                <Typography type="body2" style={{ color: currentMenu === "document-digitization" ? "#FFFFFF" : "#000000", marginLeft: '6%' }}>
-                                  Digitize Document
-                                </Typography>
-                              }
-                            />
-                          </ListItem>
-                        </div>
-                      }
+                          <ListItemText
+                            disableTypography
+                            primary={
+                              <Typography type="body2" style={{ color: currentMenu === "document-digitization" ? "#FFFFFF" : "#000000", marginLeft: '6%' }}>
+                                Digitize Document
+                              </Typography>
+                            }
+                          />
+                        </ListItem>
+                      </div>
                     </>)}
                   {JSON.parse(localStorage.getItem("userProfile")).orgID !== 'NONMT' && role && Array.isArray(role) && (role.includes("ANNOTATOR")) && (<div>
                     <Divider className={classes.divider} />
@@ -478,7 +476,7 @@ class Header extends React.Component {
                       />
                     </ListItem>
                   </div>)}
-                  
+
                   <div>
                     <Divider className={classes.divider} />
 
