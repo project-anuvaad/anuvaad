14bf5bca7704fb4d405d2ca5933063a918e04d8e
naresh <saininaresh4435@gmail.com>
2021-07-07 12:51:40 +0530

    anuvaad_tam weight file added to start.sh script

--- anuvaad-etl/anuvaad-extractor/document-processor/ocr/ocr-tesseract-server/start.sh
+++ anuvaad-etl/anuvaad-extractor/document-processor/ocr/ocr-tesseract-server/start.sh
@@ -1,2 +1,19 @@
 #!/bin/bash
+curl -L -o /usr/share/tesseract-ocr/4.00/tessdata/eng.traineddata https://github.com/tesseract-ocr/tessdata_best/raw/master/eng.traineddata
+curl -L -o /usr/share/tesseract-ocr/4.00/tessdata/hin.traineddata https://github.com/tesseract-ocr/tessdata_best/raw/master/hin.traineddata
+curl -L -o /usr/share/tesseract-ocr/4.00/tessdata/Malayalam.traineddata https://github.com/tesseract-ocr/tessdata_best/raw/master/script/Malayalam.traineddata
+curl -L -o /usr/share/tesseract-ocr/4.00/tessdata/Devanagari.traineddata https://github.com/tesseract-ocr/tessdata_best/raw/master/script/Devanagari.traineddata
+curl -L -o /usr/share/tesseract-ocr/4.00/tessdata/Latin.traineddata https://github.com/tesseract-ocr/tessdata_best/raw/master/script/Latin.traineddata
+curl -L -o /usr/share/tesseract-ocr/4.00/tessdata/Kannada.traineddata https://github.com/tesseract-ocr/tessdata_best/raw/master/script/Kannada.traineddata
+curl -L -o /usr/share/tesseract-ocr/4.00/tessdata/Tamil.traineddata https://github.com/tesseract-ocr/tessdata_best/raw/master/script/Tamil.traineddata
+curl -L -o /usr/share/tesseract-ocr/4.00/tessdata/Telugu.traineddata https://github.com/tesseract-ocr/tessdata_best/raw/master/script/Telugu.traineddata
+curl -L -o /usr/share/tesseract-ocr/4.00/tessdata/Bengali.traineddata https://github.com/tesseract-ocr/tessdata_best/raw/master/script/Bengali.traineddata
+
+tam_modelpath='/usr/share/tesseract-ocr/4.00/tessdata/anuvaad_tam.traineddata'
+url_tam='https://anuvaad-pubnet-weights.s3.amazonaws.com/anuvaad_tam.traineddata?AWSAccessKeyId=AKIAUAXLRTC3KS46AZTB&Signature=om9kxiP2McWH8bmk2IYn%2BfJgh9M%3D&Expires=1686122081'
+if ! [ -f $tam_modelpath ]; then
+  curl -o $tam_modelpath $url_tam
+  echo downloading weight file
+fi
+
 python app.py
