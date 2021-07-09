fb5bb11c12b7828c42aeb968f19322cc15e33537
Roshan2810 <rosu285shah@gmail.com>
2021-07-09 14:36:01 +0530

    Sync API call

--- anuvaad-ext/load.js
+++ anuvaad-ext/load.js
@@ -4,8 +4,8 @@ var textMappings = {};
 
 function markAndExtractTextElements(element) {
     let childNodes = Array.from(element.childNodes);
-    for (let i=0; i<childNodes.length; i++) {
-        if (!["SCRIPT", "STYLE", "IFRAME","NOSCRIPT"].includes(childNodes[i].tagName)) {
+    for (let i = 0; i < childNodes.length; i++) {
+        if (!["SCRIPT", "STYLE", "IFRAME", "NOSCRIPT"].includes(childNodes[i].tagName)) {
             markAndExtractTextElements(childNodes[i]);
         }
     }
@@ -33,33 +33,34 @@ function markAndExtractTextElements(element) {
 }
 
 function uuidv4() {
-    return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
-      (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+    return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, c =>
+        (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
     );
 }
 
-  
+
 document.body.childNodes.forEach(n => markAndExtractTextElements(n));
-    
-fetch("https://auth.anuvaad.org/nmt-inference/v4/translate", {
-  "headers": {
-    "accept": "*/*",
-    "accept-language": "en-US,en;q=0.9,hi;q=0.8",
-    "auth-token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyTmFtZSI6Im5pdGluLnB1cmlAdGFyZW50by5jb20iLCJwYXNzd29yZCI6ImInJDJiJDEyJHN2eWlaM1l0VWpXTjg2SlRpL2I3MU94VDc0b0ZPYmNBbG1IcFZ3eXdrMkY2ZWJQeENNQy8yJyIsImV4cCI6MTYyNTcyNDk1Nn0.UiwBvi8mu_MU1zBqGRmfT7kgrKzPxiDV9WiZc5e6KRM",
-    "content-type": "application/json",
-    "sec-ch-ua": "\" Not;A Brand\";v=\"99\", \"Google Chrome\";v=\"91\", \"Chromium\";v=\"91\"",
-    "sec-ch-ua-mobile": "?0",
-    "sec-fetch-dest": "empty",
-    "sec-fetch-mode": "cors",
-    "sec-fetch-site": "same-site"
-  },
-  "referrer": "https://developers.anuvaad.org/",
-  "referrerPolicy": "strict-origin-when-cross-origin",
-  "body": "{\"model_id\":70,\"source_language_code\":\"en\",\"target_language_code\":\"hi\",\"src_list\":"+JSON.stringify(texts.slice(0,100))+"}",
-  "method": "POST",
-  "mode": "cors"
-}).then(response => response.json()).then(data => {
-    data.data.forEach(te => {
+
+fetch("https://auth.anuvaad.org/anuvaad-etl/wf-manager/v1/workflow/sync/initiate", {
+    "headers": {
+        "accept": "*/*",
+        "accept-language": "en-US,en;q=0.9,hi;q=0.8",
+        "auth-token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyTmFtZSI6InJvc2hhbi5zaGFoQHRhcmVudG8uY29tIiwicGFzc3dvcmQiOiJiJyQyYiQxMiQ1Zzk5eVlmcTVOTjR4aHBQV0hHMGplZW5mSjBsU3ZkTEdPMzh0aGdmSWhaclBwQkhaSTdaaSciLCJleHAiOjE2MjU4OTcyMjN9.WMKwjLSz6sMY7btEQXi83gmuKJgcFM81E7O-xLkD4Mw",
+        "content-type": "application/json",
+        "sec-ch-ua": "\" Not;A Brand\";v=\"99\", \"Google Chrome\";v=\"91\", \"Chromium\";v=\"91\"",
+        "sec-ch-ua-mobile": "?0",
+        "sec-fetch-dest": "empty",
+        "sec-fetch-mode": "cors",
+        "sec-fetch-site": "same-site"
+    },
+    "referrer": "https://developers.anuvaad.org/",
+    "referrerPolicy": "strict-origin-when-cross-origin",
+    "body": "{\"model_id\":103,\"source_language_code\":\"en\",\"target_language_code\":\"hi\",\"sentences\":" + JSON.stringify(texts.slice(0, 25)) +",\"workflowCode\":\"WF_S_STR\"}",
+    "method": "POST",
+    "mode": "cors"
+}).then(async response => {
+    let data = await response.json()
+    data && data.hasOwnProperty('output') && Array.isArray(data['output']['translations']) && data.output.translations.forEach(te => {
         let sid = te.s_id;
         let elementId = textMappings[sid].element_id;
         let element = document.getElementById(elementId);
@@ -67,7 +68,6 @@ fetch("https://auth.anuvaad.org/nmt-inference/v4/translate", {
         let textNode = document.createTextNode(transText);
         let originalTextNode = element.childNodes[0];
         element.replaceChild(textNode, originalTextNode);
-
     });
 });
 
