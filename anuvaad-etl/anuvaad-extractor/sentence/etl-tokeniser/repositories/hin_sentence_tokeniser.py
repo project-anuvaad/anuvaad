ebb9c7003e0fb764c45da63948c7490c82c03602
Abhishek Suga <abhishek.suga@tarento.com>
2021-07-28 13:59:34 +0530

    changes made such that all kinds of danda(endpoints) in hindi tokenizer are handled without conflict

--- anuvaad-etl/anuvaad-extractor/sentence/etl-tokeniser/repositories/hin_sentence_tokeniser.py
+++ anuvaad-etl/anuvaad-extractor/sentence/etl-tokeniser/repositories/hin_sentence_tokeniser.py
@@ -141,7 +141,7 @@ class AnuvaadHindiTokenizer(object):
         return text
 
     def add_space_after_sentence_end(self, text):
-        sentence_ends = ['.','?','!',';',':','।', '॥']
+        sentence_ends = ['.','?','!',';',':','।', '॥', '|']
         for sentence_end in sentence_ends:
             pattern = re.compile(r'['+sentence_end+'][ ]') #remove already correct patterns
             text = pattern.sub(sentence_end, text)
@@ -151,17 +151,20 @@ class AnuvaadHindiTokenizer(object):
 
     def serialize_end(self, text):
         pattern_d = re.compile(r'(\u0965)')
-        text = pattern_d.sub(' END_||_END', text)
-        pattern = re.compile(r'(\u0964)')
-        pattern = re.compile(r'(\u007C)')
-        text = pattern.sub(' END_|_END ', text)
+        text = pattern_d.sub(' END_B', text)
+        pattern_e = re.compile(r'(\u0964)')
+        text = pattern_e.sub(' END_A ', text)
+        pattern_f = re.compile(r'(\u007C)')
+        text = pattern_f.sub(' END_C ', text)
         return text
 
     def deserialize_end(self, text):
-        pattern = re.compile(re.escape(' END_|_END'), re.IGNORECASE)
+        pattern = re.compile(re.escape(' END_A'), re.IGNORECASE)
         text = pattern.sub('।', text)
-        pattern = re.compile(re.escape(' END_||_END'), re.IGNORECASE)
+        pattern = re.compile(re.escape(' END_B'), re.IGNORECASE)
         text = pattern.sub('॥', text)
+        pattern = re.compile(re.escape(' END_C'), re.IGNORECASE)
+        text = pattern.sub('|', text)
         return text
 
     def serialize_bullet_points(self, text):
