13aa1e0b293cb8baf3c4d13ac572f574dd5d9e60
naresh <saininaresh4435@gmail.com>
2021-08-13 12:35:52 +0530

    prime line detection model is added

--- anuvaad-etl/anuvaad-extractor/document-processor/word-detector/craft/start.sh
+++ anuvaad-etl/anuvaad-extractor/document-processor/word-detector/craft/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 linemodelpath='./src/utilities/primalinenet/anuvaad_line_v1.pth'
 url_line='https://anuvaad-pubnet-weights.s3.amazonaws.com/anuvaad_line_v1.pth?AWSAccessKeyId=AKIAUAXLRTC3KS46AZTB&Signature=VBDfqxxZIb8BRKNGvsTTqTxZ7tE%3D&Expires=1689165197'
-#rm $tablenet
+rm $linemodelpath
 if ! [ -f $linemodelpath ]; then
   curl -o $linemodelpath $url_line
   echo downloading weight file
