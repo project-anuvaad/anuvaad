aa296a29cb87b046d833ff740d2618c5eae6a15b
Roshan2810 <rosu285shah@gmail.com>
2021-08-13 12:00:04 +0530

    Config changes

--- anuvaad-ext/utils/load.js
+++ anuvaad-ext/utils/load.js
@@ -2,6 +2,15 @@ require("regenerator-runtime");
 const { secretbox, randomBytes } = require("tweetnacl");
 const { encodeBase64, decodeUTF8 } = require("tweetnacl-util");
 const { v4: uuidv4 } = require("uuid");
+const { default: apiEndPoints } = require("../configs/apiendpoints");
+const {HOST_NAME} = require('../configs/apigw');
+
+const {
+  saveObjectInSyncStorage,
+  getObjectFromSyncStorage,
+} = require("../utils/chromeStorage");
+
+// const HOST_NAME = "https://auth.anuvaad.org";
 
 const encrypt = (message, secret_key) => {
   let secret_msg = decodeUTF8(message);
@@ -15,26 +24,30 @@ var elementIndex = 0;
 var texts = [];
 var textMappings = {};
 
-const saveObjectInLocalStorage = async function (obj) {
-  return new Promise((resolve, reject) => {
-    try {
-      chrome.storage.sync.set(obj, function () {
-        resolve();
-      });
-    } catch (ex) {
-      reject(ex);
+const getAuthToken = async (encryptedToken) => {
+  const endPoint = `${HOST_NAME}${apiEndPoints.get_token}`;
+  fetch(endPoint, {
+    method: "POST",
+    body: JSON.stringify({ id_token: encryptedToken }),
+    headers: { "Content-Type": "application/json" },
+  }).then(async (response) => {
+    let rsp_data = await response.json();
+    if (response.ok) {
+      saveObjectInSyncStorage({ token: rsp_data.data.token });
+    }else{
+      // await setCryptoToken()
     }
   });
 };
 
-function setCryptoToken() {
+const setCryptoToken = async () => {
   var payload = `${uuidv4()}::extn::${Date.now()}`;
   const secret_key = "85U62e26b2aJ68dae8eQc188e0c8z8J9";
-  const encryptedToken = encrypt(payload, secret_key);
-  saveObjectInLocalStorage({ "id-token": encryptedToken });
-}
+  const encryptedIdToken = encrypt(payload, secret_key);
+  await getAuthToken(encryptedIdToken);
+};
 
-function markAndExtractTextElements(element) {
+const markAndExtractTextElements = (element) => {
   let childNodes = Array.from(element.childNodes);
   for (let i = 0; i < childNodes.length; i++) {
     if (
@@ -67,134 +80,87 @@ function markAndExtractTextElements(element) {
     };
     elementIndex++;
   }
-}
+};
 
-// function uuidv4() {
-//   return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, (c) =>
-//     (
-//       c ^
-//       (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))
-//     ).toString(16)
-//   );
-// }
-
-async function getObjectFromLocalStorage(key) {
-  return new Promise((resolve, reject) => {
-    try {
-      chrome.storage.local.get(key, function (value) {
-        resolve(value[key]);
+const translateWebPage = (data) => {
+  let responseArray = [];
+  data &&
+    data.hasOwnProperty("output") &&
+    Array.isArray(data["output"]["translations"]) &&
+    data.output.translations.forEach((te) => {
+      if (te.s_id[te.s_id.length - 1] === "0") {
+        responseArray.push({
+          ...te,
+          s_id: te.s_id.replace("_SENTENCE-0", ""),
+        });
+      } else {
+        let length = responseArray.length - 1;
+        responseArray[length].tgt = responseArray[length].tgt + " " + te.tgt;
+        responseArray[length].src = responseArray[length].src + " " + te.src;
+        responseArray[length].tagged_tgt =
+          responseArray[length].tagged_tgt + " " + te.tagged_tgt;
+        responseArray[length].tagged_src =
+          responseArray[length].tagged_src + " " + te.tagged_src;
+        responseArray[length].s_id = responseArray[length].s_id.replace(
+          /[A-Z]+/g,
+          ""
+        );
+      }
+      responseArray.forEach((te) => {
+        let sid = te.s_id;
+        let elementId = textMappings[sid].element_id;
+        let element = document.getElementById(elementId);
+        let transText = te.tgt;
+        let textNode = document.createTextNode(transText);
+        let originalTextNode = element.childNodes[0];
+        element.replaceChild(textNode, originalTextNode);
       });
-    } catch (ex) {
-      reject(ex);
-    }
-  });
-}
+    });
+};
 
-async function getObjectFromSyncStorage(key) {
-  return new Promise((resolve, reject) => {
-    try {
-      chrome.storage.sync.get(key, function (value) {
-        resolve(value[key]);
-      });
-    } catch (ex) {
-      reject(ex);
-    }
-  });
-}
-
-async function makeSyncInitiateCall() {
-  setCryptoToken();
+const makeSyncInitiateCall = async () => {
   var requestBody = {
     paragraphs: texts,
     workflowCode: "WF_S_STKTR",
   };
-  var authToken = await getObjectFromSyncStorage("id-token");
-  console.log("authToken", authToken);
-  var authToken =
-    "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyTmFtZSI6InJvc2hhbi5zaGFoQHRhcmVudG8uY29tIiwicGFzc3dvcmQiOiJiJyQyYiQxMiQyMXhwNXhzd0VzeDB5SVBCRk9KUzZPZlpXV0d1bnpJMmlrQmh3LzNFQ1VCRUE5VVAyUC5NUyciLCJleHAiOjE2MjcwNDM0MjJ9.fWIJXnpDOEwcIWb3o_kHkTzG9X_Z4SyzgCpIgxmPvKU";
-  requestBody.source_language_code = await getObjectFromLocalStorage("s0_src");
-  requestBody.target_language_code = await getObjectFromLocalStorage("s0_tgt");
-  requestBody.locale = await getObjectFromLocalStorage("s0_src");
+  var authToken = await getObjectFromSyncStorage("token");
+  requestBody.source_language_code = await getObjectFromSyncStorage("s0_src");
+  requestBody.target_language_code = await getObjectFromSyncStorage("s0_tgt");
+  requestBody.locale = await getObjectFromSyncStorage("s0_src");
   requestBody.model_id = await fetchModelAPICall(
     requestBody.source_language_code,
     requestBody.target_language_code,
     authToken
   );
-  fetch(
-    "https://auth.anuvaad.org/anuvaad-etl/wf-manager/v1/workflow/sync/initiate",
-    {
-      headers: {
-        accept: "*/*",
-        "accept-language": "en-US,en;q=0.9,hi;q=0.8",
-        "auth-token": `${authToken}`,
-        "content-type": "application/json",
-        "sec-ch-ua":
-          '" Not;A Brand";v="99", "Google Chrome";v="91", "Chromium";v="91"',
-        "sec-ch-ua-mobile": "?0",
-        "sec-fetch-dest": "empty",
-        "sec-fetch-mode": "cors",
-        "sec-fetch-site": "same-site",
-      },
-      referrer: "https://developers.anuvaad.org/",
-      referrerPolicy: "strict-origin-when-cross-origin",
-      body: `${JSON.stringify(requestBody)}`,
-      method: "POST",
-      mode: "cors",
-    }
-  ).then(async (response) => {
+  const endPoint = `${HOST_NAME}${apiEndPoints.sync_initiate}`;
+  fetch(endPoint, {
+    headers: {
+      "auth-token": `${authToken}`,
+      "content-type": "application/json",
+    },
+    body: `${JSON.stringify(requestBody)}`,
+    method: "POST",
+  }).then(async (response) => {
     let data = await response.json();
-    let responseArray = [];
-    data &&
-      data.hasOwnProperty("output") &&
-      Array.isArray(data["output"]["translations"]) &&
-      data.output.translations.forEach((te) => {
-        if (te.s_id[te.s_id.length - 1] === "0") {
-          responseArray.push({
-            ...te,
-            s_id: te.s_id.replace("_SENTENCE-0", ""),
-          });
-        } else {
-          responseArray[responseArray.length - 1].tgt =
-            responseArray[responseArray.length - 1].tgt + " " + te.tgt;
-          responseArray[responseArray.length - 1].src =
-            responseArray[responseArray.length - 1].src + " " + te.src;
-          responseArray[responseArray.length - 1].tagged_tgt =
-            responseArray[responseArray.length - 1].tagged_tgt +
-            " " +
-            te.tagged_tgt;
-          responseArray[responseArray.length - 1].tagged_src =
-            responseArray[responseArray.length - 1].tagged_src +
-            " " +
-            te.tagged_src;
-          responseArray[responseArray.length - 1].s_id = responseArray[
-            responseArray.length - 1
-          ].s_id.replace(/[A-Z]+/g, "");
-        }
-        responseArray.forEach((te) => {
-          let sid = te.s_id;
-          let elementId = textMappings[sid].element_id;
-          let element = document.getElementById(elementId);
-          let transText = te.tgt;
-          let textNode = document.createTextNode(transText);
-          let originalTextNode = element.childNodes[0];
-          element.replaceChild(textNode, originalTextNode);
-        });
-      });
-  });
-}
-
-async function fetchModelAPICall(source, target, authToken) {
-  let token = await getObjectFromSyncStorage("token");
-  let fetchCall = fetch(
-    "https://auth.anuvaad.org/nmt-inference/v2/fetch-models",
-    {
-      method: "get",
-      headers: {
-        "Content-Type": "application/json",
-        "auth-token": `${authToken}`,
-      },
+    if (response.ok) {
+      translateWebPage(data);
+    } else if (response.status === 401) {
+      await setCryptoToken();
+      makeSyncInitiateCall();
     }
-  );
+  });
+};
+
+const fetchModelAPICall = async (source, target, authToken) => {
+  let token = await getObjectFromSyncStorage("id-token");
+  let endPoint = `${HOST_NAME}${apiEndPoints.fetch_models}`;
+  let fetchCall = fetch(endPoint, {
+    method: "get",
+    headers: {
+      "Content-Type": "application/json",
+      "auth-token": `${authToken}`,
+    },
+  });
   let response = await fetchCall.then();
   let rsp_data = await response.json();
   if (response.ok) {
@@ -209,17 +175,17 @@ async function fetchModelAPICall(source, target, authToken) {
       return modelInfo[0].model_id;
     }
   } else if (!response.ok && response.status === 401) {
-    console.log(token);
+    await setCryptoToken();
   }
-}
+};
 
-function Translate() {
+const Translate = () => {
   markAndExtractTextElements(document.body);
   makeSyncInitiateCall();
   localStorage.setItem(
     "anuvaad-dev-text-mappings",
     JSON.stringify(textMappings)
   );
-}
+};
 
 Translate();
