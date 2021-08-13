5324755b9b7969744ea2565f324948930f05c1b5
naresh <saininaresh4435@gmail.com>
2021-08-13 14:20:14 +0530

    prime line detection model is added

--- anuvaad-etl/anuvaad-extractor/document-processor/word-detector/craft/start.sh
+++ anuvaad-etl/anuvaad-extractor/document-processor/word-detector/craft/start.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-
-
 linemodelpath='./src/utilities/primalinenet/anuvaad_line_v1.pth'
 url_line='https://anuvaad-pubnet-weights.s3.amazonaws.com/anuvaad_line_v1.pth?AWSAccessKeyId=AKIAUAXLRTC3KS46AZTB&Signature=VBDfqxxZIb8BRKNGvsTTqTxZ7tE%3D&Expires=1689165197'
 
