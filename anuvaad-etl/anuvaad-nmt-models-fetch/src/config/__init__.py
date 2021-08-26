d21675dcca883e38ad0203b2b14dae07ca37aca8
Sujit27 <sujit271290@gmail.com>
2021-08-26 12:54:27 +0530

    redundant files and packages removed

--- anuvaad-etl/anuvaad-nmt-models-fetch/src/config/__init__.py
+++ anuvaad-etl/anuvaad-nmt-models-fetch/src/config/__init__.py
@@ -1,4 +1,4 @@
-from .kafka_topics import kafka_topic, bootstrap_server_boolean, bootstrap_server
+# from .kafka_topics import kafka_topic, bootstrap_server_boolean, bootstrap_server
 import os
 
 ## app configuration variables
@@ -13,16 +13,16 @@ ENABLE_CORS = True
 APP_BASE_PATH = "src/"
 
 ## config file 
-ICONFG_FILE = os.path.join(APP_BASE_PATH, 'config/iconf.json')
+# ICONFG_FILE = os.path.join(APP_BASE_PATH, 'config/iconf.json')
 
 ## Module name
-MODULE_NAME = "/nmt-inference"
+MODULE_NAME = "/nmt-inference-models"
 
 ## fetch model details
 FETCH_MODEL_CONFG = os.path.join(APP_BASE_PATH, 'config/fetch_models.json')
 
 ## LaBSE model path
-LABSE_PATH = os.path.join(APP_BASE_PATH, 'available_nmt_models/sbert.net_models_LaBSE')
+# LABSE_PATH = os.path.join(APP_BASE_PATH, 'available_nmt_models/sbert.net_models_LaBSE')
 
 ## DB details
 MONGO_SERVER_URL = os.environ.get('MONGO_CLUSTER_URL', 'localhost:27017')
@@ -33,4 +33,4 @@ MONGO_NMT_MODELS_COLLECTION = os.environ.get('MONGO_NMT_MODELS_COLLECTION', 'anv
 source = ['en','hi','mr','ta','te','kn','gu','pa','bn','ml','as','brx','doi','ks','kok','mai','mni','ne','or','sd','si','ur','sat','lus','njz','pnr','kha','grt','sa']
 
 ## truncation limit for sentence length
-trunc_limit = 200
+# trunc_limit = 200
--- anuvaad-etl/anuvaad-nmt-models-fetch/src/models/__init__.py
+++ anuvaad-etl/anuvaad-nmt-models-fetch/src/models/__init__.py
@@ -1,3 +1,3 @@
 from .response import CustomResponse
 from .status import Status
-from .nmt_models import CreateModel,KafkaClass 
+from .nmt_models import CreateModel
