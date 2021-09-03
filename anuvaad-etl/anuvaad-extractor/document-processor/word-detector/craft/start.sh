b2f949409589c08c4ccae36aa8fd6e460e0f87a0
naresh <saininaresh4435@gmail.com>
2021-09-03 17:29:34 +0530

    modification in kf service

--- anuvaad-etl/anuvaad-extractor/document-processor/word-detector/craft/start.sh
+++ anuvaad-etl/anuvaad-extractor/document-processor/word-detector/craft/start.sh
@@ -4,7 +4,7 @@ url_line='https://anuvaad-pubnet-weights.s3.amazonaws.com/anuvaad_line_v1.pth?AW
 
 #rm $linemodelpath
 # if ! [ -f $linemodelpath ]; then
-curl - o $linemodelpath $url_line
+curl -o $linemodelpath $url_line
 echo downloading weight file
 # fi
 
