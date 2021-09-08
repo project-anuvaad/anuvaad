f53b4528fa8c11aa40817c56d2453dc11e98aaad
Roshan2810 <rosu285shah@gmail.com>
2021-09-08 15:42:14 +0530

    Show pdf bug fix

--- anuvaad-fe/anuvaad-webapp/src/flux/reducers/jobs/v1_fetch_job_details.js
+++ anuvaad-fe/anuvaad-webapp/src/flux/reducers/jobs/v1_fetch_job_details.js
@@ -75,7 +75,8 @@ function get_document_details(input) {
             timeline['status'] = task['status'];
 
             if (task['stepOrder'] === 0 && task['status'] !== 'FAILED') {
-                document['converted_filename'] = job['input']['files'][0]['path'];
+                document["converted_filename"] = task["output"][0]["outputFile"];
+
             }
 
             if (task['stepOrder'] === 2) {
