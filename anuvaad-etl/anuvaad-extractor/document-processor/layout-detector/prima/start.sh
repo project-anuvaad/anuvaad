4b3d084a0202d293a59982a194ee93bdee90cdbd
srihari-nagaraj <srihari.nagaraj@tarento.com>
2021-10-01 14:36:41 +0530

    models url updated

--- anuvaad-etl/anuvaad-extractor/document-processor/layout-detector/prima/start.sh
+++ anuvaad-etl/anuvaad-extractor/document-processor/layout-detector/prima/start.sh
@@ -5,8 +5,8 @@ tablenet='./src/utilities/primalaynet/model_tablenet.pth'
 #url_table='https://anuvaad-pubnet-weights.s3.amazonaws.com/model_tablenet.pth?AWSAccessKeyId=AKIAUAXLRTC3KS46AZTB&Signature=X7h45H4UBx8VXwN6d1DfCWlqzNs%3D&Expires=1674458749'
 url_table='https://anuvaad-pubnet-weights.s3.amazonaws.com/model_tablenet_v2.pth?AWSAccessKeyId=AKIAXX2AMEIRJY2GNYVZ&Signature=JiYolqarXt0WPuDGv0Pb2xS3FtE%3D&Expires=1693558004'
 #aws s3 presign s3://anuvaad-pubnet-weights/model_final.pth --expires-in 60480000
-#url='https://anuvaad-pubnet-weights.s3.amazonaws.com/model_final.pth?AWSAccessKeyId=AKIAUAXLRTC3KS46AZTB&Signature=tkvJtYR4bYKrkyRUX1RPC%2B1Et7s%3D&Expires=1672216087'
-#url_judgement_prima_table_layout_model_v2='https://anuvaad-pubnet-weights.s3.amazonaws.com/judgement_prima_table_layout_model_v2.pth?AWSAccessKeyId=AKIAUAXLRTC3KS46AZTB&Signature=6NeIN8oQIAl449uvJP7Rkxi5RR8%3D&Expires=1675415301'
+#url='https://anuvaad-pubnet-weights.s3.amazonaws.com/model_final.pth?AWSAccessKeyId=AKIAXX2AMEIRJY2GNYVZ&Signature=Wo8rFhtEHKXKYpRxLpbBzt%2BI1J0%3D&Expires=1693558654'
+#url_judgement_prima_table_layout_model_v2='https://anuvaad-pubnet-weights.s3.amazonaws.com/judgement_prima_table_layout_model_v2.pth?AWSAccessKeyId=AKIAXX2AMEIRJY2GNYVZ&Signature=wVdEhQ7wOgaK3%2B80n%2Bs1guTi3is%3D&Expires=1693558789'
 url_judgement_prima_table_layout_model_v3='https://anuvaad-pubnet-weights.s3.amazonaws.com/judgement_prima_table_layout_modelv3.pth?AWSAccessKeyId=AKIAXX2AMEIRJY2GNYVZ&Signature=sHUrqNU4csPLKFx8bdm6tJ6WY4o%3D&Expires=1693557932'
 #url='https://anuvaad-pubnet-weights.s3.amazonaws.com/prima_judgement_trained_wgt.pth?AWSAccessKeyId=AKIAUAXLRTC3KS46AZTB'
 rm $tablenet
