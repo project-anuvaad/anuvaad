5883515453093212decedefdd545038c5173db17
naresh <saininaresh4435@gmail.com>
2022-01-26 10:28:54 +0530

    added total rows and total columns in table cells

--- anuvaad-etl/anuvaad-extractor/document-processor/layout-detector/prima/src/utilities/primalaynet/infer.py
+++ anuvaad-etl/anuvaad-extractor/document-processor/layout-detector/prima/src/utilities/primalaynet/infer.py
@@ -348,14 +348,14 @@ def cell_layout(regions,image):
 			bbox,tag,score = prima.prima_region(layout)
 			layouts_updated  = prima.update_box_format(bbox,tag,score)
 			#################### sort cell regions
-			# layouts_updated.sort(key=lambda x: x['boundingBox']['vertices'][0]['y'],reverse=False)
-			# sorted_layouts,col_count = sort_regions(copy.deepcopy(layouts_updated),True,0)
-			# if col_count==0:
-			# 	col_count=1
-			# row_count = int(len(sorted_layouts)/col_count)
-			# tab_layouts['rows']=row_count
-			# tab_layouts['columns']=col_count
-			tab_layouts['regions']=layouts_updated
+			layouts_updated.sort(key=lambda x: x['boundingBox']['vertices'][0]['y'],reverse=False)
+			sorted_layouts,col_count = sort_regions(layouts_updated,True,0)
+			if col_count==0:
+				col_count=1
+			row_count = int(len(sorted_layouts)/col_count)
+			tab_layouts['rows']=row_count
+			tab_layouts['columns']=col_count
+			tab_layouts['regions']=sorted_layouts
 			#table_region_process.append(tab_layouts)
 			other_regions.append(tab_layouts)
 		return other_regions
