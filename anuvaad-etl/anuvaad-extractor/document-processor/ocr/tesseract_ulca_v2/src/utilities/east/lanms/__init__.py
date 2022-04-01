60869376eca54f4da779b3b73afc8da13bc98f4d
srihari-nagaraj <srihari.nagaraj@tarento.com>
2022-04-01 08:34:18 +0530

    bugs fixed in tilt correction

--- anuvaad-etl/anuvaad-extractor/document-processor/ocr/tesseract_ulca_v2/src/utilities/east/lanms/__init__.py
+++ anuvaad-etl/anuvaad-extractor/document-processor/ocr/tesseract_ulca_v2/src/utilities/east/lanms/__init__.py
@@ -1,15 +1,15 @@
 import subprocess
 import os
 import numpy as np
-from .adaptor import merge_quadrangle_n9 as nms_impl
-# BASE_DIR = os.path.dirname(os.path.realpath(__file__))
+# from .adaptor import merge_quadrangle_n9 as nms_impl
+BASE_DIR = os.path.dirname(os.path.realpath(__file__))
 
-# if subprocess.call(['make', '-C', BASE_DIR]) != 0:  # return value
-#     raise RuntimeError('Cannot compile lanms: {}'.format(BASE_DIR))
+if subprocess.call(['make', '-C', BASE_DIR]) != 0:  # return value
+    raise RuntimeError('Cannot compile lanms: {}'.format(BASE_DIR))
 
 
 def merge_quadrangle_n9(polys, thres=0.3, precision=10000):
-    
+    from .adaptor import merge_quadrangle_n9 as nms_impl
     if len(polys) == 0:
         return np.array([], dtype='float32')
     p = polys.copy()
@@ -17,4 +17,6 @@ def merge_quadrangle_n9(polys, thres=0.3, precision=10000):
     ret = np.array(nms_impl(p, thres), dtype='float32')
     ret[:,:8] /= precision
     return ret
+
+
 
