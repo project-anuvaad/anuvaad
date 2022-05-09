682a9cf26623dc54a60b549d56e03c4da521b2a8
NandiniAV <nandini.achutha@tarento.com>
2022-05-09 14:00:24 +0530

    updated node internal urls

--- anuvaad-etl/anuvaad-docx-download-service/config/end-point-config.js
+++ anuvaad-etl/anuvaad-docx-download-service/config/end-point-config.js
@@ -1,9 +1,10 @@
-const HOSTNAME = process.env.NODE_HOSTNAME;
-// const HOSTNAME = process.env.NODE_HOSTNAME
-//   ? process.env.NODE_HOSTNAME.replace("http://", "").replace(
+// const HOSTNAME = process.env.NODE_HOSTNAME;
+const HOSTNAME = process.env.NODE_HOSTNAME.replace("http://", "")
+
+// const HOSTNAME = process.env.CONTENT_HANDLER_SERVER_URL
+//   ? process.env.CONTENT_HANDLER_SERVER_URL.replace("http://", "").replace(
 //       ":5001/",
 //       ""
 //     )
 //   : process.env.NODE_HOSTNAME;
-
   module.exports = { HOSTNAME };
