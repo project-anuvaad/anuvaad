131cd8a0c4ba5adb0a7b470a2c646b5d4d6712cd
Vishal Mahuli <vishalmahuli@TI-MAC-072.local>
2022-07-19 11:50:11 +0530

    global variale fix

--- anuvaad-etl/anuvaad-translator/translator/configs/translatorconfig.py
+++ anuvaad-etl/anuvaad-translator/translator/configs/translatorconfig.py
@@ -94,7 +94,7 @@ mongo_translator_collection = os.environ.get('MONGO_TRANSLATOR_CONTENT_COL', 'an
 mongo_trans_pages_collection = os.environ.get('MONGO_TRANSLATOR_PAGES_COL', 'anuvaad-etl-translator-pages-collection')
 mongo_trans_batch_collection = os.environ.get('MONGO_TRANSLATOR_BATCH_COL', 'anuvaad-etl-translator-batch-collection')
 mongo_tmx_collection = os.environ.get('MONGO_TMX_COL', 'anuvaad-tmx-collection')
-mongo_suggestion_box_collection = os.environ.get('MONGO_SUGGESTION_COL', 'anuvaad-tmx-suggestion-collection')
+mongo_suggestion_box_collection = os.environ.get('MONGO_SUGGESTIONS_COL', 'anuvaad-tmx-suggestion-box-collection')
 
 #module-configs
 context_path = os.environ.get('ANUVAAD_ETL_TRANSLATOR_CONTEXT_PATH', '/anuvaad-etl/translator')
