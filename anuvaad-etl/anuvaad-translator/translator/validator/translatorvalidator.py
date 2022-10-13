8781c56f190ebd9a3e8d546618d4c9775c890f07
srihari-nagaraj <srihari.nagaraj@tarento.com>
2022-10-13 12:44:17 +0530

    Suggestion glossary creation validation updated

--- anuvaad-etl/anuvaad-translator/translator/validator/translatorvalidator.py
+++ anuvaad-etl/anuvaad-translator/translator/validator/translatorvalidator.py
@@ -202,11 +202,11 @@ class TranslatorValidator:
                     return post_error("TRANSLATIONS_EMPTY", "Translations cannot be empty", None)
                 else:
                     for translation in input_req["translations"]:
-                        if 'src' not in translation.keys():
+                        if 'src' not in translation.keys() or translation.get('src')=="":
                             return post_error("SRC_NOT_FOUND", "src is mandatory for every translation", None)
-                        if 'tgt' not in translation.keys():
+                        if 'tgt' not in translation.keys() or translation.get('tgt')=="":
                             return post_error("TGT_NOT_FOUND", "tgt is mandatory for every translation", None)
-                        if 'locale' not in translation.keys():
+                        if 'locale' not in translation.keys or translation.get('locale')==""():
                             return post_error("LOCALE_NOT_FOUND", "locale is mandatory for every translation", None)
                         search_req = {"src": translation["src"], "tgt": translation["tgt"], "orgIDs": [input_req["orgID"]]}
                         search_res = tmx_service.suggestion_box_get(search_req)
