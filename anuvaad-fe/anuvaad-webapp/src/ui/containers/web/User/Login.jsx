8fc168f06eb1d43f5a8b80b1383651034ddd11ed
MrigankTarento <89840438+MrigankTarento@users.noreply.github.com>
2022-10-17 13:44:04 +0530

    login page similar to shoonya

--- anuvaad-fe/anuvaad-webapp/src/ui/containers/web/User/Login.jsx
+++ anuvaad-fe/anuvaad-webapp/src/ui/containers/web/User/Login.jsx
@@ -1,27 +1,30 @@
 import React from "react";
 import { MuiThemeProvider } from "@material-ui/core/styles";
 import { withRouter } from "react-router-dom";
-import Button from "@material-ui/core/Button";
-import FormControl from "@material-ui/core/FormControl";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
-import { withStyles, Typography } from "@material-ui/core";
-import CircularProgress from '@material-ui/core/CircularProgress';
-import Grid from '@material-ui/core/Grid';
-import Link from '@material-ui/core/Link';
-
+import {
+  withStyles,
+  Typography,
+  Hidden,
+  InputAdornment,
+  IconButton,
+} from "@material-ui/core";
+import Grid from "@material-ui/core/Grid";
+import Link from "@material-ui/core/Link";
+import Button from "../../../components/web/common/Button";
 import ThemeDefault from "../../../theme/web/theme-default";
 import LoginStyles from "../../../styles/web/LoginStyles";
 import history from "../../../../web.history";
-import TextField from '../../../components/web/common/TextField';
 import Snackbar from "../../../components/web/common/Snackbar";
-import { translate } from "../../../../assets/localisation";
-
 import LoginAPI from "../../../../flux/actions/apis/user/login";
-import profileDetails from '../../../../flux/actions/apis/user/profile_details';
-
-const TELEMETRY = require('../../../../utils/TelemetryManager')
+import profileDetails from "../../../../flux/actions/apis/user/profile_details";
+import OutlinedTextField from "../../../components/web/common/OutlinedTextField";
+import Visibility from "@material-ui/icons/Visibility";
+import VisibilityOff from "@material-ui/icons/VisibilityOff";
+import CustomCard from "../../../components/web/common/Card";
+import Anuvaanlogo from "../../../../assets/Anuvaanlogo.png";
 
 class Login extends React.Component {
   constructor(props) {
@@ -31,27 +34,28 @@ class Login extends React.Component {
       password: "",
       error: false,
       loading: false,
-      errMessage: ""
+      errMessage: "",
+      password: "",
+      showPassword: false,
     };
   }
 
   getSnapshotBeforeUpdate(prevProps, prevState) {
     // TELEMETRY.pageLoadStarted('login')
     /**
-    * getSnapshotBeforeUpdate() must return null
-    */
+     * getSnapshotBeforeUpdate() must return null
+     */
     return null;
   }
-  componentDidUpdate(prevProps, prevState, snapshot) {
-  }
+  componentDidUpdate(prevProps, prevState, snapshot) {}
 
   componentDidMount() {
     localStorage.removeItem("token");
-    window.addEventListener('keypress', (key) => {
-      if (key.code === 'Enter') {
+    window.addEventListener("keypress", (key) => {
+      if (key.code === "Enter") {
         this.processLoginButtonPressed();
       }
-    })
+    });
 
     // TELEMETRY.pageLoadCompleted('login')
   }
@@ -61,7 +65,7 @@ class Login extends React.Component {
    * captures text provided in email and password fields
    */
 
-  processInputReceived = prop => event => {
+  processInputReceived = (prop) => (event) => {
     this.setState({ [prop]: event.target.value });
   };
 
@@ -71,155 +75,258 @@ class Login extends React.Component {
    */
   processLoginButtonPressed = () => {
     const { email, password } = this.state;
-    this.setState({ error: false, loading: true })
+    this.setState({ error: false, loading: true });
     const apiObj = new LoginAPI(email, password);
     const apiReq = fetch(apiObj.apiEndPoint(), {
-      method: 'post',
+      method: "post",
       body: JSON.stringify(apiObj.getBody()),
-      headers: apiObj.getHeaders().headers
-    }).then(async response => {
-      const rsp_data = await response.json();
-      if (!response.ok) {
-        return Promise.reject(rsp_data.message);
-      } else {
-        let resData = rsp_data && rsp_data.data
-        localStorage.setItem("token", resData.token)
-        this.fetchUserProfileDetails(resData.token)
-      }
-    }).catch((error) => {
-      this.setState({ error: true, loading: false, errMessage:error })
-    });
+      headers: apiObj.getHeaders().headers,
+    })
+      .then(async (response) => {
+        const rsp_data = await response.json();
+        if (!response.ok) {
+          return Promise.reject(rsp_data.message);
+        } else {
+          let resData = rsp_data && rsp_data.data;
+          localStorage.setItem("token", resData.token);
+          this.fetchUserProfileDetails(resData.token);
+        }
+      })
+      .catch((error) => {
+        this.setState({ error: true, loading: false, errMessage: error });
+      });
   };
 
   handleRoles = (value) => {
-    let result = []
-    value.roles.map(element => {
-      result.push(element.roleCode)
-    })
+    let result = [];
+    value.roles.map((element) => {
+      result.push(element.roleCode);
+    });
     return result;
-  }
+  };
 
   fetchUserProfileDetails = (token) => {
-
     const apiObj = new profileDetails(token);
     const apiReq = fetch(apiObj.apiEndPoint(), {
-      method: 'post',
+      method: "post",
       body: JSON.stringify(apiObj.getBody()),
-      headers: apiObj.getHeaders().headers
-    }).then(async response => {
-      const rsp_data = await response.json();
-      if (!response.ok) {
-        return Promise.reject('');
-      } else {
-        let resData = rsp_data && rsp_data.data
-        var roles = this.handleRoles(resData);
-        localStorage.setItem("roles", roles)
-        localStorage.setItem("lang", "en")
-        localStorage.setItem("userProfile", JSON.stringify(resData));
-        if (roles.includes('SUPERADMIN')) {
-          // history.push(`${process.env.PUBLIC_URL}/dummy-page`);
-          history.push(`${process.env.PUBLIC_URL}/user-details`);
+      headers: apiObj.getHeaders().headers,
+    })
+      .then(async (response) => {
+        const rsp_data = await response.json();
+        if (!response.ok) {
+          return Promise.reject("");
+        } else {
+          let resData = rsp_data && rsp_data.data;
+          var roles = this.handleRoles(resData);
+          localStorage.setItem("roles", roles);
+          localStorage.setItem("lang", "en");
+          localStorage.setItem("userProfile", JSON.stringify(resData));
+          if (roles.includes("SUPERADMIN")) {
+            // history.push(`${process.env.PUBLIC_URL}/dummy-page`);
+            history.push(`${process.env.PUBLIC_URL}/user-details`);
+            // history.push(`${process.env.PUBLIC_URL}/create-user`)
+          } else if (roles.includes("ADMIN")) {
+            history.push(`${process.env.PUBLIC_URL}/user-details`);
+            // history.push(`${process.env.PUBLIC_URL}/create-user`)
+          } else if (roles.includes("TRANSLATOR")) {
+            history.push(`${process.env.PUBLIC_URL}/view-document`);
+          } else {
+            history.push(`${process.env.PUBLIC_URL}/view-document`);
+          }
           // history.push(`${process.env.PUBLIC_URL}/create-user`)
-        } else if (roles.includes('ADMIN')) {
-          history.push(`${process.env.PUBLIC_URL}/user-details`);
-          // history.push(`${process.env.PUBLIC_URL}/create-user`)
-        } else if (roles.includes('TRANSLATOR')) {
-          history.push(`${process.env.PUBLIC_URL}/view-document`);
         }
-        else {
-          history.push(`${process.env.PUBLIC_URL}/view-document`);
-        }
-        // history.push(`${process.env.PUBLIC_URL}/create-user`)
-      }
-    }).catch((error) => {
-      console.log('api failed because of server or network')
-    });
-  }
+      })
+      .catch((error) => {
+        console.log("api failed because of server or network");
+      });
+  };
 
+  renderLeftPanel = () => {
+    const { classes } = this.props;
+
+    return (
+      <Grid container>
+        <Hidden only="xs">
+          <Grid item xs={10} sm={10} md={10} lg={10} xl={10}>
+            <img
+              src={Anuvaanlogo}
+              alt="logo"
+              style={{
+                width: "85px",
+                margin: "10% 0px 0% 35px",
+                borderRadius: "50%",
+              }}
+            />{" "}
+          </Grid>{" "}
+        </Hidden>
+
+        <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+          <Typography
+            variant={"h2"}
+            className={classes.title}
+            style={{
+              margin: "10% 294px 10% 39px",
+              fontFamily: '"Rowdies", cursive,"Roboto" ,sans-serif',
+            }}
+          >
+            Anuvaad
+          </Typography>
+        </Grid>
+        <Hidden only="xs">
+          <Typography
+            variant={"body1"}
+            className={classes.body}
+            style={{ margin: "20px 0px 50px 39px" }}
+          >
+            Anuvaad is an open source platform to perform Document Translation
+            and Digitization at scale with editing capabilities for various
+            Indic languages.
+          </Typography>
+        </Hidden>
+      </Grid>
+    );
+  };
+
+  handleClickShowPassword = () => {
+    this.setState({ showPassword: !this.state.showPassword });
+  };
+
+  handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
+
+  TextFields = () => {
+    return (
+      <Grid container spacing={2} style={{ marginTop: "2px" }}>
+        <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+          <OutlinedTextField
+            fullWidth
+            name="email"
+            onChange={(event) => this.setState({ email: event.target.value })}
+            value={this.state.email}
+            placeholder="Enter your Email ID*"
+            InputProps={{
+              style: { fontSize: "1.25rem" },
+            }}
+          />
+        </Grid>
+        <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+          <OutlinedTextField
+            fullWidth
+            name="password"
+            type={this.state.showPassword ? "text" : "password"}
+            onChange={(event) =>
+              this.setState({ password: event.target.value })
+            }
+            value={this.state.password}
+            placeholder={"Enter your Password*"}
+            InputProps={{
+              style: { fontSize: "1.25rem" },
+              endAdornment: (
+                <InputAdornment position="end">
+                  <IconButton
+                    onClick={this.handleClickShowPassword}
+                    onMouseDown={this.handleMouseDownPassword}
+                  >
+                    {this.state.showPassword ? (
+                      <Visibility />
+                    ) : (
+                      <VisibilityOff />
+                    )}
+                  </IconButton>
+                </InputAdornment>
+              ),
+            }}
+          />
+        </Grid>
+      </Grid>
+    );
+  };
+
+  renderCardContent = () => {
+    const { classes } = this.props;
+
+    return (
+      <CustomCard title={"Sign in to Anuvaad"} cardContent={this.TextFields()} className={classes.headingStyle}>
+        <Grid container spacing={2} style={{ width: "100%" }}>
+          <Grid
+            item
+            xs={12}
+            sm={12}
+            md={12}
+            lg={12}
+            xl={12}
+            style={{textAlign: "end"}}
+          >
+            <Link
+              onClick={() => {
+                history.push(`${process.env.PUBLIC_URL}/forgot-password`);
+              }}
+              className={classes.forgotPassLink}
+            >
+              Forgot Password?
+            </Link>
+          </Grid>
+          <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+            <Button
+              fullWidth
+              onClick={this.processLoginButtonPressed.bind(this)}
+              label={"Login"}
+              className={classes.loginBtn}
+            />
+          </Grid>
+        </Grid>
+      </CustomCard>
+    );
+  };
 
   render() {
     const { classes } = this.props;
     return (
-      <MuiThemeProvider theme={ThemeDefault} >
-
-        <div style={{ height: window.innerHeight, overflow: 'hidden' }}>
-          <Grid container spacing={8}>
-            <Grid item xs={12} sm={4} lg={5} xl={5} style={{ paddingRight: "0px" }}>
-              <img src="Anuvaad.png" width="100%" height="81%" alt="" style={{ backgroundRepeat: 'repeat-y' }} />
-            </Grid>
-            <Grid item xs={12} sm={8} lg={7} xl={7} className={classes.signUpPaper} >
-              <Typography align='center' variant='h4' className={classes.typographyHeader} style={{ marginTop: '240px' }}>Sign In</Typography>
-
-              <FormControl align='center' fullWidth >
-                <TextField value={this.state.email} id="email" type="email-username" placeholder={translate('common.page.placeholder.emailUsername')}
-                  margin="dense" varient="outlined" style={{ width: '50%', marginBottom: '2%', backgroundColor: 'white' }} onChange={this.processInputReceived('email')}
-                />
-                <TextField value={this.state.password} id="passowrd" type="password" placeholder="Enter Password*"
-                  margin="dense" varient="outlined" style={{ width: '50%', marginBottom: '2%', backgroundColor: 'white' }} onChange={this.processInputReceived('password')}
-                />
-
-                <div className={classes.wrapper}>
-                  <Button
-                    id="signin-btn"
-                    variant="contained" aria-label="edit" style={{
-                      width: '50%', marginBottom: '2%', marginTop: '2%', borderRadius: '20px', height: '45px', textTransform: 'initial', fontWeight: '20px',
-                      backgroundColor: this.state.loading ? 'grey' : '#2C2799', color: 'white',
-                    }} onClick={this.processLoginButtonPressed.bind(this)}
-                    disabled={this.state.loading}>
-                    {this.state.loading && <CircularProgress size={24} className={'success'} className={classes.buttonProgress} />}
-                    Sign In
-                </Button>
-                </div>
-
-              </FormControl>
-
-              <Typography>
-                <Link id="forgotpassword" style={{ cursor: 'pointer', color: '#2C2799', marginLeft: '25%', float: 'left' }} href="#" onClick={() => { history.push(`${process.env.PUBLIC_URL}/forgot-password`); }}> {translate('updatePassword.page.label.forgotPassword')}</Link>
-                <Link id="signup" style={{ cursor: 'pointer', color: '#2C2799', marginRight: '25%', float: 'right' }} href="#" onClick={() => { history.push(`${process.env.PUBLIC_URL}/signup`); }}> {translate('singUp.page.label.signUp')}</Link>
-              </Typography>
-            </Grid>
+      <MuiThemeProvider theme={ThemeDefault}>
+        <Grid container>
+          <Grid
+            item
+            xs={12}
+            sm={4}
+            md={3}
+            lg={3}
+            color={"primary"}
+            className={classes.appInfo}
+          >
+            {this.renderLeftPanel()}
           </Grid>
-          <div className={classes.buttonsDiv} />
-          {this.state.error && (
-            <Snackbar
-              anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
-              open={this.state.error}
-              autoHideDuration={4000}
-              onClose={this.handleClose}
-              variant="error"
-              message={this.state.errMessage}
-            />
-          )}
-        </div>
+          <Grid item xs={12} sm={9} md={9} lg={9} className={classes.parent}>
+            <form autoComplete="off">{this.renderCardContent()}</form>
+          </Grid>
+        </Grid>
 
+        {this.state.error && (
+          <Snackbar
+            anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
+            open={this.state.error}
+            autoHideDuration={4000}
+            onClose={this.handleClose}
+            variant="error"
+            message={this.state.errMessage}
+          />
+        )}
       </MuiThemeProvider>
-
     );
   }
 }
-
 
 Login.propTypes = {
   user: PropTypes.object.isRequired,
 };
 
-const mapStateToProps = state => ({
-  user: state.login
+const mapStateToProps = (state) => ({
+  user: state.login,
 });
 
-const mapDispatchToProps = dispatch =>
-  bindActionCreators(
-    {
-    },
-    dispatch
-  );
+const mapDispatchToProps = (dispatch) => bindActionCreators({}, dispatch);
 
 export default withRouter(
-  withStyles(LoginStyles)(
-    connect(
-      mapStateToProps,
-      mapDispatchToProps
-    )(Login)
-  )
+  withStyles(LoginStyles)(connect(mapStateToProps, mapDispatchToProps)(Login))
 );
-
--- anuvaad-fe/anuvaad-webapp/src/ui/styles/web/LoginStyles.js
+++ anuvaad-fe/anuvaad-webapp/src/ui/styles/web/LoginStyles.js
@@ -164,6 +164,79 @@ const LoginStyles = theme => ({
     margin: theme.spacing(1),
     position: 'relative',
   },
+  appInfo: {
+    background: "rgba(44, 39, 153, 1)",
+    minHeight: "100vh",
+    color:"white",
+    "@media (max-width:650px)": {
+      background: "white",
+      minHeight: "15vh",
+    },
+  },
+  title: {
+    width: "20%",
+    height: "auto",
+    margin: "22% 294px 10% 39px",
+    cursor:"pointer",
+    lineHeight: "1.53",
+    letterSpacing: "3.9px",
+    textAlign: "left",
+    "@media (max-width:650px)": {
+      margin: "0% 0% 00% 90%",
+      color: "black",
+    },
+    fontSize: "2.5rem",    
+  },
+  body: {
+    width: "80%",
+    height: "auto",
+    margin: "30px 0px 50px 39px",
+    lineHeight: "1.5",
+    letterSpacing: "1.6px",
+    textAlign: "left",
+    color: "#f2f2f4",
+    "@media (max-width:1040px)": {
+      letterSpacing: "1px",
+      maxWidth: "280px",
+    },
+    "@media (min-width:1790px)": {
+      width: "85%",
+    },
+    fontSize: "1.25rem",
+    fontFamily: "Roboto, sans-serif",
+    fontWeight: 400,
+  },
+  parent: {
+    display: "flex",
+    alignItems: "center",
+    flexDirection: "column",
+    justifyContent: "center",
+  },
+  forgotPassLink: {
+    color: "rgb(44, 39, 153)",
+    textDecoration: "underline rgba(44, 39, 153, 0.4)",
+    cursor: "pointer",
+    fontFamily: "Roboto, san-serif",
+    fontSize: "14px",
+  },
+  loginBtn: {
+    borderRadius: "20px",
+    color: "rgb(255, 255, 255)",
+    backgroundColor: "rgb(44, 39, 153)",
+    textTransform: "none",
+    fontFamily: "Roboto, san-serif",
+    "&:hover": {
+      backgroundColor: 'rgb(39, 30, 79);'
+    },
+  },
+  headingStyle: {
+    marginBottom: "15px",
+    fontSize: "1.6875rem",
+    fontFamily: '"Rowdies", cursive,"Roboto" ,sans-serif',
+    fontWeight: 300,
+    letterSpacing: 0,
+    lineHeight: "1.167",
+  },
 });
 
 export default LoginStyles;
