d36e8e8522e6aed09808c1a872dbcd730f549f77
SarathkumarSM <sarathkumarsm2603@gmail.com>
2022-10-19 14:44:01 +0530

    replaced the transform back in response_generation

--- anuvaad-etl/anuvaad-extractor/file_translator/etl-file-translator/resources/response_generation.py
+++ anuvaad-etl/anuvaad-extractor/file_translator/etl-file-translator/resources/response_generation.py
@@ -44,7 +44,7 @@ class Response(object):
                     #
                     log_info(f"Test31: in_file_type = {in_file_type}", None)
                     log_info(f"Test31: transform_flow = {transform_flow}", None)
-                    if in_file_type == "docx": #and transform_flow:
+                    if in_file_type == "docx" and transform_flow:
                         docx_transform_obj = DocxTransform(input_filename=input_filename, json_data=self.json_data)
                         #
                         log_info(f"Test31: docx_transform_object = {docx_transform_obj}",None)
