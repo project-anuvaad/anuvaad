5b5ae363121a4942c751b5097446e2cd71d75faf
MrigankTarento <89840438+MrigankTarento@users.noreply.github.com>
2022-10-21 18:25:57 +0530

    changed the header logo

--- anuvaad-fe/anuvaad-webapp/src/ui/components/web/common/TopHeader.jsx
+++ anuvaad-fe/anuvaad-webapp/src/ui/components/web/common/TopHeader.jsx
@@ -20,7 +20,7 @@ import CloseIcon from '@material-ui/icons/Close';
 import SettingsOutlinedIcon from '@material-ui/icons/SettingsOutlined';
 import React, { useState, useEffect, useRef } from "react";
 import { Link as RouterLink } from "react-router-dom";
-import AnuvaadLogo from "../../../../assets/AnuvaadLogo.svg";
+import AnuvaadLogo from "../../../../assets/HeaderTransparentLogo.png";
 import configs from "../../../../configs/configs";
 import headerMenuConfig from "../../../../configs/headerMenuConfig";
 
@@ -36,7 +36,7 @@ const useStyles = makeStyles((theme) => ({
         },
     },
     logo: {
-        height: "2rem",
+        // height: "2rem",
         width: "6rem"
     },
     // ".MuiButton-label":{
