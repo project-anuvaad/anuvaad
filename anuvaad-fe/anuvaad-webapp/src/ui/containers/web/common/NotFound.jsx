6783572a3baff723b064b3dd98ffba39232b44e7
MrigankTarento <89840438+MrigankTarento@users.noreply.github.com>
2022-11-28 15:40:40 +0530

    redirection logic changed for unknown routes and index routes

--- anuvaad-fe/anuvaad-webapp/src/ui/containers/web/common/NotFound.jsx
+++ anuvaad-fe/anuvaad-webapp/src/ui/containers/web/common/NotFound.jsx
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { translate } from '../../../../assets/localisation';
+import history from "../../../../web.history";
 
-const NotFound = () => (
-  <div>
+const NotFound = () => {
+  useEffect(() => {
+    const currentUserRole = localStorage.getItem("roles");
+    if (currentUserRole) {
+      if (currentUserRole == "TRANSLATOR" || currentUserRole == "ANNOTATOR") {
+        history.push(`${process.env.PUBLIC_URL}/view-document`);
+      } else {
+        history.push(`${process.env.PUBLIC_URL}/user-details`);
+      }
+    }
+  }, [])
+  return (<div>
     <div style={{ textAlign: 'center' }}>
       <center>{translate('notFound.page.text.notFoundError')}</center>
     </div>
-  </div>
-);
+  </div>)
+};
 
 export default NotFound;
