1b633bedd607fc4ad37d1d7fbf73fb86918fdb18
srihari-nagaraj <srihari.nagaraj@tarento.com>
2023-01-16 12:58:55 +0530

    temp fix for analytics

--- anuvaad-api/anuvaad-metrics/anuvaad-org-judgement-count/src/resources/db_retrival.py
+++ anuvaad-api/anuvaad-metrics/anuvaad-org-judgement-count/src/resources/db_retrival.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+import datetime
 import json
 import shutil
 import threading
@@ -16,6 +16,7 @@ from utilities import (
     write_to_csv_user,
     generate_email_notification,
     send_email,
+    write_to_csv_user_daily_crn
 )
 import uuid
 import requests
@@ -50,7 +51,23 @@ def FetchJudgementCount():
         file_save = str(filename)[:-10] + "_JUD_Org_level_Statistics.csv"
         keys = body.keys()
         if "config" in keys and body.get("config") == "cron":
-            get_trans_user_data_from_db_weekly_crn()
+            if not os.path.exists(
+            config.DOWNLOAD_FOLDER + "/" + config.DAILY_CRON_FILE_NAME1
+        ) and not os.path.exists(config.DOWNLOAD_FOLDER + "/" + config.DAILY_CRON_FILE_NAME2):
+                shutil.copyfile(
+                    config.DOWNLOAD_FOLDER + "/" + config.backup_file_1,
+                    config.DOWNLOAD_FOLDER + "/" + config.DAILY_CRON_FILE_NAME1,
+                )
+                shutil.copyfile(
+                    config.DOWNLOAD_FOLDER + "/" + config.backup_file_2,
+                    config.DOWNLOAD_FOLDER + "/" + config.DAILY_CRON_FILE_NAME2,
+                )
+                log_info(
+                    f"files copied from weekly cron to daily cron {config.backup_file_1,config.backup_file_2}",
+                    MODULE_CONTEXT,
+                )
+                get_trans_user_data_from_db_daily_day_crn()
+            # get_trans_user_data_from_db_weekly_crn()
         elif "config" in keys and body.get("config") == "copy":
             copy_cron_csv()
         elif body.get("org"):
@@ -70,7 +87,13 @@ def FetchJudgementCount():
                 f"Data returned from {config.USER_COLLECTION} collection",
                 MODULE_CONTEXT,
             )
-
+        elif "config" in keys and body.get("config") == "remove":
+            if os.path.exists(config.DOWNLOAD_FOLDER + "/" + config.DAILY_CRON_FILE_NAME1):
+                os.remove(config.DOWNLOAD_FOLDER + "/" + config.DAILY_CRON_FILE_NAME1)
+            if os.path.exists(config.DOWNLOAD_FOLDER + "/" + config.DAILY_CRON_FILE_NAME2):
+                os.remove(config.DOWNLOAD_FOLDER + "/" + config.DAILY_CRON_FILE_NAME2)
+            else: 
+                return {"msg":"already deleted"}
         try:
             if "config" in keys:
                 pass
@@ -234,7 +257,7 @@ def FetchJudgementCount_user_wise():
                 log_info(
                     "Generating email notification for found data !!!!", MODULE_CONTEXT
                 )
-                tdy_date = datetime.now(IST).strftime("%Y:%m:%d %H:%M:%S")
+                # tdy_date = datetime.now(IST).strftime("%Y:%m:%d %H:%M:%S")
                 msg = generate_email_notification(users, "Data Generated Successfully")
                 for i, j in enumerate(files):
                     with open(j, "rb") as content_file:
@@ -558,6 +581,76 @@ def get_trans_user_data_from_db_weekly_crn():
         send_email(msg)
         log_exception(
             "Generated alert email in exception weekly cron job : {}".format(str(e)),
+            MODULE_CONTEXT,
+            e,
+        )
+        return
+
+
+def get_trans_user_data_from_db_daily_day_crn():
+    users = config.EMAIL_NOTIFIER
+    log_info("fetch data started", MODULE_CONTEXT)
+    # filename = uuid.uuid4().hex
+    daily_cron_file_name1 = config.DAILY_CRON_FILE_NAME1
+    daily_cron_file_name2 = config.DAILY_CRON_FILE_NAME2
+    # weekly_cron_file_name1 = config.WEEKLY_CRON_FILE_NAME1
+    # weekly_cron_file_name2 = config.WEEKLY_CRON_FILE_NAME2
+    # file_save = str(filename)[:-10]+'_USER_WISE_JUD_Org_level_Statistics.csv'
+    if os.path.exists(
+        config.DOWNLOAD_FOLDER + "/" + daily_cron_file_name1
+    ) and os.path.exists(config.DOWNLOAD_FOLDER + "/" + daily_cron_file_name2):
+        pass
+    else:
+        msg = generate_email_notification(
+            users, "could not get the data files not found"
+        )
+        send_email(msg)
+        log_info(
+            f"Generated alert email for daily cron (files not found),{daily_cron_file_name1,daily_cron_file_name2} ",
+            MODULE_CONTEXT,
+        )
+    user_docs = stats.get_all_users_active_inactive(usr_collection)
+    log_info(
+        f"Data returned from user {config.USER_COLLECTION} collection", MODULE_CONTEXT
+    )
+    try:
+        df = pd.read_csv(config.DOWNLOAD_FOLDER + "/" + daily_cron_file_name1)
+        from_datee = df["created_on"].max()
+        from_date = datetime.datetime.strptime(str(from_datee), "%Y-%m-%d %H:%M:%S.%f")
+        now = datetime.datetime.now()
+        date_time = now.strftime("%Y-%m-%d")
+        end_date = datetime.datetime.strptime(str(date_time), "%Y-%m-%d")
+        # from_date, end_date = stats.get_time_frame_for_analytics()
+        for doc in user_docs:
+            # log_info(f'fetching details for {doc} userID',MODULE_CONTEXT)
+            ch_docs = stats.fetch_data_for_language_trans_tokenized_for_scheduer_only(
+                ch_collection, doc, from_date, end_date
+            )
+            saved_docs = stats.fetch_data_for_userwise_trans_user_tokenized(
+                ch_collection, doc, from_date, end_date
+            )
+            # log_info(f'Details collected for for userID : {doc} ',MODULE_CONTEXT)
+            write_to_csv_user_daily_crn(
+                [x for x in ch_docs],
+                (config.DOWNLOAD_FOLDER + "/" + daily_cron_file_name1),
+            )
+            write_to_csv_user_daily_crn(
+                [x for x in saved_docs],
+                (config.DOWNLOAD_FOLDER + "/" + daily_cron_file_name2),
+            )
+        log_info(
+            f"Data written into files {daily_cron_file_name1,daily_cron_file_name2}",
+            MODULE_CONTEXT,
+        )
+        return
+    except Exception as e:
+        log_exception("Error in fetching the data: {}".format(e), MODULE_CONTEXT, e)
+        msg = generate_email_notification(
+            users, "could not get the data something went wrong : {}".format(e)
+        )
+        send_email(msg)
+        log_exception(
+            "Generated alert email in exception daily cron job : {}".format(str(e)),
             MODULE_CONTEXT,
             e,
         )
