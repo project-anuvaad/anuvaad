d790a75e34630375051e4be20b0cfa9d7cddcb77
srihari-nagaraj <srihari.nagaraj@tarento.com>
2023-03-31 09:57:29 +0530

    refactoring query optimize for analytics

--- anuvaad-api/anuvaad-metrics/anuvaad-org-judgement-count/src/services/__init__.py
+++ anuvaad-api/anuvaad-metrics/anuvaad-org-judgement-count/src/services/__init__.py
@@ -1,6 +1,4 @@
 from .crons import (
-    get_trans_user_data_from_db_weekly_crn_file2,
-    get_trans_user_data_from_db_weekly_crn_file1,
-    get_trans_user_data_from_db_daily_day_crn,
     copy_cron_csv,
+    get_trans_user_data_from_db_weekly_crn,
 )
