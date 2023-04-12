452f67599cd0f55a1531b9fb9c86207a6c88dd58
srihari-nagaraj <srihari.nagaraj@tarento.com>
2023-04-12 20:54:49 +0530

    query changes

--- anuvaad-etl/anuvaad-extractor/content-handler/src/models/block.py
+++ anuvaad-etl/anuvaad-extractor/content-handler/src/models/block.py
@@ -113,11 +113,14 @@ class BlockModel(object):
         try:
             collections = get_db()[MONGO_s3_LINK_STORE]
             record = collections.find({"job_id":data["job_id"]})
-            if record.count() == 0:
-                collections.insert(data)
+            # if record.count() == 0:
             if record.count() != 0:
+                print(True)
                 collections.update_one({'job_id': data['job_id']},
-                            { '$set': { "file_link.parallel_doc" : data['file_link']} })
+                            { '$set': { "file_link.parallel_doc" : data['file_link']['parallel_doc']} })
+            else:
+                print(False)
+                collections.insert(data)
             
         except Exception as e:
             log_exception("db connection exception |{}".format(str(e)),  AppContext.getContext(), e)
