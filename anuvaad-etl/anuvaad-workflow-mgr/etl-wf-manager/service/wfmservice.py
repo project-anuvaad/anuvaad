babd844ec5ccdc8177b0b06a1f999d2b213c62e1
rathan-muralidhar <rathan.muralidhar@gmail.com>
2023-05-15 13:58:54 +0530

    Minor Fix

--- anuvaad-etl/anuvaad-workflow-mgr/etl-wf-manager/service/wfmservice.py
+++ anuvaad-etl/anuvaad-workflow-mgr/etl-wf-manager/service/wfmservice.py
@@ -366,6 +366,7 @@ class WFMService:
     # Method to search jobs on multiple criteria.
     def get_job_details_bulk(self, req_criteria, skip_pagination, isReviewer=False):
         try:
+            criteria = {}
             if isReviewer == False:
                 criteria = {"metadata.userID": {"$in": req_criteria["userIDs"]}}
             if 'jobIDs' in req_criteria.keys():
