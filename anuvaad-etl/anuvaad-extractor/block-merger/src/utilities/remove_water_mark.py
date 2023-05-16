0cb4f36d4761e778148dcd67e67bb7ecf021200c
PromodhPinto <promodhpinto5368@gmail.com>
2023-05-16 16:01:41 +0530

    watermark update

--- anuvaad-etl/anuvaad-extractor/block-merger/src/utilities/remove_water_mark.py
+++ anuvaad-etl/anuvaad-extractor/block-merger/src/utilities/remove_water_mark.py
@@ -38,6 +38,6 @@ def clean_image(image):
 #      image[config.WATERMARK_THRESHOLD_LOW < image ] = 255
 #      return image
 
-def background_image(image):
-     image[config.WATERMARK_THRESHOLD_LOW > image ] = 255
-     return image
+# def background_image(image):
+#      image[config.WATERMARK_THRESHOLD_LOW > image ] = 255
+#      return image
