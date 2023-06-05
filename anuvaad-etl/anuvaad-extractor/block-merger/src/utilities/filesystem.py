e14e84ca2c0773b17006d38daa0992b59a2cdeb2
PromodhPinto <promodhpinto5368@gmail.com>
2023-06-05 10:17:40 +0530

    remove extra images

--- anuvaad-etl/anuvaad-extractor/block-merger/src/utilities/filesystem.py
+++ anuvaad-etl/anuvaad-extractor/block-merger/src/utilities/filesystem.py
@@ -2,6 +2,7 @@ import os
 import shutil
 import glob
 import pdf2image
+import xml.etree.ElementTree as ET
 from anuvaad_auditor.loghandler import log_info
 from anuvaad_auditor.loghandler import log_error
 
@@ -35,6 +36,40 @@ def extract_image_paths_from_pdf(filepath, workspace_output_dir):
     paths           = pdf2image.convert_from_path(filepath, dpi=300, output_file=image_filename, output_folder=working_dir, fmt='jpg', paths_only=True)
     return paths
 
+def remove_extra_images(xml_file):
+    # Parse the XML file
+    output_path = xml_file
+    tree = ET.parse(xml_file)
+    root = tree.getroot()
+    # Find all page elements in the XML file
+    page_elements = root.findall('.//page')
+
+    # Iterate over the page elements
+    for page_element in page_elements:
+        # Find all image elements within the page element
+        image_elements = page_element.findall('image')
+
+        # Track the first image per page
+        first_image_per_page = None
+
+        # Iterate over the image elements
+        for image_element in image_elements:
+            # Remove image elements except the first per page
+            if first_image_per_page is None:
+                first_image_per_page = image_element
+            elif image_element != first_image_per_page:
+                page_element.remove(image_element)
+                # Remove the corresponding image file
+                image_path = image_element.attrib['src']
+                # image_path = os.path.join(image_dir, image_filename)
+                if os.path.exists(image_path):
+                    os.remove(image_path)
+
+    # Save the modified XML file
+    print(output_path)
+    tree.write(output_path)
+
+
 def extract_xml_path_from_digital_pdf(filepath, workspace_output_dir):
     """
         function extracts the XML by using PDF2HTML commandline tool
@@ -53,7 +88,7 @@ def extract_xml_path_from_digital_pdf(filepath, workspace_output_dir):
     os.system(cmd)
 
     xml_files      = read_directory_files(working_dir, pattern='*.xml')
-
+    remove_extra_images(xml_files[0])
     return xml_files[0]
 
 def extract_html_bg_image_paths_from_digital_pdf(filepath, workspace_output_dir):
