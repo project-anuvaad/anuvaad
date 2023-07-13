ea12e579f54ea10b5c56596d7db8ecd70ef3abd3
Abhishek suga <abhishek.suga@tarento.com>
2023-07-13 17:24:15 +0530

    Added support for sindhi devanagari language

--- anuvaad-etl/anuvaad-extractor/sentence/etl-tokeniser/services/service.py
+++ anuvaad-etl/anuvaad-extractor/sentence/etl-tokeniser/services/service.py
@@ -33,7 +33,7 @@ class Tokenisation(object):
                     if text_locale == 'en':
                         tokenised_sentence_data = AnuvaadEngTokenizer().tokenize(paragraph)
                         tokenised_text.extend(tokenised_sentence_data)
-                    elif text_locale == 'hi' or text_locale == 'mr' or text_locale == 'ne' or text_locale == 'gom' or text_locale == 'brx':
+                    elif text_locale == 'hi' or text_locale == 'mr' or text_locale == 'ne' or text_locale == 'gom' or text_locale == 'brx' or text_locale == 'sd_Deva':
                         tokenised_sentence_data = AnuvaadHindiTokenizer().tokenize(paragraph)
                         tokenised_text.extend(tokenised_sentence_data)
                     elif text_locale == 'kn':
