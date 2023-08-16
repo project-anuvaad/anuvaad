b25755d8ec9919ae1405af568e828dfcc447bda1
MrigankTarento <mrigank.shringi@tarento.com>
2023-08-16 10:58:28 +0530

    fixed config issue

--- anuvaad-fe/anuvaad-webapp/src/configs/configs.js
+++ anuvaad-fe/anuvaad-webapp/src/configs/configs.js
@@ -1,6 +1,6 @@
 const configs = {
     BASE_URL: 'http://52.40.71.62:5000',
-    BASE_URL_AUTO: process.env.REACT_APP_APIGW_BASE_URL ? process.env.REACT_APP_APIGW_BASE_URL : 'https://users-auth.anuvaad.org',
+    BASE_URL_AUTO: process.env.REACT_APP_APIGW_BASE_URL ? process.env.REACT_APP_APIGW_BASE_URL : 'https://auth.anuvaad.org',
     BASE_URL_NMT: 'http://52.40.71.62:3000',
     DASBOARD_URL: process.env.REACT_APP_DASHBOARD_URL ? process.env.REACT_APP_DASHBOARD_URL : 'https://dev-dashboard.anuvaad.org/api',
     DEV_SALT: process.env.SALT ? process.env.SALT : '85U62e26b2aJ68dae8eQc188e0c8z8J9',
