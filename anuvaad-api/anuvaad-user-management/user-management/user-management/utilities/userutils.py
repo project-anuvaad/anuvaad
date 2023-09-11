060d484d3916ba89bd38eb739ede0e579d8ff299
dhiraj suthar <dhiru579@gmail.com>
2023-09-11 17:36:01 +0530

    fix: no uppercase in username

--- anuvaad-api/anuvaad-user-management/user-management/user-management/utilities/userutils.py
+++ anuvaad-api/anuvaad-user-management/user-management/user-management/utilities/userutils.py
@@ -58,7 +58,7 @@ class UserUtils:
     def validate_email(email):
         """Regex validation for emails."""
 
-        regex = "([a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+)"
+        regex = "(^[a-z0-9\_\.\+\-]+@[a-z0-9-]+\.[a-z0-9-.]+$)"
         if re.search(regex, email):
             return True
         else:
