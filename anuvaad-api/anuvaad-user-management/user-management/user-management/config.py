81846829198a0a5461e569546972353e9ad03b1f
Siddanthshaiva <siddanth.shaiva@gmail.com>
2023-12-20 12:31:27 +0530

    minor fix

--- anuvaad-api/anuvaad-user-management/user-management/user-management/config.py
+++ anuvaad-api/anuvaad-user-management/user-management/user-management/config.py
@@ -23,7 +23,7 @@ ACTIVE_USERS_EXP_TIME   = int(os.environ.get('ACTIVE_USERS_EXP_TIME', 5 * 60 )) 
 #mongodb-configs
 MONGO_DB_SCHEMA                 =   os.environ.get('MONGO_DB_IDENTIFIER', 'usermanagement')
 USR_MONGO_COLLECTION            =   os.environ.get('UMS_USR_COLLECTION', 'sample')
-VERIFY_USERS                    =   os.environ.get('VERIFY_USER', 'verify-user')
+VERIFY_USERS                    =   os.environ.get('VERIFY_USER_COLLECTION', 'verify-user')
 MFA_MONGO_COLLECTION            =   os.environ.get('UMS_MFA_COLLECTION', 'mfa')
 USR_TOKEN_MONGO_COLLECTION      =   os.environ.get('UMS_USR_TOKEN_COLLECTION', 'usertokens')
 USR_TEMP_TOKEN_MONGO_COLLECTION =   os.environ.get('UMS_USR_TEMP_TOKEN_COLLECTION', 'usertemptoken')
@@ -77,7 +77,7 @@ MAIL_SETTINGS = {
 
 #React-app base url
 BASE_URL                =   os.environ.get('REACT_APP_BASE_URL','https://developers.anuvaad.org/')
-EMAIL_GET_URL_NOTIFICATION = os.environ.get('NOTIFICATION_URL', 'http://127.0.0.1:5001/anuvaad/user-mgmt/v1/users/validated-and-onboard')
+EMAIL_GET_URL_NOTIFICATION = os.environ.get('NOTIFICATION_ENDPOINT', 'http://127.0.0.1:5001/anuvaad/user-mgmt/v1/users/validated-and-onboard')
 
 # MFA configs
 MFA_ENABLED = bool(os.environ.get("MFA_ENABLED",False))
