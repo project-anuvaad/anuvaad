e2430499271eb0c58691313745d5e8aa7c8785f1
Siddanthshaiva <siddanth.shaiva@gmail.com>
2023-12-20 12:17:23 +0530

    nomenclat

--- anuvaad-api/anuvaad-user-management/user-management/user-management/config.py
+++ anuvaad-api/anuvaad-user-management/user-management/user-management/config.py
@@ -23,7 +23,7 @@ ACTIVE_USERS_EXP_TIME   = int(os.environ.get('ACTIVE_USERS_EXP_TIME', 5 * 60 )) 
 #mongodb-configs
 MONGO_DB_SCHEMA                 =   os.environ.get('MONGO_DB_IDENTIFIER', 'usermanagement')
 USR_MONGO_COLLECTION            =   os.environ.get('UMS_USR_COLLECTION', 'sample')
-VERIFY_USERS                    =   os.environ.get('VERIFY-USER', 'verify-user')
+VERIFY_USERS                    =   os.environ.get('VERIFY_USER', 'verify-user')
 MFA_MONGO_COLLECTION            =   os.environ.get('UMS_MFA_COLLECTION', 'mfa')
 USR_TOKEN_MONGO_COLLECTION      =   os.environ.get('UMS_USR_TOKEN_COLLECTION', 'usertokens')
 USR_TEMP_TOKEN_MONGO_COLLECTION =   os.environ.get('UMS_USR_TEMP_TOKEN_COLLECTION', 'usertemptoken')
