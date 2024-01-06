c5ac30e247bfb42d9edd10c77e95863bf1fef6c8
PromodhPinto <promodhpinto5368@gmail.com>
2024-01-07 01:46:02 +0530

    minor changes

--- anuvaad-etl/anuvaad-extractor/document-processor/ocr/ocr-tesseract-server/src/utilities/tesseract/multiprocess.py
+++ anuvaad-etl/anuvaad-extractor/document-processor/ocr/ocr-tesseract-server/src/utilities/tesseract/multiprocess.py
@@ -203,8 +203,10 @@ def multi_processing_tesseract(page_regions, image_path, lang, width, height):
                                         # Replace the values of ['text'] in the JSON data sequentially
                                         index = 0
                                         for idx, entry in enumerate(updated_lines):
+                                            entries = entry.get('regions', [])
                                             # if first_vertex_y is None:
-                                            dynamic_first_vertex_y = entry['regions'][idx]['boundingBox']['vertices'][0]['y']
+                                            if idx < len(entries):
+                                                dynamic_first_vertex_y = entry['regions'][idx]['boundingBox']['vertices'][0]['y']
                                             # first_vertex_y = dynamic_first_vertex_y
                                             for region in entry['regions']:
                                                 # Check if index is greater than or equal to len(split_text)
