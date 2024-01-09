d533260de0901fc5999cfd893b4a28f439ede58e
Siddanthshaiva <siddanth.shaiva@gmail.com>
2024-01-09 16:55:09 +0530

    add phoneNumber and comment to validate-signup API

--- anuvaad-api/anuvaad-user-management/user-management/user-management/resources/users.py
+++ anuvaad-api/anuvaad-user-management/user-management/user-management/resources/users.py
@@ -175,22 +175,22 @@ class validateSignUp(Resource):
     def post(self):
         body = request.get_json()
         #log_info(f"{token}somethinf somthing soimthiung",MODULE_CONTEXT)
-        req_params = ['fullName','email','orgID','averageDocTranslationsPerDay']
+        req_params = ['fullName','email','orgID','averageDocTranslationsPerDay','phoneNumber','comment']
         if isinstance(body,dict):
             for param in body.keys():
                 if param not in req_params:
-                    return post_error("Data Missing", "Please enter following details. 'fullName','email','orgID','averageDocTranslationsPerDay'", None), 400
+                    return post_error("Data Missing", "Please enter following details. 'fullName','email','orgID','averageDocTranslationsPerDay', 'phoneNumber' and 'comment'", None), 400
 
         
-        userName,email,orgID,averageDocTranslationsPerDay  = body['fullName'], body['email'], body['orgID'], body['averageDocTranslationsPerDay']
+        userName,email,orgID,averageDocTranslationsPerDay, phoneNumber, comment  = body['fullName'], body['email'], body['orgID'], body['averageDocTranslationsPerDay'], body['phoneNumber']
         usr_details = userRepo.validate_user_for_docs(email)
         if not usr_details:#add user to collections, create token and generate timeStamp.
             token_and_epoch_gen = userRepo.token_generation_and_timestamp()
-            adduser = userRepo.add_usr_details(userName, email, orgID,token_and_epoch_gen, averageDocTranslationsPerDay)
+            adduser = userRepo.add_usr_details(userName, email, orgID,token_and_epoch_gen, averageDocTranslationsPerDay, phoneNumber,comment)
             if adduser == "SUCCESS":
                 #return 
                 token = token_and_epoch_gen[0]
-                userRepo.send_mail_to_admin(email,userName, orgID, averageDocTranslationsPerDay,admin_email, token)
+                userRepo.send_mail_to_admin(email,userName, orgID, averageDocTranslationsPerDay,phoneNumber,admin_email, token)
                 res = CustomResponse(Status.SUCCESS_USER_RESPONSE_PAGE.value, adduser)
                 return res.getresjson(), 200
         return post_error("Existing user","User already exists, please login.", None), 400
