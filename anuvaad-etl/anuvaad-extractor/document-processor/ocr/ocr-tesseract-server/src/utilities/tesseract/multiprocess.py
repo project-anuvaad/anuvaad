bf445ffbc1c31520b27650b018f303a518e65ba2
PromodhPinto <promodhpinto5368@gmail.com>
2024-01-25 12:04:42 +0530

    minor changes

--- anuvaad-etl/anuvaad-extractor/document-processor/ocr/ocr-tesseract-server/src/utilities/tesseract/multiprocess.py
+++ anuvaad-etl/anuvaad-extractor/document-processor/ocr/ocr-tesseract-server/src/utilities/tesseract/multiprocess.py
@@ -205,8 +205,8 @@ def multi_processing_tesseract(page_regions, image_path, lang, width, height):
                                     elif config.HANDWRITTEN_OCR:
                                         alphabet = f'{lang_detected}_lexicon.txt'
 
-                                        pretrained = f'src/utilities/indic_hw_ocr/models/{lang_detected}_indic_pretrained_best_wer.pth'
-                                        out_dir = f'src/utilities/indic_hw_ocr/{lang_detected}'
+                                        pretrained = f'./src/utilities/indic_hw_ocr/models/{lang_detected}_indic_pretrained_best_wer.pth'
+                                        out_dir = f'./src/utilities/indic_hw_ocr/{lang_detected}'
                                         language = {lang_detected}
 
                                         test_root = 'crops/'
