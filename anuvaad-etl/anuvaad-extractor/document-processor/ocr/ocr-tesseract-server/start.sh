f398acb1991c1200a28b740e1d365520e3b1498c
PromodhPinto <promodhpinto5368@gmail.com>
2024-01-25 12:19:06 +0530

    minor changes

--- anuvaad-etl/anuvaad-extractor/document-processor/ocr/ocr-tesseract-server/start.sh
+++ anuvaad-etl/anuvaad-extractor/document-processor/ocr/ocr-tesseract-server/start.sh
@@ -29,7 +29,8 @@ url_mar="https://anuvaad-pubnet-weights.s3.amazonaws.com/anuvaad_mar.traineddata
 
 ori_modelpath="/usr/share/tesseract-ocr/4.00/tessdata/anuvaad_ori.traineddata"
 url_ori="https://anuvaad-pubnet-weights.s3.amazonaws.com/anuvaad_ori.traineddata?AWSAccessKeyId=${AWSACCESSKEYID_VALUE}&Signature=e%2BZ2y%2BS15yZuYFiqqGTIcnNzBM8%3D&Expires=1702454243"
-ta_hw_path="./src/utilities/indic_hw_ocr/models/tamil_indic_pretrained_best_wer.pth"
+
+ta_hw_path="./src/utilities/indic_hw_ocr/models/Tamil_indic_pretrained_best_wer.pth"
 ta_hw="https://anuvaad-pubnet-weights.s3.amazonaws.com/tamil_indic_pretrained_best_wer.pth?AWSAccessKeyId=${AWSACCESSKEYID_VALUE}&Signature=NPUNt1GFjPm%2BrURuStze0ryrmsY%3D&Expires=1766607336"
 #rm $tam_modelpath
 
