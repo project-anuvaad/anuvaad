a7e87dffef59f91a970932dfb39c9396d5d275bd
PromodhPinto <promodhpinto5368@gmail.com>
2024-01-30 00:44:19 +0530

    minor changes

--- anuvaad-etl/anuvaad-extractor/document-processor/ocr/ocr-tesseract-server/src/services/ocr.py
+++ anuvaad-etl/anuvaad-extractor/document-processor/ocr/ocr-tesseract-server/src/services/ocr.py
@@ -89,10 +89,10 @@ def verify__table_structure(regions):
 
 def segment_regions(lang,path,file,words, lines,regions,file_properties,idx):
     width, height = file_properties.get_pageinfo(0)
-    langs = ['gu','bn']
+    langs = ['gu']
     if lang in langs:
         v_list, n_text_regions = region_unifier.region_unifier(idx,file,words,regions,path)
-    else: v_list, n_text_regions = region_unifier.region_unifier(idx,file,lines,regions,path)
+    else: v_list, n_text_regions = region_unifier.region_unifier(idx,file,lines,regions,path)s
     log_info("tesseract ocr started", None)
     #if idx==2:
     v_list = multi_processing_tesseract(v_list,path,lang,width, height)
