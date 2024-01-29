6c3f56bb3ad6475bd943fa43a417e9983719d599
PromodhPinto <promodhpinto5368@gmail.com>
2024-01-29 20:13:31 +0530

    minor changes

--- anuvaad-etl/anuvaad-extractor/document-processor/word-detector/craft/src/utilities/craft_pytorch/detect.py
+++ anuvaad-etl/anuvaad-extractor/document-processor/word-detector/craft/src/utilities/craft_pytorch/detect.py
@@ -243,9 +243,11 @@ def detect_text(images,language) :
     try:
         word_coordinates = []
         line_coordinates = []
+        line_in_df = None
+        word_in_dfs = None
         for index,image_set in enumerate(images):
-            lang_p = language[index]
             lang = 'hi'
+            lang_p = language[index]
             langs = ['ta','hi','en']
             if lang_p in langs:
                 word_in_dfs = detect_text_per_file(image_set,network=False,\
@@ -255,8 +257,10 @@ def detect_text(images,language) :
                 line_in_df  = detect_text_per_file(image_set,network=True,\
                                                     text_threshold=config.LANGUAGE_LINE_THRESOLDS[lang]['text_threshold'],\
                                                     low_text_threshold= config.LANGUAGE_LINE_THRESOLDS[lang]['low_text'],link_threshold =config.LANGUAGE_LINE_THRESOLDS[lang]['link_threshold'])
-            word_coordinates.append(word_in_dfs)
-            line_coordinates.append((line_in_df))
+            if word_in_dfs:
+                word_coordinates.append(word_in_dfs)
+            if line_in_df:
+                line_coordinates.append(line_in_df)
     except Exception as e :
         log_error('error detecting text' + str(e), app_context.application_context, e)
         return None,None
