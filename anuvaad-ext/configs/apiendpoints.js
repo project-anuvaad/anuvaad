4b6447be75471e773714accaa8cf7597bbb5f096
MrigankTarento <mrigank.shringi@tarento.com>
2024-02-15 14:48:55 +0530

    API call refactor for chrome extension

--- anuvaad-ext/configs/apiendpoints.js
+++ anuvaad-ext/configs/apiendpoints.js
@@ -1,5 +1,5 @@
 const apiEndPoints = {
-  fetch_models: "/nmt-inference/v2/fetch-models",
+  fetch_models: "/aai4b-nmt-inference/v3/fetch-models",
   sync_initiate: "/anuvaad-etl/wf-manager/v1/workflow/sync/initiate",
   fetch_language: "/fetch-languages",
   get_token: "/anuvaad/user-mgmt/v1/extension/users/get/token",
